ae6d20121e6666d0046b5dec1bb9281e8531df98
Tim McConechy <tim.mcconechy@infor.com>
2014-04-29 10:37:42 -0400

    More Fixes across themes

--- public/js/demo.js
+++ public/js/demo.js
@@ -2,14 +2,21 @@
 * Demo JS Code
 */
 $(function($) {
+  var theme = $('#theme').val();
 
+  //Change Theme when we select in the Drop Down
   $('#theme').on('change', function() {
-    var theme = $(this).val();
+    theme = $(this).val();
     //swap style sheets..
     $('body').fadeOut(function() {
       $('#stylesheet').attr('href', '/stylesheets/'+ theme +'.css');
       $(this).fadeIn();
     });
   });
+
+  //Set Initial Theme
+  if (theme !== 'grey-theme') {
+    $('#stylesheet').attr('href', '/stylesheets/'+ theme +'.css');
+  }
 
 });
