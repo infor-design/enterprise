3a823014e78dd3819a226c63830269586a200125
Ed Coyle <Ed.Coyle@infor.com>
2014-09-02 16:51:54 -0400

    Fixed issues with Backspacing one or multiple characters in structured format.  Removed dependencies on the settings.pattern property in most of the code to make individual instances use their own patterns instead of the global pattern.  General commenting, cleanup, and readability changes.

--- js/format.js
+++ js/format.js
@@ -39,7 +39,6 @@
         this.init();
     }
 
-    // Actual Plugin Code
     Plugin.prototype = {
       init: function(){
         var self = this;
@@ -56,29 +55,42 @@
         };
 
         // Order of operations when choosing pattern strings:
-        // jQuery Options > HTML5 'data-format' attribute > Generic pattern string based on "type" attribute > nothing.
+        // HTML5 'data-format' attribute > Generic pattern string based on "type" attribute > nothing.
         //
         // if no pattern is provided in settings, use a pre-determined pattern based
         // on element type, or grab the pattern from the element itself.
-        if (!settings.pattern || settings.pattern === '') {
-          var elemPattern = settings.pattern = self.element.attr('data-format') || '';
-          if (!elemPattern) {
-            self.getPatternForType();
-          }
+        self.pattern = self.element.attr('data-format') || settings.pattern || '';
+        if (!self.pattern || self.pattern === '') {
+          self.getPatternForType();
         }
+
+        // Point all keyboard related events to the handleKeyEvents() method, which knows how to
+        // deal with key syphoning and event propogation.
         self.element.on('keydown.format keypress.format ' + self.env.pasteEvent, null, function(e) {
           self.handleKeyEvents.apply(self, arguments);
         });
+
+        // when the element is focused, store its initial value.
         self.element.on('focus.format', null, function(e) {
-          self.focusEvent.apply( self, arguments );
-        });
-        self.element.on('blur.format', null, function(e) {
-          self.handleEnter.apply( self, arguments );
+          self.initValue = self.element.val();
         });
 
-        console.log('Input field formatter active on element #' + self.element );
+        // remove the value when blurred
+        self.element.on('blur.format', null, function(e) {
+          delete this.initValue;
+        });
+
+        // Test contents of the input field.  If there are characters, run them
+        // against the mask and fill them in as necessary.
+        var val = self.element.val();
+        if ( val.length > 0 ) {
+          self.processStringAgainstMask( val );
+        }
+
       },
 
+      // Builds a fake element and gets the name of the event that will be used for "paste"
+      // Used for cross-browser compatability.
       getPasteEvent: function() {
         var el = document.createElement('input'),
             name = 'onpaste';
@@ -86,8 +98,53 @@
         return ((typeof el[name] === 'function') ? 'paste' : 'input') + '.format';
       },
 
-      //Helper Function for Caret positioning
-      //from jQuery.maskedInput
+      // Gets rid of event firing and bubbling in all browsers.
+      killEvent: function(e) {
+        e.returnValue = false;
+        if (e.preventDefault) {
+          e.preventDefault();
+        }
+        return false;
+      },
+
+      // used for getting the proper keyCode ID number with cross-browser compatability.
+      typedCode: function(event) {
+        var code=0;
+        if ( event === null && window.event ) {
+          event = window.event;
+        }
+        if( event !== null ) {
+          if ( event.keyCode ) {
+            code = event.keyCode;
+          } else if ( event.which ) {
+            code = event.which;
+          }
+        }
+        return code;
+      },
+
+      // Uses the "type" attribute on an element to determine a default pattern.
+      // This is called when "$.format" is invoked on a field that contains an empty "data-format" attribute.
+      getPatternForType: function() {
+        var self = this,
+          type = self.element.attr('type');
+
+        // TODO: flesh this out
+        switch(type) {
+          case 'number':
+            self.pattern = '99.99';
+            break;
+          case 'tel':
+            self.pattern = '(999) 999-9999';
+            break;
+          default:
+            self.pattern = '**********';
+        }
+      },
+
+      // Helper Function for Caret positioning.  If you provide "begin" and "end" arguments, the caret position
+      // will change.  If you simply call the method with no arguments, it returns an object containing the cursor's
+      // beginning and ending posititons.
       caret: function(begin, end) {
         var self = this,
           range;
@@ -126,47 +183,40 @@
         }
       },
 
-      // used for getting the proper keyCode for cross-browser compatability.
-      typedCode: function(event) {
-        var code=0;
-        if ( event === null && window.event ) {
-          event = window.event;
-        }
-        if( event !== null ) {
-          if ( event.keyCode ) {
-            code = event.keyCode;
-          } else if ( event.which ) {
-            code = event.which;
-          }
-        }
-        return code;
-      },
+      // Moves the text input cursor a specified distance in a specified direction
+      moveCursor: function( direction, distance ) {
+        var self = this;
+        var pos = self.caret();
 
-      getPatternForType: function() {
-        var self = this,
-          type = self.element.attr('type');
+        var _direction = self.evaluateDirecton(direction);
+        var _d = distance || 1;
 
-        // TODO: flesh this out
-        switch(type) {
-          case 'number':
-            settings.pattern = '99.99';
+        switch(_direction) {
+          case 'next':
+            self.caret( pos.begin + _d );
             break;
-          case 'tel':
-            settings.pattern = '(999) 999-9999';
+          case 'prev':
+            self.caret( pos.begin - _d );
             break;
           default:
-            settings.pattern = '**********';
+            break;
         }
       },
 
-      killEvent: function(e) {
-        e.returnValue = false;
-        if (e.preventDefault) {
-          e.preventDefault();
+      // Get the direction in which to position the cursor (if necessary)
+      // defaults to 'current', which will not move the cursor.
+      evaluateDirecton: function( direction ) {
+        var _direction = 'current';
+        var directions = ['current', 'next', 'prev'];
+        if ( direction ) {
+          var i = $.inArray( direction, directions );
+          _direction = directions[i];
         }
-        return false;
+        return _direction;
       },
 
+      // The catch-all event for handling keyboard events within this input field. Grabs information about the keys
+      // being pressed, event type, matching pattern characters, and determines what to do with them.
       handleKeyEvents: function(e) {
         var evt = e || window.event;
         var eventType = evt.originalEvent.type;
@@ -187,12 +237,12 @@
           if (key === 8 || key === 46 || (this.env.iPhone && key === 127)) {
             this.handleBackspace(evt);
           } else if (key === 13) { // enter
-            this.handleEnter(evt);
+            this.element.trigger('blur', evt);
           } else if (key === 27) { // escape
             this.handleEscape(evt);
           } else if (36 < key && key < 41) { // arrow keys (in Firefox)
             return;
-          } else if ( e.shiftKey && 36 < key && key < 41 ) { // arrow keys AND shift key (for moving the cursor)
+          } else if ( evt.shiftKey && 36 < key && key < 41 ) { // arrow keys AND shift key (for moving the cursor)
             return;
           }
         }
@@ -200,11 +250,11 @@
         if ( eventType === 'keypress' ) {
           console.log('KEYPRESS!');
           // Ignore all of these keys or combinations containing these keys
-          if (e.ctrlKey || e.altKey || e.metaKey || key < 32) {
+          if (evt.ctrlKey || evt.altKey || evt.metaKey || key < 32) {
             return;
           // Need to additionally check for arrow key combinations here because some browsers
           // Will fire keydown and keypress events for arrow keys.
-          } else if ( e.shiftKey  && 36 < key && key < 41) {
+          } else if ( evt.shiftKey  && 36 < key && key < 41) {
             return;
           } else if ( 36 < key && key < 41 ) {
             return;
@@ -213,7 +263,7 @@
         }
 
         if ( eventType === 'paste' ) {
-          console.log('PASTE!');
+          this.handlePaste(evt);
         }
 
         if ( eventType === 'input') {
@@ -221,23 +271,25 @@
         }
       },
 
-      // Handles the backspace key
+      // When using Backspace, correctly remove the intended text content and place the caret
+      // in the correct place.
       handleBackspace: function(e) {
         var val = this.element.val();
         if ( 0 < val.length ) {
           var pos = this.caret();
           var dCaret = pos.end - pos.begin;
-          var sliceLength = dCaret > 0 ? dCaret : 1;
-          this.element.val( val.slice(0, -sliceLength) );
+          var trueCaretPosBegin = dCaret > 0 ? pos.begin : pos.begin - 1;
+          var selectedText = val.slice(trueCaretPosBegin, pos.end);
+
+          val = this.deleteAtIndex(val, selectedText, trueCaretPosBegin);
+          this.element.val( val );
+          this.caret( trueCaretPosBegin );
         }
         return this.killEvent(e);
       },
 
-      handleEnter: function(e) {
-        var el = e.currentTarget.tagName + '#' + e.currentTarget.id;
-        console.log('Element ' + el + ' Blurred');
-      },
-
+      // When escaping from a modified field, place the initial value of the field
+      // back in place of the discarded edits.
       handleEscape: function(e) {
         var self = this;
         self.element.val( self.initValue );
@@ -246,18 +298,53 @@
         return self.killEvent(e);
       },
 
+      // Intercepts the Paste event, modifies the contents of the clipboard to fit within the size
+      // and character limits of the mask, and writes the result to the input field.
+      handlePaste: function(e) {
+        console.log('Paste Intercepted on element #' + this.element.attr('id') );
+
+        var paste = e.originalEvent.clipboardData && e.originalEvent.clipboardData.getData ?
+          e.originalEvent.clipboardData.getData('text/plain') : // Standard
+          window.clipboardData && window.clipboardData.getData ?
+          window.clipboardData.getData('Text') : // MS
+          false;
+
+        console.log('Original Pasted Content: ' + paste);
+
+        if (paste) {
+          // cut down the total size of the paste input to only be as long as the pattern allows.
+          var maxPasteInput = paste.substring( 0, this.pattern.length );
+          console.log('working with subsection of pasted content: ' + maxPasteInput);
+          this.processStringAgainstMask(maxPasteInput, e);
+        }
+
+        this.killEvent(e);
+      },
+
+      // Attempts to match the character provided from a pattern against the array of
+      // pattern matching characters ("definitions"). If the character is not in the array,
+      // it is considered "literal", and will be placed into the input field as part of the mask.
       isCharacterLiteral: function( patternChar ) {
         return $.inArray( patternChar, Object.keys(settings.definitions) ) === -1;
       },
 
+      // The following methods are used for modifying the contents of strings based on caret position.
+      // TODO: Move these to a more global space for use in other plugins?
+      insertAtIndex: function(string, value, index) {
+          return string.substring(0, index) + value + string.substring(index);
+      },
+      replaceAtIndex: function(string, value, index) {
+        return string.substr(0, index) + value + string.substr(index+value.length);
+      },
+      deleteAtIndex: function(string, value, index) {
+        return string.substr(0, index) + string.substr(index + value.length);
+      },
+
+      // Resets properties used for internal storage between keypresses to their default values
       resetStorage: function() {
         delete this.originalPos;
         delete this.currentMaskBeginIndex;
         this.buffer = '';
-      },
-
-      insertAtIndex: function (string, value, index) {
-          return string.substring(0, index) + value + string.substring(index);
       },
 
       // Writes the current value of the internal text buffer out to the Input Field.
@@ -266,7 +353,7 @@
         var val = this.element.val(),
           pos = this.originalPos,
           buffSize = this.buffer.length,
-          pattSize = settings.pattern.length;
+          pattSize = this.pattern.length;
 
         // insert the buffer's contents
         val = this.insertAtIndex( val, this.buffer, pos.begin );
@@ -287,13 +374,16 @@
         this.caret( actualCaretPos );
       },
 
+      // Filter the character that was just typed into the mask to determine if it belongs.
+      // In some cases, extra characters that belong in the correctly-masked text will be added before
+      // The typed character is placed.  If the character doesn't belong, stop the event and reset.
       processMask: function( typedChar, patternChar, e ) {
         var self = this;
         self.originalPos = self.caret();
         self.currentMaskBeginIndex = self.currentMaskBeginIndex || self.originalPos.begin;
 
         // don't do anything if you're at the end of the pattern.  You can't type anymore.
-        if ( self.currentMaskBeginIndex >= settings.pattern.length ) {
+        if ( self.currentMaskBeginIndex >= self.pattern.length ) {
           self.resetStorage();
           return self.killEvent(e);
         }
@@ -331,39 +421,20 @@
         }
       },
 
+      // Takes an entire string of characters and runs it against the processMask() method until it's complete
+      processStringAgainstMask: function( string, originalEvent ) {
+        var charArray = string.split('');
+        for(var i = 0; i < charArray.length; i++) {
+          var patternChar = this.getCharacter();
+          this.processMask( charArray[i], patternChar, originalEvent );
+        }
+      },
+
+      // Takes a character from the pattern string in Settings, gets the corresponding Regex string
+      // from the definitions array in Settings, and tests the character against the Regex.
       testCharAgainstRegex: function(typedChar, patternChar) {
         var regex = settings.definitions[patternChar];
         return regex.test(typedChar);
-      },
-
-      moveCursor: function( direction ) {
-        var self = this;
-        var pos = self.caret();
-
-        var _direction = self.evaluateDirecton(direction);
-
-        switch(_direction) {
-          case 'next':
-            self.caret( pos.begin + 1 );
-            break;
-          case 'prev':
-            self.caret( pos.begin - 1 );
-            break;
-          default:
-            break;
-        }
-      },
-
-      // Get the direction in which to position the cursor (if necessary)
-      // defaults to 'current', which will not move the cursor.
-      evaluateDirecton: function( direction ) {
-        var _direction = 'current';
-        var directions = ['current', 'next', 'prev'];
-        if ( direction ) {
-          var i = $.inArray( direction, directions );
-          _direction = directions[i];
-        }
-        return _direction;
       },
 
       // Returns the character at the current/next/previous cursor position.
@@ -371,7 +442,7 @@
       // If an optional index is provided, the cursor position will shift to that index value.
       getCharacter: function( direction, maskIndex ) {
         var _direction = this.evaluateDirecton(direction);
-        var mask = settings.pattern;
+        var mask = this.pattern;
         var index = maskIndex ? maskIndex : this.caret().begin;
 
         switch(_direction) {
@@ -382,24 +453,6 @@
           default:
             return mask.substring( index, index + 1 );
         }
-      },
-
-      pasteEvent: function(e) {
-        console.log('Paste Intercepted on element #' + this.element.attr('id') );
-      },
-
-      keyDownEvent: function(e) {
-
-      },
-
-      keyUpEvent: function(e) {
-
-      },
-
-      focusEvent: function(e) {
-        var el = e.currentTarget.tagName + '#' + e.currentTarget.id;
-        this.initValue = this.element.val();
-        console.log('Element ' + el + ' Focused');
       },
 
       destroy: function() {
