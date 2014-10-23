2c17104f4edf3e9a6d6e91ee3866013fd7643f5e
Tim McConechy <tim.mcconechy@infor.com>
2014-10-23 11:08:33 -0400

    fixed issue in multiselect

--- js/popupmenu.js
+++ js/popupmenu.js
@@ -245,7 +245,7 @@
           menuWidth = this.menu.outerWidth(),
           menuHeight = this.menu.outerHeight();
 
-        if (settings.trigger === 'rightClick' || (e !== null && settings.trigger === 'immediate')) {
+        if (settings.trigger === 'rightClick' || (e !== null && e !== undefined && settings.trigger === 'immediate')) {
           wrapper.css({'left': (e.type === 'keypress' ? target.offset().left : e.pageX),
                         'top': (e.type === 'keypress' ? target.offset().top : e.pageY)});
         } else {
