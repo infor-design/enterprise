7ce981f8650f015424576c4e940e3608b1f852dd
Tim McConechy <tim.mcconechy@infor.com>
2014-12-05 14:56:18 -0500

    Getting ready to build, fixed merge conflict

--- js/button.js
+++ js/button.js
@@ -28,31 +28,100 @@
     // Plugin Methods
     Plugin.prototype = {
       init: function() {
-        // Add Ripple Effect
-        this.element.addClass('ripple');
-        this.element.on('mousedown.button', function (e) {
-          if (e.button === 2) {
-            return;
+        var self = this;
+
+        this.isTouch = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+        this.element.on('touchstart.button mousedown.button', function (e) {
+          if (self.isTouch && e.type === 'mousedown') {
+            return false;
           }
 
-          var btn = $(this),
-              ripple = $('<div></div>').addClass('ripple-effect'),
-              btnOffset = btn.offset(),
-              xPos = e.pageX - btnOffset.left,
-              yPos = e.pageY - btnOffset.top;
+          var element = $(this);
 
+          // If the ripple wrapper does not exists, create it
+          if (!element.find('.ripple-wrapper').length) {
+            element.append('<div class=ripple-wrapper></div>');
+          }
+
+          var wrapper = $(this).find('.ripple-wrapper'),
+            wrapperOffset = wrapper.offset(),
+            relX,
+            relY;
+
+          if (!self.isTouch) {
+            // Get the mouse position relative to the ripple wrapper
+            relX = e.pageX - wrapperOffset.left;
+            relY = e.pageY - wrapperOffset.top;
+          } else {
+            // Make sure the user is using only one finger and then get the touch position relative to the ripple wrapper
+            e = e.originalEvent;
+
+            if (e.touches.length === 1) {
+              relX = e.touches[0].pageX - wrapperOffset.left;
+              relY = e.touches[0].pageY - wrapperOffset.top;
+            } else {
+              return;
+            }
+          }
+
+          // Make the new ripple
+          var ripple = $('<div></div>').addClass('ripple')
+            .css({'left': relX, 'top': relY});
+
+          wrapper.append(ripple);
+
+          // Make sure the ripple has the styles applied (ugly hack but it works)
+          (function() { return window.getComputedStyle(ripple[0]).opacity; })();
+
+          // Set the new size
+          var size = (Math.max($(this).outerWidth(), $(this).outerHeight()) / ripple.outerWidth()) * 2.5;
+
+          // Decide if use CSS transitions or jQuery transitions
+          // Start the transition
           ripple.css({
-              top: yPos - (ripple.height()/2),
-              left: xPos - (ripple.width()/2)
-            }).appendTo(btn);
+            '-ms-transform': 'scale(' + size + ')',
+            '-moz-transform': 'scale(' + size + ')',
+            '-webkit-transform': 'scale(' + size + ')',
+            'transform': 'scale(' + size + ')'
+          });
+          ripple.addClass('ripple-on');
+          ripple.data('animating', 'on');
+          ripple.data('mousedown', 'on');
 
-        }).on('mouseup.button, mouseleave.button', function () {
-          var btn = $(this).find('.ripple-effect');
-          setTimeout(function () {
-            btn.remove();
-          }, 400);
+          // This function is called when the transition 'on' ends
+          setTimeout(function() {
+            ripple.data('animating', 'off');
+            if (ripple.data('mousedown') === 'off') {
+              self.rippleOut(ripple);
+            }
+          }, 500);
+
+          // On mouseup or on mouseleave, set the mousedown flag to 'off' and try to destroy the ripple
+          element.on('mouseup mouseleave', function() {
+            ripple.data('mousedown', 'off');
+            // If the transition 'on' is finished then we can destroy the ripple with transition 'out'
+            if (ripple.data('animating') === 'off') {
+              self.rippleOut(ripple);
+            }
+          });
+        });
+      },
+
+      // Fade out the ripple and then destroy it
+      rippleOut: function(ripple) {
+        // Unbind events from ripple
+        ripple.off();
+
+        // Start the out animation
+        ripple.addClass('ripple-out');
+
+        // This function is called when the transition "out" ends
+        ripple.on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function(){
+          ripple.remove();
         });
       }
+
     };
 
     // Initialize the plugin (Once)
