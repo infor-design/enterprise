d73d840928222e4b96c665a9f26dfd1360f8806a
Deepak Verma <deepak.verma@infor.com>
2015-06-03 13:07:43 -0400

    - Updated homepage masonry algorithm

--- js/homepage.js
+++ js/homepage.js
@@ -25,6 +25,7 @@
         defaults = {
           gutterSize: 20,
           widgetWidth: 360,
+          widgetHeight: 370,
           animate: true,
           timeout: 400,
           columns: 3,
@@ -52,31 +53,118 @@
 
         //Initial Sizing
         this.resize(this, false);
-       },
+      },
 
-      setBlockSizes: function (phone, tablet) {
-        var self = this;
+      initColumns: function() {        
+        this.rowsAndCols = [];// Keeping all blocks as rows and columns
+        this.rowsAndCols[0] = [];
 
-        this.blocks = [];
-        this.element.find('.card, .widget').each(function () {
-          var card = $(this);
+        for (var i = 0, l = this.settings.columns; i < l; i++) {
+          this.rowsAndCols[0][i] = true;// Make all columns available in first row[true]
+        }
+      },
 
-          if (phone && (card.hasClass('triple-width') || card.hasClass('double-width'))) {
-            card.addClass('to-single');
-          } else if (tablet && (card.hasClass('triple-width'))) {
-            card.addClass('to-double').removeClass('to-single');
-          } else if (tablet && (card.hasClass('double-width') && card.prev().offset().top === card.offset().top)) {
-            card.addClass('to-single').removeClass('to-double');
-          } else {
-            card.removeClass('to-single to-double');
+      // Return [x and y] where we can fit this block
+      getAvailability: function(block) {
+        var i, j, l, cols,
+          self = this, 
+          rows = self.rowsAndCols.length, 
+          smallest = {},
+          abort = false;
+
+        // Loop thru each row and column soon it found first available spot
+        // Then check for if block's width can fit in(yes), asign to [smallest] and break both loops
+        for (i = 0, l = rows; i < l && !abort; i++) {
+          for (j = 0, cols = self.rowsAndCols[i].length; j < cols && !abort; j++) {
+            if ((self.rowsAndCols[i][j]) && ((block.w + j) <= cols)) {
+              smallest.row = i;
+              smallest.col = j;
+              abort = true;
+            }
           }
+        }
 
-          self.blocks.push({w: card.width(), h: card.height(), elem: card, text: card.text()});
+        // If did not found any available spot from previous loops
+        // Add new row and asign to [smallest] first column in this new row
+        if (!Object.getOwnPropertyNames(smallest).length) {
+          self.rowsAndCols[rows] = []; 
+          for (i = 0, l = self.settings.columns; i < l; i++) {
+            self.rowsAndCols[rows][i] = true;
+          }
+          smallest.row = rows;
+          smallest.col = 0;
+        }
+
+        return smallest; //{x:0, y:0}
+      },
+
+      // Make all spots as unavailable, depends on block's width and height
+      // Soon we used this block
+      fitBlock: function(r, c, block) {
+        var i, j, l, l2, 
+          self = this, 
+          addRow = true;
+
+        block.x = c;
+        block.y = r;
+
+        if ((block.w === 1) && (block.h === 1)) { // Single block can fit anywhere
+          self.rowsAndCols[r][c] = false;
+        } else {
+          // If more then one row or column then loop thru to block's width and height
+          // If height is more then current rows then add new row
+          // Mark those spots as unavailable[false]
+          if(block.w !== 1) {
+            // Left to right
+            for (i = r, l = block.h + r; i < l; i++) {
+              for (j = c, l2 = block.w + c; j < l2; j++) {
+                self.rowsAndCols[i] = self.rowsAndCols[i] || [];
+                self.rowsAndCols[i][j] = false;
+              }
+            }
+          } else {
+            // Top to bottom
+            for (i = r, l = block.h + r; i < l; i++) {
+              for (j = c, l2 = block.h + c; j < l2; j++) {
+                self.rowsAndCols[i] = self.rowsAndCols[i] || [];
+                self.rowsAndCols[i][c] = false;
+              }
+            }
+          }
+        }
+
+        // Check if reach to end of columns then assign flag[addRow]
+        for (i = 0, l = self.rowsAndCols[r].length; i < l; i++) {
+          if(self.rowsAndCols[r][i]) {
+            addRow = false;                 
+          }
+        }
+
+        // If reach to end of columns and next row is not avaiable then add new row
+        // Make all columns available, if not assigned earlier as unavailable
+        if (addRow) {
+          var newRow = r +1;
+          self.rowsAndCols[newRow] = self.rowsAndCols[newRow] || [];
+          for (i = 0, l = self.settings.columns; i < l; i++) {
+            if (self.rowsAndCols[newRow][i] === undefined) {
+              self.rowsAndCols[newRow][i] = true;
+            }
+          }              
+        }
+      },
+
+      // Setup each block sizes, based on classes provided from markup
+      setBlocks: function() {
+        var self = this;
+        self.blocks = [];
+
+        self.element.find('.card, .widget').each(function () {
+          var card = $(this),
+            h = card.hasClass('double-height') ? 2 : 1,
+            w = card.hasClass('triple-width') ? 3 : card.hasClass('double-width') ? 2 : 1;
+
+          self.blocks.push({w: w, h: h, elem: card, text: card.text()});
         });
-
-        var maxAttr = this.element.attr('data-columns');
-        this.settings.columns = parseInt((maxAttr ? maxAttr : this.settings.columns));
-
       },
 
       attachEvents: function () {
@@ -97,157 +185,90 @@
 
       },
 
-      initBreakpoint: function (w, h) {
-        this.root = { x: 0, y: 0, w: w, h: h };
-      },
-
-      refresh: function(animate) {
-        this.resize(this, (animate ? animate : this.settings.animate));
-      },
-
       // Resize Method
       resize: function(self, animate) {
         //Sizes of "breakpoints" is  320, 660, 1000 , 1340 (for 320)
         //or 360, 740, 1120, 1500 or (for 360)
-        var bpXL = (self.settings.widgetWidth * 4) + (self.settings.gutterSize * 3),
-          bpDesktop = (self.settings.widgetWidth * 3) + (self.settings.gutterSize *2),
-          bpTablet = (self.settings.widgetWidth * 2) + self.settings.gutterSize,
-          bpPhone = self.settings.widgetWidth;
+        var bpXL    = (self.settings.widgetWidth * 4) + (self.settings.gutterSize * 3),
+          bpDesktop = (self.settings.widgetWidth * 3) + (self.settings.gutterSize * 2),
+          bpTablet  = (self.settings.widgetWidth * 2) + self.settings.gutterSize,
+          bpPhone   = self.settings.widgetWidth;
 
         var bp = bpXL, //1340,
-          elemWidth = self.element.outerWidth();  //Math min against window.screen.width for single line mobile support
+          elemWidth = self.element.outerWidth(); //Math min against window.screen.width for single line mobile support
 
-        //$('.card').first().text(self.element.outerWidth() + '' window.screen.width));
+        elemWidth -= 30; //extra break space
 
-         // Find the Breakpoints
-        var xl = (elemWidth >= bpXL),
+        // Find the Breakpoints
+        var xl    = (elemWidth >= bpXL),
           desktop = (elemWidth >= bpDesktop && elemWidth <= bpXL),
-          tablet = (elemWidth >= bpTablet && elemWidth <= bpDesktop),
-          phone = (elemWidth <= bpTablet);
+          tablet  = (elemWidth >= bpTablet && elemWidth <= bpDesktop),
+          phone   = (elemWidth <= bpTablet);
 
-        self.setBlockSizes(phone, tablet, desktop);
+        var maxAttr = this.element.attr('data-columns');
+        this.settings.columns = parseInt((maxAttr ? maxAttr : this.settings.columns));
 
+        // Assign columns as breakpoint sizes
         if (xl && self.settings.columns === 4) {
+          self.settings.columns = 4;
           bp = bpXL;
         }
-        if (desktop) {
-          bp = bpDesktop;
-        }
-        if (xl && self.settings.columns === 3) {
+        if ((desktop) || (xl && self.settings.columns === 3)) {
+          self.settings.columns = 3;
           bp = bpDesktop;
         }
         if (tablet) {
+          self.settings.columns = 2;
           bp = bpTablet;
         }
         if (phone) {
+          self.settings.columns = 1;
           bp = bpPhone;
         }
 
-        self.initBreakpoint(bp, 999999);
         self.element.find('.content').css('margin-left', '-' + (bp/2) + 'px');
 
-        self.fit(self.blocks, phone, tablet);
+        this.setBlocks(); //setup blocks
+        this.initColumns(); //setup colums
 
-        for (var n = 0 ; n < self.blocks.length ; n++) {
-          var block = self.blocks[n], pos;
+        // Loop thru each block, make fit where available and
+        // If block more wider than available size, make as  available size
+        // Assign new left and top css positions
+        for (var i = 0, l = self.blocks.length; i < l; i++) {
+          var left, top, pos, available,
+            block = self.blocks[i];
 
-          if (block.fit) {
-            pos = {left: block.fit.x, top: block.fit.y};
+          // Remove extra classes if assigned earlier
+          block.elem.removeClass('to-double').removeClass('to-single');
 
-            if (animate) {
-              block.elem.animate(pos, self.settings.timeout, self.settings.easing);
+          // If block more wider than available size, make as  available size
+          if (block.w > self.settings.columns) {
+            block.w = self.settings.columns;
+            if (self.settings.columns === 1) {
+              block.elem.addClass('to-single');
+            } else if (self.settings.columns === 2) {
+              block.elem.addClass('to-double').removeClass('to-single');
             }
-            else {
-              block.elem.css(pos);
-            }
-            //block.elem.attr('tabindex', n);
-          } else {
-           //Error Shouldnt Happen... but when it does it Doesnt Fit
-           console.log('Bad');
           }
+
+          // Get Availability
+          available = self.getAvailability(block);
+
+          // Set positions
+          left = (self.settings.widgetWidth + self.settings.gutterSize) * available.col;
+          top = (self.settings.widgetHeight + self.settings.gutterSize) * available.row;
+          pos = {left: left, top: top};
+
+          if (animate) {
+            block.elem.animate(pos, self.settings.timeout, self.settings.easing);
+          }
+          else {
+            block.elem.css(pos);
+          }
+
+          // Mark all spots as unavailable for this block, as we just used this one
+          self.fitBlock(available.row, available.col, block);
         }
-
-      },
-
-      //This is a very simple binary tree based bin packing algorithm that is initialized
-      //with a fixed width and height and will fit each block into the first node where
-      //it fits and then split that node into 2 parts (down and right) to track the
-      //remaining whitespace.
-      fit: function(blocks) {//, phone, tablet) {
-        var n, node, block;
-
-        for (n = 0; n < this.blocks.length; n++) {
-          block = blocks[n];
-          var elem = $(block.elem);
-
-          node = this.findNode(this.root, block.w, block.h, elem);
-
-          if (node) {
-            block.fit = this.splitNode(node, block.w, block.h);
-          }
-
-          //Exception Rearranges
-          /* if (watchNext) {
-            block.fit = {x: 0, y: 386};
-            watchNext = false;
-          }
-
-          //homepage-3up
-          if (!phone && !tablet && elem.hasClass('double-height double-width') && self.blocks[n-1] && self.blocks[n-1].fit.x === 0 && self.blocks[n-1].fit.y ===0){
-            block.fit = {x: 376, y: 0};
-            watchNext = true;
-          }
-
-          //homepage-5up
-          if (!phone && !tablet && elem.hasClass('double-height') && !elem.hasClass('double-width') && self.blocks[n-1] && self.blocks[n-1].fit.x === 0 && self.blocks[n-1].fit.y ===0){
-            block.fit = {x: 376, y: 0};
-            watch1 = n+1;
-            watch2 = n+2;
-            watch3 = n+3;
-          }
-
-          if (n === watch1) {
-            block.fit = {x: 756, y: 0};
-          }
-          if (n === watch2) {
-            block.fit = {x: 0, y: 386};
-          }
-          if (n === watch3) {
-            block.fit = {x: 756, y: 386};
-          }
-
-          //homepage-wide
-          if (!phone && dropNext) {
-            block.fit = {x: 0, y: 388};
-            dropNext = false;
-          }
-
-          if (!phone && !tablet && !elem.hasClass('double-width') && elem.hasClass('double-height')  &&
-            self.blocks[n-1] && self.blocks[n-1].fit.x === 0 && self.blocks[n-1].fit.y ===0 &&
-            self.blocks[n-1].elem.hasClass('double-width')) {
-
-            block.fit = {x: 756, y: 0};
-            dropNext = true;
-          } */
-
-        }
-      },
-
-      findNode: function(root, w, h, elem) {
-        if (root.used) {
-          return this.findNode(root.right, w, h, elem) || this.findNode(root.down, w, h, elem);
-        } else if ((w <= root.w) && (h <= root.h)) {
-          return root;
-        } else {
-          return null;
-        }
-      },
-
-      splitNode: function(node, w, h) {
-        node.used = true;
-        node.down  = { x: node.x,     y: node.y + h + settings.gutterSize, w: node.w,     h: node.h - h };
-        node.right = { x: node.x + w + settings.gutterSize, y: node.y,     w: node.w - w, h: h          };
-        return node;
       },
 
       detachEvents: function () {
