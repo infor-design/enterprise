800a1ba46364fb40280458e326fefdab3176ace7
Tim McConechy <tim.mcconechy@infor.com>
2015-07-08 14:30:34 -0400

    - Recoded Bar Progress - Added Field Set Styling - Fixed a color label

--- app.js
+++ app.js
@@ -50,6 +50,8 @@ app.configure(function() {
 
   app.get('/controls/*', function(req, res) {
     var end = req.url.replace('/controls/','');
+    controlOptions.subtitle = end.charAt(0).toUpperCase() + end.slice(1);
+
     res.render('controls/' + end, controlOptions);
   });
 
