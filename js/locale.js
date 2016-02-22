357a55bb084d8a4256304b73b02de9afbf312fd8
Tim McConechy <tim.mcconechy@infor.com>
2016-02-22 10:21:19 -0500

    - Fixed issue with time formatting - HFC-3415

--- js/locale.js
+++ js/locale.js
@@ -185,6 +185,13 @@
       ret = ret.replace('y', year);
 
       //Time
+      var showDayPeriods = ret.indexOf(' a') > -1;
+      //PM
+      ret = ret.replace(' a', ' '+ (hours >= 12 ? cal.dayPeriods[1] : cal.dayPeriods[0]));
+
+      if (showDayPeriods && hours === 0) {
+        hours = 12;
+      }
       ret = ret.replace('hh', (hours > 12 ? hours - 12 : hours));
       ret = ret.replace('h', (hours > 12 ? hours - 12 : hours));
       ret = ret.replace('HH', hours);
@@ -198,9 +205,6 @@
         ret = ret.replace('MM', this.pad(month+1, 2));  //number padded
         ret = ret.replace('M', month+1);                //number unpadded
       }
-
-      //PM
-      ret = ret.replace(' a', ' '+ (hours >= 12 ? cal.dayPeriods[1] : cal.dayPeriods[0]));
 
       //Day of Week
       ret = ret.replace('EEEE', cal.days.wide[value.getDay()]);  //Day of Week
