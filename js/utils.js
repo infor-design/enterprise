45be335c071d45c233e7fc498e4e59276f5a0897
Deepak Verma <deepak.verma@infor.com>
2016-03-24 18:52:49 -0400

    - Added utils function for copy to clipboard

--- js/utils.js
+++ js/utils.js
@@ -344,6 +344,36 @@
     return { event: this, cancel: cancel, pause: pause, resume: resume };
   };
 
+  // Copies a string to the clipboard. Must be called from within an event handler such as click.
+  // May return false if it failed, but this is not always
+  // possible. Browser support for Chrome 43+, Firefox 42+, Edge and IE 10+.
+  // No Safari support, as of (Nov. 2015). Returns false.
+  // IE: The clipboard feature may be disabled by an adminstrator. By default a prompt is
+  // shown the first time the clipboard is used (per session).
+  $.copyToClipboard = function(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+      // IE specific code path to prevent textarea being shown while dialog is visible.
+      return window.clipboardData.setData('Text', text);
+    }
+    else if (document.queryCommandSupported && document.queryCommandSupported('copy')) {
+      var textarea = document.createElement('textarea');
+      textarea.textContent = text;
+      textarea.style.position = 'fixed'; // Prevent scrolling to bottom of page in MS Edge.
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+        return document.execCommand('copy'); // Security exception may be thrown by some browsers.
+      }
+      catch (ex) {
+        // console.warn('Copy to clipboard failed.', ex);
+        return false;
+      }
+      finally {
+        document.body.removeChild(textarea);
+      }
+    }
+  };
+
   //Functions For Sanitising and Escaping Html
   $.escapeHTML = function(value) {
     var newValue = value;
