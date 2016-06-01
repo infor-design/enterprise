43dd7103a00fcc17787ac7fe89c5a81a19f5a527
tmcconechy <tim.mcconechy@infor.com>
2016-06-01 09:57:50 -0400

    - Added code for "secondary border" - aka "ghost" button - HFC-3885

--- js/initialize.js
+++ js/initialize.js
@@ -195,7 +195,7 @@
           ['multiselect', 'select[multiple]:not(.dropdown), .multiselect:not([data-init])'],
 
           //Button with Effects
-          ['button', '.btn, .btn-secondary, .btn-primary, .btn-modal-primary, .btn-tertiary, .btn-icon, .btn-actions, .btn-menu, .btn-split'],
+          ['button', '.btn, .btn-secondary, .btn-primary, .btn-modal-primary, .btn-tertiary, .btn-icon, .btn-actions, .btn-menu, .btn-split, .btn-secondary-border'],
 
           //Hide Focus
           ['hideFocus', 'a.hide-focus, a.tick, a.hyperlink'],
