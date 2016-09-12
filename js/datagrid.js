70babe5128e13b511379ee45cf927c1e9e4cb387
tmcconechy <tim.mcconechy@infor.com>
2016-09-12 09:44:54 -0400

    - Fix linting for SOHO-4591

--- js/datagrid.js
+++ js/datagrid.js
@@ -35,8 +35,9 @@ window.Formatters = {
       var value2 = Locale.parseDate(value, (typeof col.dateFormat === 'string' ? {pattern: col.dateFormat}: col.dateFormat));
       if (value2) {
         formatted = Locale.formatDate(value2, (typeof col.dateFormat === 'string' ? {pattern: col.dateFormat}: col.dateFormat));
-      } else
-		  formatted = Locale.formatDate(value, (typeof col.dateFormat === 'string' ? {pattern: col.dateFormat}: col.dateFormat));
+      } else {
+		    formatted = Locale.formatDate(value, (typeof col.dateFormat === 'string' ? {pattern: col.dateFormat}: col.dateFormat));
+      }
     } else {
       formatted = Locale.formatDate(value, (typeof col.dateFormat === 'string' ? {pattern: col.dateFormat}: col.dateFormat));
     }
