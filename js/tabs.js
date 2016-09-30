abc6a1881848fda3c4a48e9ea9ce77df437aaf8b
Ed Coyle <Ed.Coyle@infor.com>
2016-09-30 10:20:46 -0400

    Other:
    - Made Dropdown Tab menus close when a removable sub-item is removed.

--- js/tabs.js
+++ js/tabs.js
@@ -1415,7 +1415,15 @@
         }
         if (isLastDropdownTabItem(parentMenu)) {
           prevLi = this.getPreviousTab(trigger);
-          this.remove(trigger);
+
+          setTimeout(function() {
+            self.remove(trigger);
+          }, 1);
+        }
+
+        // Close dropdown tab's menu
+        if (trigger && trigger.length) {
+          trigger.data('popupmenu').close();
         }
 
         // Adjust tablist height
