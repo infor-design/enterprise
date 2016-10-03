e16835e48028e7298f634928505631808e6336a9
Chris Elliott <ctelliott@gmail.com>
2016-10-03 15:13:20 -0400

    SOHO-4787 Added bump-version.js script

---
+++ bin/bump-version.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+	filename = 'package.json',
+	file = require('../' + filename),
+	version = process.env.bamboo_version || process.env.bamboo_jira_version;
+
+if (!version) {
+	console.log("Either bamboo_version or bamboo_jira_version ENV must be set.");
+	process.exit(1);
+} else {
+  file.version = version;
+}
+
+fs.writeFile(filename, JSON.stringify(file, null, 2), function (err) {
+	if (err) return console.log(err);
+	console.log('Updated version to:' + file.version);
+});
