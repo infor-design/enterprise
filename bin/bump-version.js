e5d466a945fa93f1414014a4f3fba1d3ee7cd969
Chris Elliott <ctelliott@gmail.com>
2016-10-03 16:59:15 -0400

    SOHO-4787 bump-version script now exits if the new vercion and current version match

--- bin/bump-version.js
+++ bin/bump-version.js
@@ -3,10 +3,13 @@
 var fs = require('fs'),
 	filename = 'package.json',
 	file = require('../' + filename),
-	version = process.env.bamboo_jira_version || process.env.bamboo_version;
+	version = process.env.bamboo_version || process.env.bamboo_jira_version;
 
 if (!version) {
-	console.log('[Error] Either bamboo_jira_version or bamboo_version ENV var must be set.');
+	console.log("[Error] Either bamboo_version or bamboo_jira_version ENV must be set.");
+	process.exit(1);
+} else if (version == file.version) {
+  console.log("[Error] New version cannot match current version.");
 	process.exit(1);
 } else {
   file.version = version;
@@ -14,5 +17,6 @@ if (!version) {
 
 fs.writeFile(filename, JSON.stringify(file, null, 2), function (err) {
 	if (err) return console.log(err);
-	console.log('[Info] Updated version to: ' + file.version);
+	console.log('[Info] Updated version to:' + file.version);
 });
+
