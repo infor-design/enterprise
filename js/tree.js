9473895f22e0b2b232d52e595a94388905bad821
tmcconechy <tim.mcconechy@infor.com>
2016-10-07 13:33:40 -0400

    - Changed Tree to allow for attach arbitrary data to the nodes - SOHO-4844

--- js/tree.js
+++ js/tree.js
@@ -856,14 +856,17 @@
 
       //Sync a node with its dataset 'record'
       syncNode: function (node) {
-        var entry = {},
+        var entry = node.data('jsonData'),
           self = this;
 
-        entry = {
-          node: node,
-          id: node.attr('id'),
-          text: node.find('.tree-text').text()
-        };
+        entry = node.data('jsonData');
+        if (entry === undefined) {
+          entry = {};
+        }
+
+        entry.node = node;
+        entry.id = node.attr('id');
+        entry.text = node.find('.tree-text').text();
 
         if (node.hasClass('is-open')) {
           entry.open = true;
