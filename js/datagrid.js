b68a55d2aa2f39a6c68aa97b785792106f05b5c0
tmcconechy <tim.mcconechy@infor.com>
2016-11-29 16:25:28 -0500

    - Pulled out SOHO-4927

--- js/datagrid.js
+++ js/datagrid.js
@@ -4718,11 +4718,7 @@ $.fn.datagrid = function(options) {
       }
 
       //Find the cell if it exists
-      if (row instanceof jQuery && ((row.is('td') && !isGroupRow) || row.is('tr'))) {
-        self.activeCell.node = row;
-      } else {
-        self.activeCell.node = self.cellNode((isGroupRow ? rowElem: rowNum), (isGroupRow ? 0 : cell)).attr('tabindex', '0');
-      }
+      self.activeCell.node = self.cellNode((isGroupRow ? rowElem: rowNum), (isGroupRow ? 0 : cell)).attr('tabindex', '0');
 
       if (self.activeCell.node && prevCell.node.length === 1) {
         self.activeCell.row = rowNum;
