6ac4608b3d0629330d8c3d5794d292e19c19feab
Christopher Lepore <chris.lepore@infor.com>
2016-12-16 15:12:36 -0500

    Fix typo

--- js/scrollaction.js
+++ js/scrollaction.js
@@ -3,7 +3,7 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module
     define(['jquery'], factory);
-  } else if (typeof exports === 'Object') {
+  } else if (typeof exports === 'object') {
     // Node/CommonJS
     module.exports = factory(require('jquery'));
   } else {
