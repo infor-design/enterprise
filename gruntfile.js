7e9555ff21e62508c4c133fc9d4dd7f712c7cd40
Dave Hall <daveghall@gmail.com>
2017-01-20 15:52:21 -0500

    SOHO-5558, Added js-uglify task for min dist

--- gruntfile.js
+++ gruntfile.js
@@ -110,6 +110,13 @@ module.exports = function(grunt) {
     'concat:basic',
   ]);
 
+  grunt.registerTask('js-uglify', [
+    'copy:amd',
+    'strip_code',
+    'concat:basic',
+    'uglify',
+  ]);
+
   // Don't do any uglify/minify/jshint while the Dev Watch is running.
   grunt.registerTask('sohoxi-watch', [
     'revision', 'sass', 'copy:amd', 'strip_code','concat', 'clean:amd', 'copy:main', 'usebanner'
