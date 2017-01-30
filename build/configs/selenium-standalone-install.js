7d8f958da96054a2ed931a98aaf423d41e33f329
Ed Coyle <Ed.Coyle@infor.com>
2017-01-30 09:51:38 -0500

    - Updated version of Selenium Standalone used for installing test suite dependencies - Externalized configuration for Seleniun Standalone installation. - Set a hard version on Selenium Standalone to fix versioning problems with IE Webdriver.

---
+++ build/configs/selenium-standalone-install.js
@@ -0,0 +1,18 @@
+var ARCH = process.arch;
+
+module.exports = {
+  version: '2.53.0',
+  baseURL: 'http://selenium-release.storage.googleapis.com',
+  drivers: {
+    chrome: {
+      version: '2.21',
+      arch: ARCH,
+      baseURL: 'https://chromedriver.storage.googleapis.com'
+    },
+    ie: {
+      version: '3.0',
+      arch: ARCH,
+      baseURL: 'https://selenium-release.storage.googleapis.com'
+    }
+  }
+};
