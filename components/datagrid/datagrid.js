7df86ea09955d2291b7017b7e4549f708c8e7d04
Tim McConechy <tim.mcconechy@infor.com>
2017-07-26 10:50:11 -0400

    - Added fix for SOHO-6563

--- components/datagrid/datagrid.js
+++ components/datagrid/datagrid.js
@@ -3442,10 +3442,11 @@ $.fn.datagrid = function(options) {
         var diff = this.elemWidth - this.totalWidth;
 
         if ((diff > 0) && diff  > colWidth && !this.widthPercent && !this.headerRow) {
-          colWidth = diff - 2;
+          colWidth = diff - (this.isIe ? 19: 2);
+
           this.headerWidths[index] = {id: col.id, width: colWidth, widthPercent: this.widthPercent};
           col.width = colWidth;
-          this.totalWidth =  this.elemWidth -2;
+          this.totalWidth =  this.elemWidth - (this.isIe ? 19: 2);
         }
 
         if (this.widthPercent) {
