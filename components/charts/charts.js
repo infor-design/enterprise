bca68df5bd10a1825857206cf5bf22bccc71ca00
Deepak Verma <deepak.verma@infor.com>
2017-07-31 12:55:28 -0400

    2017-07-31 SOHO-6443 - Added support to get selected and normalize selected data for column, column-grouped, column-stacked, and column-singular Charts http://localhost:4000/components/column/example-get-selected http://localhost:4000/components/column-grouped/example-get-selected http://localhost:4000/components/column-stacked/example-get-selected http://localhost:4000/components/column-stacked/example-singular-get-selected

--- components/charts/charts.js
+++ components/charts/charts.js
@@ -3670,7 +3670,8 @@ window.Chart = function(container) {
       thisGroup = d3.select(o.selector.parentNode),
       thisGroupId = parseInt((thisGroup.node() ? thisGroup.attr('data-group-id') : 0), 10),
       triggerData = [],
-      selectedBars = [];
+      selectedBars = [],
+      thisData;
 
     if (isStacked || isTypePie) {
       dataset = dataset || null;
@@ -3714,12 +3715,11 @@ window.Chart = function(container) {
               .selectAll('.bar').classed('is-selected', true).style('opacity', 1);
           }
 
-          var thisData;
           svg.selectAll('.bar.is-selected').each(function(d, i) {
             var bar = d3.select(this);
 
             thisData = s.dataset;
-            thisData = thisData ? (isStacked ? thisData[o.i].data[i] : thisData[i].data[o.i]) : d;
+            thisData = thisData ? (isStacked ? isSingular ? (thisData[0].data[o.i]) : (thisData[o.i].data[i]) : thisData[i].data[o.i]) : d;
             selectedBars.push({elem: bar[0], data: thisData});
           });
           triggerData = selectedBars;
@@ -3728,7 +3728,9 @@ window.Chart = function(container) {
 
       // Single and stacked only -NOT grouped-
       else if (isSingular && isStacked && isTypeColumn) {
+        thisData = dataset[0] && dataset[0].data ? dataset[0].data : o.d;
         selector.classed('is-selected', true).style('opacity', 1);
+        triggerData.push({elem: selector[0], data: thisData[o.i]});
       }
 
       // Single or groups only -NOT stacked-
@@ -3741,8 +3743,8 @@ window.Chart = function(container) {
         d3.select(svg.selectAll('.bartext')[0][o.i]).style('font-weight', 'bolder');
         d3.select(svg.selectAll('.target-bartext')[0][o.i]).style('font-weight', 'bolder');
 
-        if (isGrouped || isPositiveNegative) {
-          if (!isPositiveNegative) {
+        if (isGrouped || isPositiveNegative || isTypeColumn) {
+          if (!isPositiveNegative && !isTypeColumn || (isTypeColumn && isGrouped)) {
             thisGroup.classed('is-selected', true)
               .selectAll('.bar').classed('is-selected', true).style('opacity', 1);
           }
