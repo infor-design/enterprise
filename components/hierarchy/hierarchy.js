ab93e2800c2914e791a19cdde6ab80a15bf24ea9
krishollenbeck <hollenbeck.kris@gmail.com>
2017-08-16 10:17:36 -0500

    - Added an example of lazy loading data after the initial dataset has rendered. - Refactored / cleaned up some of the state checks. - Got rid of the constants object with all the preset strings. This was probably more confusing than helpful. Changed setButtonState() to updateState(). A centralized function for handling the state of the leafs vs setting flags along the way all over the place.

--- components/hierarchy/hierarchy.js
+++ components/hierarchy/hierarchy.js
@@ -39,26 +39,6 @@
       'azure08', 'turquoise02', 'amethyst06', 'slate06', 'amber06', 'emerald07', 'ruby06'
     ];
 
-    var constants = {
-      container       : 'container',
-      chart           : 'content',
-      toplevel        : 'top-level',
-      sublevel        : 'sub-level',
-      noSublevel      : 'no-sublevel',
-      sublist         : 'sublist',
-      leaf            : 'leaf',
-      multiRoot       : 'multi-root',
-      root            : 'root',
-      branchExpanded  : 'branch-expanded',
-      branchCollapsed : 'branch-collapsed',
-      expanded        : 'expand',
-      collapsed       : 'collapse',
-      close           : 'close',
-      open            : 'open',
-      line            : 'line',
-      selected        : 'selected'
-    };
-
     /**
     * The displays custimizable hierarchical data such as an org chart.
     *
@@ -96,9 +76,6 @@
         if (settings.dataset) {
           if (settings.dataset[0].children.length > 0) {
             var data = settings.dataset[0] === undefined ? settings.dataset : settings.dataset[0];
-
-            data.isRootNode = true;
-            data.isExpanded = true;
             this.render(data);
           } else {
             $(this.element).append('<p style=\'padding:10px;\'>No data available</p>');
@@ -126,25 +103,25 @@
             settings.newData = [];
           }
 
-          var nodeId = $(this).closest('.' + constants.leaf).attr('id');
-          var dataObject = self.data(nodeId, settings.dataset, settings.newData);
+          var nodeId = $(this).closest('.leaf').attr('id');
+          var nodeData = $('#' + nodeId).data();
           var domObject = {
             branch: $(this).closest('li'),
-            leaf: $(this).closest('.' + constants.leaf),
+            leaf: $(this).closest('.leaf'),
             button: $(this)
           };
 
-          if (dataObject.isExpanded) {
-            self.collapse(event, dataObject, domObject);
+          if (nodeData.isExpanded) {
+            self.collapse(event, nodeData, domObject);
           } else {
-            self.expand(event, dataObject, domObject);
+            self.expand(event, nodeData, domObject);
           }
 
         });
 
-        self.element.on('keypress', '.' + constants.leaf, function(event) {
+        self.element.on('keypress', '.leaf', function(event) {
           var nodeId     = $(this).attr('id');
-          var nodeData   = self.data(nodeId, settings.dataset, settings.newData);
+          var nodeData   = $('#' + nodeId);
 
           if (event.which === 13) {
             if (nodeData.isExpanded) {
@@ -156,13 +133,13 @@
         });
 
         // Select
-        self.element.on('mousedown', '.' + constants.leaf, function(event) {
+        self.element.on('mousedown', '.leaf', function(event) {
           var nodeData = $(this).data();
           var targetInfo = {target: event.target, pageX: event.pageX, pageY: event.pageY};
           var eventType = 'selected';
 
-          $('.is-' + constants.selected).removeClass('is-'+constants.selected);
-          $('#' + nodeData.id).addClass('is-'+constants.selected);
+          $('.is-selected').removeClass('is-selected');
+          $('#' + nodeData.id).addClass('is-selected');
 
           // Is collapse event
           if ( $(event.target).is('button') && $(event.target).find('use').prop('href').baseVal === '#icon-caret-up') {
@@ -252,23 +229,24 @@
       },
 
       /**
-      * Add data as children for the given nodeId.
+      *  Add data as children for the given nodeId.
       */
       add: function (nodeId, currentDataObject, newDataObject) {
+
         var self            = this;
         var id              = currentDataObject.id !== undefined ? currentDataObject.id : nodeId;
         var node            = $('#' + id);
         var parentContainer = node.parent().hasClass('leaf-container') ? node.parent().parent() : node.parent();
         var selectorObject  = {};
-        var isSubLevelChild = parentContainer.parent().attr('class') !== constants.sublevel;
-        var subListExists   = parentContainer.children('.' + constants.sublist).length === 1;
+        var isSubLevelChild = parentContainer.parent().attr('class') !== 'sub-level';
+        var subListExists   = parentContainer.children('.sublist').length === 1;
 
         if (isSubLevelChild) {
           if (subListExists) {
-            selectorObject.element = parentContainer.children('.' + constants.sublist);
+            selectorObject.element = parentContainer.children('.sublist');
           } else {
-            selectorObject.el = parentContainer.append('<ul class=\'' + constants.sublist + '\'></ul>');
-            selectorObject.element = $(selectorObject.el).find('.' + constants.sublist);
+            selectorObject.el = parentContainer.append('<ul class=\'sublist\'></ul>');
+            selectorObject.element = $(selectorObject.el).find('.sublist');
           }
         } else {
           selectorObject.el = parentContainer.children('ul');
@@ -281,6 +259,8 @@
           }
           self.createLeaf(newDataObject, selectorObject.element);
         }
+
+        self.updateState(node, false, null, 'add');
       },
 
       /**
@@ -289,22 +269,18 @@
       expand: function(event, nodeData, domObject) {
         var self = this,
           node = domObject.leaf,
-          nodeTopLevel  = node.next().not('.' + constants.line);
+          nodeTopLevel  = node.next().not('.line');
 
         nodeTopLevel.animateOpen();
-        self.element.trigger(constants.expanded, [nodeData, settings.dataset]);
+        self.element.trigger('expanded', [nodeData, settings.dataset]);
 
         if (node.hasClass('root')) {
           nodeTopLevel  = nodeTopLevel.next('ul');
           nodeTopLevel.animateOpen();
         }
 
-        nodeData.isExpanded = true;
-
-        self.setNodeData(nodeData);
-
-        node.parent().removeClass(constants.branchCollapsed).addClass(constants.branchExpanded);
-        self.setButtonState(node, nodeData);
+        node.parent().removeClass('branch-collapsed').addClass('branch-expanded');
+        self.updateState(node, false, null, 'expand');
       },
 
       /**
@@ -313,10 +289,10 @@
       collapse: function(event, nodeData, domObject) {
         var self = this,
           node = domObject.leaf,
-          nodeTopLevel  = node.next().not('.' + constants.line);
+          nodeTopLevel  = node.next().not('.line');
 
         nodeTopLevel.animateClosed().on('animateclosedcomplete', function () {
-          self.element.trigger(constants.collapsed, [nodeData, settings.dataset]);
+          self.element.trigger('collapsed', [nodeData, settings.dataset]);
         });
 
         if (node.hasClass('root')) {
@@ -324,11 +300,8 @@
           nodeTopLevel.animateClosed();
         }
 
-        nodeData.isExpanded = false;
-
-        self.setNodeData(nodeData);
-        node.parent().removeClass(constants.branchExpanded).addClass(constants.branchCollapsed);
-        self.setButtonState(node, nodeData);
+        node.parent().removeClass('branch-expanded').addClass('branch-collapsed');
+        self.updateState(node, false, null, 'collapse');
       },
 
       //Main render method
@@ -340,13 +313,13 @@
         var rootNodeHTML = [];
         var structure    = {
           legend    : '<legend><ul></ul></legend>',
-          chart     : '<ul class=\'' + constants.container + '\'>'+ '<li class=\'' + constants.chart + '\'></li></ul>',
-          toplevel  : '<ul class=\'' + constants.toplevel + '\'></ul>',
-          sublevel  : '<ul class=\'' + constants.sublevel + '\'></ul>'
+          chart     : '<ul class=\'container\'><li class=\'chart\'></li></ul>',
+          toplevel  : '<ul class=\'top-level\'></ul>',
+          sublevel  : '<ul class=\'sub-level\'></ul>'
         };
 
         var chartContainer  = this.element.append(structure.chart);
-        var chart = $('.' + constants.chart, chartContainer);
+        var chart = $('.chart', chartContainer);
 
         if (legend.length !== 0) {
           this.element.prepend(structure.legend);
@@ -358,12 +331,10 @@
         this.setColor(data);
 
         if (data.isMultiRoot) {
-          var multiRootHTML = '<div class=\'' + constants.leaf + ' ' + constants.multiRoot + '\'><div class=\'' +
-            constants.inner + '\'><h2>' +
-            data.multiRootText +'</h2></div></div>';
+          var multiRootHTML = '<div class=\'leaf multiRoot\'><div><h2>' + data.multiRootText +'</h2></div></div>';
 
           rootNodeHTML.push(multiRootHTML);
-          $(rootNodeHTML[0]).addClass(constants.root).appendTo(chart);
+          $(rootNodeHTML[0]).addClass('root').appendTo(chart);
 
         } else {
 
@@ -371,13 +342,12 @@
           var leaf = leafTemplate.render({dataset: data});
           rootNodeHTML.push(leaf);
 
-          $(rootNodeHTML[0]).addClass(constants.root).appendTo(chart);
-          this.setNodeData(data);
-          this.setButtonState($('.leaf.root'), data);
+          $(rootNodeHTML[0]).addClass('root').appendTo(chart);
+          this.updateState($('.leaf.root'), true);
         }
 
         if (!hasTopLevel) {
-          $('<div class=\'' + constants.line + '\'></div>').insertAfter('.' + constants.root);
+          $('<div class=\'line\'></div>').insertAfter('.root');
         }
 
         function renderSubChildren(self, subArray, data) {
@@ -429,10 +399,10 @@
         }
 
         if (!hasSubLevel) {
-          $('.' + constants.topLevel).addClass(constants.noSublevel);
+          $('.top-level').addClass('no-sublevel');
         }
 
-        var containerWidth = this.element.find('.' + constants.container).outerWidth();
+        var containerWidth = this.element.find('.container').outerWidth();
         var windowWidth = $(window).width();
         var center = (containerWidth - windowWidth) / 2;
         this.element.scrollLeft(center);
@@ -480,24 +450,39 @@
         }
       },
 
-      // Creates a leaf node under element for nodeData
+      /**
+       * Private function
+       *  Creates a leaf node under element for nodeData
+       * @param nodeData
+       * @param container
+       */
       createLeaf: function(nodeData, container) {
         var self           = this;
-        //console.log(nodeData.id);
         var chartClassName = self.element.attr('class');
-        var chart          = $('.' + chartClassName + ' .' + constants.chart, self.container);
+        var chart          = $('.' + chartClassName + ' .chart', self.container);
         var elClassName    = container.attr('class');
         var el             = elClassName !== undefined ? $('.' + elClassName) : container;
 
         if (el.length < 1) {
-          if (elClassName === constants.toplevel) {
-            container.insertAfter('.' + constants.root);
+          if (elClassName === 'top-level') {
+            container.insertAfter('.root');
           } else {
             container.appendTo(chart);
           }
         }
 
-        function processDataForleaf(nodeData, isLast) {
+        if (nodeData.length) {
+          for (var i = 0, l = nodeData.length; i < l; i++) {
+            var isLast = i === nodeData.length -1;
+            processDataForLeaf(nodeData[i], isLast);
+            self.updateState($('#' + nodeData[i].id), false, nodeData[i]);
+          }
+        } else {
+          processDataForLeaf(nodeData, true);
+          self.updateState($('#' + nodeData.id), false, nodeData);
+        }
+
+        function processDataForLeaf(nodeData, isLast) {
           self.setColor(nodeData);
 
           var leafTemplate = Tmpl.compile('{{#dataset}}' + $('#' + settings.templateId).html() + '{{/dataset}}');
@@ -513,49 +498,32 @@
             lineHtml += '<span class=\'ln\'></span>';
           }
 
-          var lf = $(leaf);
-          self.setButtonState(lf, nodeData);
-
-          if (elClassName !== constants.sublevel && elClassName !== constants.toplevel) {
-            $(lf).append('<span class=\'horizontal-line\'></span>');
+          if (elClassName !== 'sub-level' && elClassName !== 'top-level') {
+            $(leaf).append('<span class=\'horizontal-line\'></span>');
           }
 
-          var branchState = nodeData.isExpanded || nodeData.isExpanded === undefined ? constants.branchExpanded : constants.branchCollapsed;
+          var branchState = nodeData.isExpanded || nodeData.isExpanded === undefined ? 'branch-expanded' : 'branch-collapsed';
           if (nodeData.isLeaf) {
             branchState = '';
           }
 
-          parent.append('<li class=' + branchState + '>' + lineHtml + $(lf)[0].outerHTML + '</li>');
-
-          self.setNodeData(nodeData);
+          parent.append('<li class=' + branchState + '>' + lineHtml + $(leaf)[0].outerHTML + '</li>');
 
           if (nodeData.children) {
             var childrenNodes = '';
-            nodeData.isLoaded = true;
-
-            if (nodeData.isExpanded) {
-              nodeData.isExpanded = true;
-              $('#' + nodeData.id).data(nodeData);
-            }
-            else {
-              nodeData.isExpanded = false;
-            }
 
             for (var j = 0, l = nodeData.children.length; j < l; j++) {
               self.setColor(nodeData.children[j]);
 
-              var childleaf = leafTemplate.render({dataset: nodeData.children[j]});
-              var c = $(childleaf);
+              var childLeaf = leafTemplate.render({dataset: nodeData.children[j]});
 
-              self.setButtonState(c, nodeData.children[j]);
-
-              $(c).append('<span class=\'horizontal-line\'></span>');
+              $(childLeaf).append('<span class=\'horizontal-line\'></span>');
 
               if (j === nodeData.children.length - 1) {
-                childrenNodes += '<li><span class=\'ln last-line\'></span>' + $(c)[0].outerHTML + '</li>';
+                childrenNodes += '<li><span class=\'ln last-line\'></span>' + $(childLeaf)[0].outerHTML + '</li>';
               }
               else {
-                childrenNodes += '<li><span class=\'ln\'></span>' + $(c)[0].outerHTML + '</li>';
+                childrenNodes += '<li><span class=\'ln\'></span>' + $(childLeaf)[0].outerHTML + '</li>';
               }
             }
 
@@ -564,23 +532,17 @@
 
             var childLength = nodeData.children.length;
             while (childLength--) {
-              self.setNodeData(nodeData.children[childLength]);
+              self.updateState($('#' + nodeData.children[childLength].id), false, nodeData.children[childLength]);
             }
           }
         }
-
-        if (nodeData.length) {
-          for (var i = 0, l = nodeData.length; i < l; i++) {
-            var isLast = i === nodeData.length -1;
-            processDataForleaf(nodeData[i], isLast);
-          }
-        } else {
-          processDataForleaf(nodeData, true);
-        }
-
       },
 
-      // Determine the color from settings
+      /**
+       * Private function
+       * Determine the color from settings
+       * @param data
+       */
       setColor: function(data) {
         for (var i = 0, l = settings.legend.length; i < l; i++) {
           if (data[settings.legendKey] === settings.legend[i].value) {
@@ -603,13 +565,9 @@
         }
       },
 
-      setNodeData: function(nodeData) {
-        var leafObject   = $('#' + nodeData.id).data(nodeData);
-        leafObject.data  = nodeData;
-      },
-
       /**
       * Return whether or not a particular node is a leaf
+      * private function
       */
       isLeaf: function(dataNode) {
 
@@ -629,23 +587,64 @@
         return true;
       },
 
-      //set the classes and svg on the button
-      setButtonState: function (leaf, data) {
-        var btn = leaf.find('.btn');
+      /**
+       * Handle all leaf state here,
+       * get the current state via .data() and re-attach the new state
+       * private function
+       * @param leaf
+       * @param isRoot
+       */
+      updateState: function (leaf, isRoot, nodeData, eventType) {
 
+        // set data if it has not been set already
+        if ($.isEmptyObject($(leaf).data()) && nodeData) {
+          $(leaf).data(nodeData);
+        }
+
+        var btn = $(leaf).find('.btn');
+        var data = $(leaf).data();
+
+        // data has been loaded if it has children
+        if (data.children || eventType === 'add') {
+          data.isLoaded = true;
+        }
+
+        if (isRoot) {
+          data.isRootNode = true;
+          data.isLoaded = true;
+        }
+
+        if ((data.isExpanded === undefined && data.children) || eventType === 'expand') {
         if (data.isExpanded || data.isExpanded === undefined && !data.isLeaf) {
           btn.find('svg.icon').changeIcon('caret-up');
           btn.addClass('btn-expand').removeClass('btn-collapse');
           data.isExpanded = true;
+        }
+
+        // defaults to collapsed state
+        if (data.isExpanded === undefined || eventType === 'collapse') {
+          data.isExpanded = false;
+        }
+
+        if (data.isExpanded) {
+          btn.find('svg.icon').changeIcon('caret-up');
+          btn.addClass('btn-expand').removeClass('btn-collapse');
         } else {
           btn.find('svg.icon').changeIcon('caret-down');
           btn.addClass('btn-collapse').removeClass('btn-expand');
-          data.isExpanded = false;
         }
 
         if (data.isLeaf || data.isRootNode) {
           btn.addClass('btn-hidden');
         }
+
+        if (data.isLeaf) {
+          data.isLoaded = false;
+          data.isExpanded = false;
+        }
+
+        // Reset data
+        $(leaf).data(data);
       }
 
     };
