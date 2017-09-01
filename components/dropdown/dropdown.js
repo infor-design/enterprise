26483a1717eb48e6b5e1307081a1f61bb27c3fbf
Ed Coyle <Ed.Coyle@infor.com>
2017-09-01 10:56:31 -0400

    - Slightly modified Dropdown positions

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1335,10 +1335,9 @@
         positionOpts.useParentWidth = useParentWidth;
 
         // use negative height of the pseudoElem to get the Dropdown list to overlap the input.
-        var isShort = this.element.closest('.field-short').length > 0;
-        positionOpts.y = isShort ? -26 : Soho.env.browser.name === 'firefox' ? -32 : -34;
+        var isNotChrome = Soho.env.browser.name !== 'chrome';
+        positionOpts.y = -(parseInt(parentElement[0].clientHeight) + parseInt(parentElementStyle.borderTopWidth) + parseInt(parentElementStyle.borderBottomWidth) - (isNotChrome ? 1 : 0));
         positionOpts.x = 0;
-        //parseInt(parentElementStyle.height) +  parseInt(parentElementStyle.borderTopWidth) +  parseInt(parentElementStyle.borderBottomWidth) * -1;
 
         this.list.one('afterplace.dropdown', dropdownAfterPlaceCallback).place(positionOpts);
         this.list.data('place').place(positionOpts);
