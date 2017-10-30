f7209cdf4114341c01fa3f77fd8bc8eb5d211e8c
Ed Coyle <Ed.Coyle@infor.com>
2017-10-30 13:05:35 -0400

    SOHO-7005
    - Added RenderLoop `unregister()` API
    - Cleaned up checks on RenderLoop `register()` method's arguments

--- components/utils/renderloop.js
+++ components/utils/renderloop.js
@@ -30,6 +30,7 @@
    */
   function RenderLoop() {
     this.callbacks = [];
+    this.element = $('body');
     return this;
   }
 
@@ -37,7 +38,7 @@
   RenderLoop.prototype = {
 
     /**
-     *
+     * Start the entire render loop
      */
     start: function() {
       this.doLoop = true;
@@ -58,8 +59,27 @@
 
         // TODO: execute callbacks
         self.callbacks.forEach(function(obj) {
-          var modifiedArgs;
+          // Remove if we've set the `doRemoveOnNextTick` flag.
+          if (obj.doRemoveOnNextTick) {
+            self._remove(obj);
+            self.element.triggerHandler('remove.renderLoop', [obj]);
+            return;
+          }
 
+          // Check duration
+          if (typeof obj.duration === 'number' && obj.duration > -1) {
+            if (!obj.start) {
+              obj.start = now;
+            }
+
+            if (obj.duration <= now - obj.start) {
+              obj.doRemoveOnNextTick = true;
+              return;
+            }
+          }
+
+          // Call the function
+          var modifiedArgs;
           if (typeof obj.cb === 'function') {
             modifiedArgs = Array.prototype.slice.call(arguments);
             modifiedArgs.push({
@@ -70,10 +90,13 @@
 
             obj.cb.apply(null, modifiedArgs);
             return;
+          } else {
+            // If it's not a function, prune the whole object
+            // TODO: remove from queue
+            this._remove(obj);
+            self.element.triggerHandler('remove.renderLoop', [obj]);
           }
 
-          // If it's not a function, prune the whole object
-          // TODO: remove from queue
         });
 
         // Continue the loop
@@ -84,12 +107,15 @@
       tick();
     },
 
-
+    /**
+     * Stop the entire render loop
+     */
     stop: function() {
       this.doLoop = false;
     },
 
     /**
+     * External method for getting the callback queue contents
      * @returns {Array}
      */
     queue: function() {
@@ -100,18 +126,31 @@
      * @param {function} callback
      * @param {Number} [duration] = 0
      * @param {String} [namespace]
+     * @returns {boolean}
      */
     register: function(callback, duration, namespace) {
       if (typeof callback !== 'function') {
         throw new Error('cannot register callback to RenderLoop because callback is not a function');
       }
 
+      var noNamespace = typeof namespace !== 'string' || !namespace.length;
+
       // valid for a callback not to have a duration, as long as it's namespaced for future manual removal
-      if (!duration) {
+      if (typeof duration === 'string') {
+        if (noNamespace) {
+          namespace = duration;
+          duration = -1;
+          noNamespace = false;
+        } else {
+          var numberDuration = parseInt(duration);
+          if (!isNaN(numberDuration)) {
+            duration = numberDuration;
+          }
+        }
+      } else if (typeof duration !== 'number') {
         duration = -1;
       }
 
-      var noNamespace = typeof namespace !== 'string' || !namespace.length;
       if (duration < 1 && noNamespace) {
         throw new Error('cannot register a callback to RenderLoop with no duration and no namespace');
       }
@@ -126,6 +165,8 @@
         duration: duration,
         start: timestamp()
       });
+
+      return true;
     },
 
     /**
@@ -133,8 +174,45 @@
      * @param {String} [namespace]
      */
     unregister: function(callback, namespace) {
-      // TODO: remove by callback
-      // TODO: remove by namespace
+      if (typeof callback !== 'function' && typeof callback !== 'string' && typeof namespace !== 'string') {
+        throw new Error('must provide either a callback function or a namespace string to remove an entry from the RenderLoop queue.');
+      }
+
+      // If callback is defined as a string, simply swap it for the namespace.
+      if (typeof callback === 'string') {
+        namespace = callback;
+        callback = undefined;
+      }
+
+      return this._remove({
+        cb: callback,
+        id: namespace
+      });
+    },
+
+    /**
+     * @private
+     * Actually does the removal of a registered callback from the queue
+     * Pulled out into its own function because it can be automatically called by the tick, or manually triggered from an external API call.
+     * @param {Object} obj
+     * @returns {boolean}
+     */
+    _remove: function(obj) {
+      var queueStartSize = this.callbacks.length;
+
+      if (typeof obj.callback === 'function') {
+        // Remove by callback method
+        this.callbacks = this.callbacks.filter(function(item) {
+          return ''+item.cb !== ''+obj.cb;
+        });
+      } else if (typeof obj.id === 'string') {
+        // Remove by namespace
+        this.callbacks = this.callbacks.filter(function(item) {
+          return item.id !== obj.id;
+        });
+      }
+
+      return queueStartSize !== this.callbacks.length;
     }
 
   };
