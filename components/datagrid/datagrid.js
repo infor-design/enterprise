9cb172170382a640ede71234485c2956ad877ef5
James Allen Tadique <jamesallen.tadique@infor.com>
2017-11-09 11:21:32 +0800

    Fix SOHO-6011 Results Display (n of m)

--- components/datagrid/datagrid.js
+++ components/datagrid/datagrid.js
@@ -1561,7 +1561,7 @@ $.fn.datagrid = function(options) {
         disableClientFilter: false, //Disable Filter Logic client side and let your server do it
         disableClientSort: false, //Disable Sort Logic client side and let your server do it
         resultsText: null,  // Can provide a custom function to adjust results text
-        fullViewResult : false, // Paging results display (false for m, true for n of m)
+        showResultTotal : false, // Paging results display (true for n of m, false for m)
         virtualized: false, // Prevent Unused rows from being added to the DOM
         virtualRowBuffer: 10, //how many extra rows top and bottom to allow as a buffer
         rowReorder: false, //Allows you to reorder rows. Requires rowReorder formatter
@@ -1616,7 +1616,7 @@ $.fn.datagrid = function(options) {
   * @param {Boolean} disableClientFilter &nbsp;-&nbsp Disable Filter Logic client side and let your server do it
   * @param {Boolean} disableClientSort &nbsp;-&nbsp Disable Sort Logic client side and let your server do it
   * @param {String} resultsText &nbsp;-&nbsp Can provide a custom function to adjust results text on the toolbar
-  * @param {Boolean} fullViewResult &nbsp;-&nbsp Paging results display (false for m, true for n of m)
+  * @param {Boolean} showResultTotal &nbsp;-&nbsp Paging results display (true for n of m, false for m)
   * @param {Boolean} rowReorder &nbsp;-&nbsp If set you can reorder rows. Requires rowReorder formatter
   * @param {Boolean} showDirty &nbsp;-&nbsp  If true the dirty indicator will be shown on the rows
   * @param {Boolean} allowOneExpandedRow  &nbsp;-&nbsp Controls if you cna expand more than one expandable row.
@@ -4917,7 +4917,7 @@ $.fn.datagrid = function(options) {
       }
 
       var countText;
-      if (self.settings.fullViewResult) {
+      if (self.settings.showResultTotal) {
         countText = '(' + Locale.formatNumber(count, {style: 'integer'}) + ' of ' + Locale.formatNumber(totals, {style: 'integer'}) + ' ' + Locale.translate(totals === 1 ? 'Result' : 'Results') + ')';        
       } else {
         countText = '(' + Locale.formatNumber(count, {style: 'integer'}) + ' ' + Locale.translate(totals === 1 ? 'Result' : 'Results') + ')';        
