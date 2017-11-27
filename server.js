72d71edd3c8c79ecdc45a1d22c4ddc2ef41d2faf
Ed Coyle <Ed.Coyle@infor.com>
2017-11-27 10:37:55 -0500

    - Changed colors of Soho thing just for fun

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 /* jshint node:true */
 
-var app = require('./app'), // With the express server and routes defined, we can start to listen for requests.
-  port = process.env.PORT || 4000,
-  colors = require('colors'); // jshint ignore:line
+var app = require('./app');
+var chalk = require('chalk');
 
+// With the express server and routes defined, we can start to listen for requests.
+var port = process.env.PORT || 4000;
 app.listen(port);
 
 //Show Awesome Image Of Soho
-console.log('         .        .            |       .        .        . '.green);
-console.log('              *        .       |   .        .              '.green);
-console.log('        .                     /-\\     .           .   .    '.green);
-console.log(' .               .    .      |"""|              :        . '.green);
-console.log('         .                  /"""""\\  .      *       |>.    '.green);
-console.log('                           | # # # |     .        /\\|  ___ '.green);
-console.log('    __     ___   ___   .   |# # # #| ___      ___/<>\\ |:::|'.green);
-console.log('  / ""|  __|~~| |""|       |# # # #||"""|  __|"""|^^| |:::|'.green);
-console.log(' /""""| |::|""|~~~~||_____ |# # # #||"""|-|::|"""|""|_|   |'.green);
-console.log(' |""""| |::|""|""""|:::::| |# # # #||"""|t|::|"""|""|"""""|'.green);
-console.log(' |""""|_|  |""|""""|:::::| |# # # #||"""|||::|"""|""""""""|'.green);
-console.log(' |""""|::::|""|""""|:::::| |# # # #||"""|||::|"""|""""""""|'.green);
+console.log(chalk.gray('         .        .            ') + chalk.blue('|') + chalk.gray('       .        .        . '));
+console.log(chalk.gray('              *        .       ') + chalk.blue('|') + chalk.gray('   .        .              '));
+console.log(chalk.gray('        .                     ') + chalk.blue('/-\\') + chalk.gray('     .           .   .    '));
+console.log(chalk.gray(' .               .    .      ') + chalk.blue('|"""|') + chalk.gray('                  ') + chalk.blue(':') + chalk.gray('       . '));
+console.log(chalk.gray('         .                  ') + chalk.blue('/"""""\\') + chalk.gray('  .      *       ') + chalk.blue('|>.') + chalk.gray('    '));
+console.log(chalk.gray('                           ') + chalk.blue('|') + chalk.yellow(' # # # ') + chalk.blue('|') + chalk.gray('     .        ') + chalk.blue('/\\|  ___ '));
+console.log(chalk.blue('    __     ___   ___') + chalk.gray('   .   ') + chalk.blue('|') + chalk.yellow('# # # #') + chalk.blue('| ___      ___/<>\\ |:::|'));
+console.log(chalk.blue('  / ""|  __|~~| |""|       |') + chalk.yellow('# # # #') + chalk.blue('||"""|  __|"""|^^| |:::|'));
+console.log(chalk.blue(' /""""| |::|""|~~~~||_____ |') + chalk.yellow('# # # #') + chalk.blue('||"""|-|::|"""|""|_|   |'));
+console.log(chalk.blue(' |""""| |::|""|""""|:::::| |') + chalk.yellow('# # # #') + chalk.blue('||"""|t|::|"""|""|"""""|'));
+console.log(chalk.blue(' |""""|_|  |""|""""|:::::| |') + chalk.yellow('# # # #') + chalk.blue('||"""|||::|"""|""""""""|'));
+console.log(chalk.blue(' |""""|::::|""|""""|:::::| |') + chalk.yellow('# # # #') + chalk.blue('||"""|||::|"""|""""""""|'));
 
 console.log('Soho server is running at http://localhost:%s/. Press Ctrl+C to stop.', port);
