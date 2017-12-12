b7969104f126c751ed0cd5ac711ebaf2b85b2ebb
tjamesallen15 <tjamesallen15@yahoo.com>
2017-12-12 14:17:26 +0800

    SOHO-6974 Fix Padding

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -2531,6 +2531,7 @@
           }
 
           sizeableTabs[i].style.width = visibleTabSize + 'px';
+          sizeableTabs[i].style.paddingRight = '50px';
           a[0].style.width = visibleTabSize + 'px';
         }
 
