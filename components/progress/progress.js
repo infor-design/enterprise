9f9ced68e7a8a9b2415f9498f0aab9d91ba11df0
Deepak Verma <deepak.verma@infor.com>
2018-01-11 11:36:04 -0500

    SOHO-6976 - Converted FileUploadAdvanced and Progress control to ES6

--- components/progress/progress.js
+++ components/progress/progress.js
@@ -1,102 +1,105 @@
-/* start-amd-strip-block */
-(function(factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module
-    define(['jquery'], factory);
-  } else if (typeof exports === 'object') {
-    // Node/CommonJS
-    module.exports = factory(require('jquery'));
-  } else {
-    // Browser globals
-    factory(jQuery);
-  }
-}(function($) {
-/* end-amd-strip-block */
+/* jshint esversion:6 */
+import * as debug from '../utils/debug';
+import { utils } from '../utils/utils';
 
-  $.fn.progress = function(options) {
+// jQuery components
+import '../tooltip/tooltip.jquery';
 
-    // Settings and Options
-    var pluginName = 'progress',
-        defaults = {
-        },
-        settings = $.extend({}, defaults, options);
+/**
+ * Component Name
+ */
+const COMPONENT_NAME = 'progress';
 
-    /**
-    * The Progress Component displays loading information.
-    *
-    * @class Progress
-    * @param None
-    */
-    function Progress(element) {
-      this.element = $(element);
-      this.settings = settings;
-      Soho.logTimeStart(pluginName);
-      this.init();
-      Soho.logTimeEnd(pluginName);
+/**
+ * Default Progress Options
+ */
+const PROGRESS_DEFAULTS = {
+};
+
+/**
+* A list of items with add/remove/delete and sort functionality.
+*
+* @class Progress
+* @param {String} element The component element.
+* @param {String} settings The component settings.
+*/
+function Progress(element, settings) {
+  this.settings = utils.mergeSettings(element, settings, PROGRESS_DEFAULTS);
+
+  this.element = $(element);
+  debug.logTimeStart(COMPONENT_NAME);
+  this.init();
+  debug.logTimeEnd(COMPONENT_NAME);
+}
+
+// Actual Progress Code
+Progress.prototype = {
+
+  init() {
+    this.update();
+
+    this.element.off('updated.progress').on('updated.progress', (e) => {
+      e.stopPropagation();
+      this.update();
+    });
+  },
+
+  updateAria(value) {
+    this.element.attr({ role: 'progressbar', 'aria-valuenow': value, 'aria-maxvalue': '100' });
+
+    const container = this.element.parent();
+    if (container.data('tooltip')) {
+      container.data('tooltip').content = `${value}%`;
+    } else {
+      container.attr('title', `${value}%`).tooltip();
+    }
+  },
+
+  // Unbind all events
+  unbind() {
+    this.element.off('updated.progress');
+  },
+
+  /**
+  * Update the progress bar.
+  *
+  * @param {string} value  The percent value to use to fill. 0-100
+  * @returns {void}
+  */
+  update(value) {
+    let perc = this.element.attr('data-value');
+
+    if (value) {
+      perc = value;
+      this.element.attr('data-value', value);
     }
 
-    // Actual Progress Code
-    Progress.prototype = {
+    this.element[0].style.width = `${perc}%`;
+    this.updateAria(perc);
+  },
 
-      init: function() {
-        var self = this;
-        self.update();
+  /**
+   * Resync the UI and Settings.
+   * @param {Object} settings The settings to apply.
+   * @returns {Object} The api
+   */
+  updated(settings) {
+    if (typeof settings !== 'undefined') {
+      this.settings = utils.mergeSettings(this.element, settings, PROGRESS_DEFAULTS);
+    }
+    return this
+      .unbind()
+      .init();
+  },
 
-        this.element.off('updated.progress').on('updated.progress', function (e) {
-          e.stopPropagation();
-          self.update();
-        });
-      },
+  /**
+  * Teardown and remove any added markup and events.
+  * @returns {void}
+  */
+  destroy() {
+    this.unbind();
+    $.removeData(this.element[0], COMPONENT_NAME);
+  }
+};
 
-      updateAria: function (value) {
-        this.element.attr({'role': 'progressbar', 'aria-valuenow': value, 'aria-maxvalue':'100'});
-
-        var container = this.element.parent();
-        if (container.data('tooltip')) {
-          container.data('tooltip').content = value + '%';
-        } else {
-          container.attr('title', value + '%').tooltip();
-        }
-      },
-
-      /**
-      * Update the progress bar.
-      *
-      * @param {string} value  The percent value to use to fill. 0-100
-      */
-      update: function (value) {
-
-        var perc = this.element.attr('data-value');
-
-        if (value) {
-          perc = value;
-          this.element.attr('data-value', value);
-        }
-
-        this.element[0].style.width = perc + '%';
-        this.updateAria(perc);
-      },
-
-      /**
-      * Teardown and remove any added markup and events.
-      */
-      destroy: function() {
-        $.removeData(this.element[0], pluginName);
-        this.element.off('updated.progress');
-      }
-    };
-
-    // Initialize the plugin (Once)
-    return this.each(function() {
-      var instance = $.data(this, pluginName);
-      if (instance) {
-        instance.settings = $.extend({}, defaults, options);
-      } else {
-        instance = $.data(this, pluginName, new Progress(this, settings));
-      }
-    });
-  };
-
-/* start-amd-strip-block */
-}));
-/* end-amd-strip-block */
+export { Progress, COMPONENT_NAME };
