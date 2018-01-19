56a16aa2ce17ff6d7ee88f0f384f3e68c6a2ccbe
Deepak Verma <deepak.verma@infor.com>
2018-01-19 15:34:30 -0500

    SOHO-6976 - Fixed linter error in es6 Homepage control

--- components/homepage/homepage.js
+++ components/homepage/homepage.js
@@ -7,19 +7,21 @@ import { Locale } from '../locale/locale';
 const COMPONENT_NAME = 'homepage';
 
 const HOMEPAGE_DEFAULTS = {
-  gutterSize: 20, //Private
-  widgetWidth: 360, //Private
-  widgetHeight: 370, //Private
+  gutterSize: 20, // Private
+  widgetWidth: 360, // Private
+  widgetHeight: 370, // Private
   animate: true,
-  timeout: 100, //Private
+  timeout: 100, // Private
   columns: 3,
-  easing: 'blockslide' //Private
+  easing: 'blockslide' // Private
 };
 
 /**
 * The Homepage handles card layout at multiple breakpoints.
 *
 * @class Homepage
+* @param {String} element The component element.
+* @param {String} settings The component settings.
 * @param {boolean} animate  Disable animation during resize
 * @param {number} columns  Display in 3 (default) or 4 column layout
 *
@@ -41,10 +43,16 @@ Homepage.prototype = {
     this.initHeroWidget();
     this.handleEvents();
 
-    //Initial Sizing
+    // Initial Sizing
     this.resize(this, false);
   },
 
+  /**
+   * Initialize columns.
+   * @private
+   * @param {Number} row to be initialize.
+   * @returns {void}
+   */
   initColumns(row) {
     row = row || 0;
     this.rowsAndCols[row] = [];
@@ -54,6 +62,11 @@ Homepage.prototype = {
     }
   },
 
+  /**
+   * Initialize hero widget.
+   * @private
+   * @returns {void}
+   */
   initHeroWidget() {
     let heroWidget = $('.hero-widget');
     if (heroWidget.length > 1) {
@@ -62,33 +75,46 @@ Homepage.prototype = {
     this.heroWidget = heroWidget;
   },
 
+  /**
+   * Initialize rows and cols.
+   * @private
+   * @returns {void}
+   */
   initRowsAndCols() {
     this.rowsAndCols = [];// Keeping all blocks as rows and columns
     this.initColumns();
   },
 
-  // Return [x and y] where we can fit this block
+  /**
+   * Get availability where we can fit this given block.
+   * @private
+   * @param {Object} block to get availability.
+   * @returns {Object} [x and y] where we can fit this block
+   */
   getAvailability(block) {
-    let i, j, n, l, cols, innerCheck,
-      rows = this.rowsAndCols.length,
-      smallest = {},
-      abort = false;
+    let abort = false;
+    const smallest = {};
+    const rows = this.rowsAndCols.length;
 
     // Loop thru each row and column soon it found first available spot
     // Then check for if block's width can fit in(yes), asign to [smallest] and break both loops
-    for (i = 0, l = rows; i < l && !abort; i++) {
-      for (j = 0, innerCheck = true, cols = this.rowsAndCols[i].length; j < cols && !abort; j++) {
+    for (let i = 0, l = rows; i < l && !abort; i++) {
+      for (
+        let j = 0, innerCheck = true, cols = this.rowsAndCols[i].length;
+        j < cols && !abort;
+        j++
+      ) {
         if ((this.rowsAndCols[i][j]) && ((block.w + j) <= cols)) {
-          if ((block.w > 1) && (cols > (j+1))) {
-            for (n = 0; n < block.w; n++) {
+          if ((block.w > 1) && (cols > (j + 1))) {
+            for (let n = 0; n < block.w; n++) {
               if (!this.rowsAndCols[i][j + n]) {
                 innerCheck = false;
                 break;
               }
             }
           }
-          if ((block.h > 1) && (rows > (i+1))) {
-            for (n = 0; n < block.h; n++) {
+          if ((block.h > 1) && (rows > (i + 1))) {
+            for (let n = 0; n < block.h; n++) {
               if (!this.rowsAndCols[i + n][j]) {
                 innerCheck = false;
                 break;
@@ -112,50 +138,55 @@ Homepage.prototype = {
       smallest.col = 0;
     }
 
-    return smallest; //{x:0, y:0}
+    return smallest; // {x:0, y:0}
   },
 
-  // Make all spots as unavailable, depends on block's width and height
-  // Soon we used this block
+  /**
+   * Make all spots as unavailable, depends on block's width and height
+   * Soon we used this block
+   * @private
+   * @param {Number} r as row.
+   * @param {Number} c as col.
+   * @param {Number} block to fit.
+   * @returns {void}
+   */
   fitBlock(r, c, block) {
-    let i, j, l, l2,
-      addRow = true;
+    let addRow = true;
 
     block.x = c;
     block.y = r;
 
     if ((block.w === 1) && (block.h === 1)) { // Single block can fit anywhere
       this.rowsAndCols[r][c] = false;
-    } else {
+    } else if (block.w !== 1) {
       // If more then one row or column then loop thru to block's width and height
       // If height is more then current rows then add new row
       // Mark those spots as unavailable[false]
-      if (block.w !== 1) {
-        // Left to right
-        for (i = r, l = block.h + r; i < l; i++) {
-          for (j = c, l2 = block.w + c; j < l2; j++) {
-            if (!this.rowsAndCols[i]) {
-              this.initColumns(i);
-            }
-            this.rowsAndCols[i][j] = false;
+
+      // Left to right
+      for (let i = r, l = block.h + r; i < l; i++) {
+        for (let j = c, l2 = block.w + c; j < l2; j++) {
+          if (!this.rowsAndCols[i]) {
+            this.initColumns(i);
           }
+          this.rowsAndCols[i][j] = false;
         }
-      } else {
-        // Top to bottom
-        for (i = r, l = block.h + r; i < l; i++) {
-          for (j = c, l2 = block.h + c; j < l2; j++) {
-            if (!this.rowsAndCols[i]) {
-              this.initColumns(i);
-            }
-            this.rowsAndCols[i][c] = false;
+      }
+    } else {
+      // Top to bottom
+      for (let i = r, l = block.h + r; i < l; i++) {
+        for (let j = c, l2 = block.h + c; j < l2; j++) {
+          if (!this.rowsAndCols[i]) {
+            this.initColumns(i);
           }
+          this.rowsAndCols[i][c] = false;
         }
       }
     }
 
     // Check if reach to end of columns then assign flag[addRow]
-    for (i = 0, l = this.rowsAndCols[r].length; i < l; i++) {
-      if(this.rowsAndCols[r][i]) {
+    for (let i = 0, l = this.rowsAndCols[r].length; i < l; i++) {
+      if (this.rowsAndCols[r][i]) {
         addRow = false;
       }
     }
@@ -163,23 +194,37 @@ Homepage.prototype = {
     // If reach to end of columns and next row is not avaiable then add new row
     // Make all columns available, if not assigned earlier as unavailable
     if (addRow) {
-      if (!this.rowsAndCols[r +1]) {
-        this.initColumns(r +1);
+      if (!this.rowsAndCols[r + 1]) {
+        this.initColumns(r + 1);
       }
     }
   },
 
-  // Setup each block sizes, based on classes provided from markup
+  /**
+   * Setup each block sizes, based on classes provided from markup
+   * @private
+   * @returns {void}
+   */
   setBlocks() {
     const cards = this.element.find('.card, .widget');
     this.blocks = [];
 
     for (let i = 0, l = cards.length; i < l; i++) {
-      const card = $(cards[i]),
-        h = card.hasClass('double-height') ? 2 : 1,
-        w = card.hasClass('quad-width') ? 4 : card.hasClass('triple-width') ? 3 : card.hasClass('double-width') ? 2 : 1;
+      const card = $(cards[i]);
+      const h = card.hasClass('double-height') ? 2 : 1;
+      let w;
 
-      this.blocks.push({w: w, h: h, elem: card, text: card.text()});
+      if (card.hasClass('quad-width')) {
+        w = 4;
+      } else if (card.hasClass('triple-width')) {
+        w = 3;
+      } else if (card.hasClass('double-width')) {
+        w = 2;
+      } else {
+        w = 1;
+      }
+
+      this.blocks.push({ w, h, elem: card, text: card.text() });
     }
 
     // Max sized columns brings to top
@@ -190,41 +235,55 @@ Homepage.prototype = {
         }
         w += this.blocks[i].w;
         if (w >= this.settings.columns) {
-          w = 0; //reset
-          j = (this.blocks[j].w >= this.settings.columns) ? j+1 : i; //record to move
+          w = 0; // reset
+          j = (this.blocks[j].w >= this.settings.columns) ? j + 1 : i; // record to move
         }
       }
     }
   },
 
-  //Move an array element position
+  /**
+   * Move an array element position
+   * @private
+   * @param {Array} arr .
+   * @param {Number} from index.
+   * @param {Number} to index.
+   * @returns {void}
+   */
   arrayIndexMove(arr, from, to) {
     arr.splice(to, 0, arr.splice(from, 1)[0]);
   },
 
-  // Resize Method
+  /**
+   * Resize Method
+   * @private
+   * @param {Object} self .
+   * @param {Boolean} animate .
+   * @returns {void}
+   */
   resize(self, animate) {
-    //Sizes of "breakpoints" is  320, 660, 1000 , 1340 (for 320)
-    //or 360, 740, 1120, 1500 or (for 360)
-    const bpXL = (self.settings.widgetWidth * 4) + (self.settings.gutterSize * 3),
-      bpDesktop = (self.settings.widgetWidth * 3) + (self.settings.gutterSize * 2),
-      bpTablet = (self.settings.widgetWidth * 2) + self.settings.gutterSize,
-      bpPhone = self.settings.widgetWidth;
+    // Sizes of "breakpoints" is  320, 660, 1000 , 1340 (for 320)
+    // or 360, 740, 1120, 1500 or (for 360)
+    const bpXL = (self.settings.widgetWidth * 4) + (self.settings.gutterSize * 3);
+    const bpDesktop = (self.settings.widgetWidth * 3) + (self.settings.gutterSize * 2);
+    const bpTablet = (self.settings.widgetWidth * 2) + self.settings.gutterSize;
+    const bpPhone = self.settings.widgetWidth;
 
-    let bp = bpXL; //1340,
-    const elemWidth = self.element.outerWidth(); //Math min against window.screen.width for single line mobile support
+    let bp = bpXL; // 1340
+    // Math min against window.screen.width for single line mobile support
+    const elemWidth = self.element.outerWidth();
 
     // elemWidth -= 30; //extra break space
 
     // Find the Breakpoints
-    const xl = (elemWidth >= bpXL),
-      desktop = (elemWidth >= bpDesktop && elemWidth <= bpXL),
-      tablet = (elemWidth >= bpTablet && elemWidth <= bpDesktop),
-      phone = (elemWidth <= bpTablet);
+    const xl = (elemWidth >= bpXL);
+    const desktop = (elemWidth >= bpDesktop && elemWidth <= bpXL);
+    const tablet = (elemWidth >= bpTablet && elemWidth <= bpDesktop);
+    const phone = (elemWidth <= bpTablet);
 
-    const maxAttr = this.element.attr('data-columns'),
-      content = self.element.find('> .content');
-    this.settings.columns = parseInt((maxAttr || this.settings.columns));
+    const maxAttr = this.element.attr('data-columns');
+    const content = self.element.find('> .content');
+    this.settings.columns = parseInt((maxAttr || this.settings.columns), 10);
 
     // Assign columns as breakpoint sizes
     if (xl && self.settings.columns === 4) {
@@ -245,18 +304,18 @@ Homepage.prototype = {
     }
 
     if (content.length) {
-      content[0].style.marginLeft = '-' + (bp/2) + 'px';
+      content[0].style.marginLeft = `-${(bp / 2)}px`;
     }
 
-    this.setBlocks(); //setup blocks
-    this.initRowsAndCols(); //setup colums
+    this.setBlocks(); // setup blocks
+    this.initRowsAndCols(); // setup colums
 
     // Loop thru each block, make fit where available and
     // If block more wider than available size, make as  available size
     // Assign new left and top css positions
     for (let i = 0, l = self.blocks.length; i < l; i++) {
-      let left, top, pos, available,
-        block = self.blocks[i];
+      // let left, top, pos, available,
+      const block = self.blocks[i];
 
       // Remove extra classes if assigned earlier
       block.elem.removeClass('to-single to-double to-triple');
@@ -267,66 +326,81 @@ Homepage.prototype = {
 
         if (self.settings.columns === 1) {
           block.elem.addClass('to-single');
-        }
-        else if (self.settings.columns === 2) {
+        } else if (self.settings.columns === 2) {
           block.elem.addClass('to-double');
-        }
-        else if (self.settings.columns === 3) {
+        } else if (self.settings.columns === 3) {
           block.elem.addClass('to-triple');
         }
       }
 
       // Get Availability
-      available = self.getAvailability(block);
+      const available = self.getAvailability(block);
 
       // Set positions
-      const box = self.settings.widgetWidth + self.settings.gutterSize,
-        totalWidth = box * self.settings.columns;
+      const box = self.settings.widgetWidth + self.settings.gutterSize;
+      const totalWidth = box * self.settings.columns;
 
-      left = Locale.isRTL() ? totalWidth - ((box * block.w) + (box * available.col)) : box * available.col;
-      top = (self.settings.widgetHeight + self.settings.gutterSize) * available.row;
-      pos = {left: left, top: top};
+      const left = Locale.isRTL() ? totalWidth - ((box * block.w) + (box * available.col)) : box * available.col;// eslint-disable-line
+      const top = (self.settings.widgetHeight + self.settings.gutterSize) * available.row;
+      const pos = { left, top };
 
       if (animate) {
-        const easing = self.settings.easing,
-          blockslide = [0.09, 0.11, 0.24, 0.91];
+        const easing = self.settings.easing;
+        const blockslide = [0.09, 0.11, 0.24, 0.91];
 
         if (easing === 'blockslide') {
           if (self.isTransitionsSupports) {
             self.applyCubicBezier(block.elem, blockslide);
-            block.elem[0].style.left = pos.left + 'px';
-            block.elem[0].style.top = pos.top + 'px';
-          }
-          // IE-9
-          else {
+            block.elem[0].style.left = `${pos.left}px`;
+            block.elem[0].style.top = `${pos.top}px`;
+          } else {
+            // IE-9
             block.elem.animate(pos, self.settings.timeout);
           }
-        }
-
-        // Other easing effects ie (linear, swing)
-        else {
+        } else {
+          // Other easing effects ie (linear, swing)
           block.elem.animate(pos, self.settings.timeout, easing);
         }
-      }
-      else {
-        block.elem[0].style.left = pos.left + 'px';
-        block.elem[0].style.top = pos.top + 'px';
+      } else {
+        block.elem[0].style.left = `${pos.left}px`;
+        block.elem[0].style.top = `${pos.top}px`;
       }
 
       // Mark all spots as unavailable for this block, as we just used this one
       self.fitBlock(available.row, available.col, block);
     }
+
+    /**
+    * Fires after the page is resized and layout is set.
+    * Can be used for any special adjustments.
+    * @event resize
+    * @type {Object}
+    * @property {Object} event - The jquery event object
+    */
     self.element.triggerHandler('resize', self.settings.columns);
   },
 
+  /**
+   * Apply cubic-bezier effects
+   * @private
+   * @param {Object} el as element.
+   * @param {String} cubicBezier effect to apply.
+   * @returns {void}
+   */
   applyCubicBezier(el, cubicBezier) {
-    el[0].style['-webkit-transition'] = 'all .3s cubic-bezier('+ cubicBezier +')';
-    el[0].style['-moz-transition'] = 'all .3s cubic-bezier('+ cubicBezier +')';
-    el[0].style['-ms-transition'] = 'all .3s cubic-bezier('+ cubicBezier +')';
-    el[0].style['-o-transition'] = 'all .3s cubic-bezier('+ cubicBezier +')';
-    el[0].style.transition = 'all .3s cubic-bezier('+ cubicBezier +')';
+    const value = `all .3s cubic-bezier(${cubicBezier})`;
+    el[0].style['-webkit-transition'] = value;
+    el[0].style['-moz-transition'] = value;
+    el[0].style['-ms-transition'] = value;
+    el[0].style['-o-transition'] = value;
+    el[0].style.transition = value;
   },
 
+  /**
+   * Check if browser supports transitions
+   * @private
+   * @returns {Boolean} true if supports transitions
+   */
   supportsTransitions() {
     const s = document.createElement('p').style;
     let p = 'transition';
@@ -347,11 +421,21 @@ Homepage.prototype = {
     return false;
   },
 
+  /**
+   * Detach events
+   * @private
+   * @returns {void}
+   */
   detachEvents() {
     $('body').off('resize.homepage');
     $('.application-menu').off('applicationmenuopen.homepage applicationmenuclose.homepage');
   },
 
+  /**
+   * Resync the UI and Settings.
+   * @param {Object} settings The settings to apply.
+   * @returns {Object} The api
+   */
   updated(settings) {
     if (typeof settings !== 'undefined') {
       this.settings = utils.mergeSettings(this.element, settings, HOMEPAGE_DEFAULTS);
@@ -362,19 +446,18 @@ Homepage.prototype = {
   },
 
   /**
-  * Teardown - Remove added markup and events
-  */
+   * Destroy this component instance and remove the link from its base element.
+   * @returns {void}
+   */
   destroy() {
     this.detachEvents();
     $.removeData(this.element[0], COMPONENT_NAME);
   },
 
   /**
-   *  This component fires the following events.
-   *
-   * @fires Homepage#events
-   * @param {object} resize  Fires after the page is resized and layout is set. Can be used for any special adjustments.
-   *
+   * Attach Events used by the Control
+   * @private
+   * @returns {void}
    */
   handleEvents() {
     $('body').on('resize.homepage', () => {
