a3ed323725a5fe66c566137433beac8927941f8e
Deepak Verma <deepak.verma@infor.com>
2018-01-19 14:03:39 -0500

    SOHO-6976 - Fixed linter error in es6 Circlepager

--- components/circlepager/circlepager.jquery.js
+++ components/circlepager/circlepager.jquery.js
@@ -2,8 +2,8 @@ import { CirclePager, COMPONENT_NAME } from './circlepager';
 
 // Initialize the plugin (Once)
 $.fn.circlepager = function (settings) {
-  return this.each(function() {
-    var instance = $.data(this, COMPONENT_NAME);
+  return this.each(function () {
+    let instance = $.data(this, COMPONENT_NAME);
     if (instance) {
       instance.updated(settings);
     } else {
