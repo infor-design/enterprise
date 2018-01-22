bbbb50cb001ad593cab53604af89e7714d884830
Deepak Verma <deepak.verma@infor.com>
2018-01-22 14:41:37 -0500

    SOHO-6976 - Fixed es6 linter error for Editor

--- components/editor/editor.jquery.js
+++ components/editor/editor.jquery.js
@@ -2,8 +2,8 @@ import { Editor, COMPONENT_NAME } from './editor';
 
 // Initialize the plugin (Once)
 $.fn.editor = function (settings) {
-  return this.each(function() {
-    var instance = $.data(this, COMPONENT_NAME);
+  return this.each(function () {
+    let instance = $.data(this, COMPONENT_NAME);
     if (instance) {
       instance.updated(settings);
     } else {
