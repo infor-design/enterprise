94074ae9e913c04a456e8964a272623eadbbee52
Ed Coyle <Ed.Coyle@infor.com>
2018-02-02 17:16:44 -0500

    SOHO-6976
    - Fixed `grunt-contrib-copy` process to pull vendor libs from `node_modules` folder, instead of storing them in `/components/vendor`
    - Fixed names of vendor libs to be generic in our demoapp.
    - Added `eslint.log` output from NPM task that runs eslint
    - Added `eslint.log` to `.gitignore`

--- build/configs/copy.js
+++ build/configs/copy.js
@@ -6,10 +6,24 @@ module.exports = {
         { expand: true, flatten: true, src: ['components/angular/sohoxi-angular.js'], dest: 'dist/js/', filter: 'isFile' },
         { expand: true, flatten: true, src: ['components/sohoxi-migrate-4.4.0.js'], dest: 'dist/js/', filter: 'isFile' },
         { expand: true, flatten: true, src: ['js/*.js'], dest: 'dist/js/all/', filter: 'isFile' },
-        { expand: true, flatten: true, src: ['components/vendor/jquery-3*.js'], dest: 'dist/js/', filter: 'isFile' },
-        { expand: true, flatten: true, src: ['components/vendor/jquery-3*.min.js'], dest: 'dist/js/', filter: 'isFile' },
-        { expand: true, flatten: true, src: ['components/vendor/jquery-3*.map'], dest: 'dist/js/', filter: 'isFile' },
-        { expand: true, flatten: true, src: ['components/vendor/d3.*'], dest: 'dist/js/', filter: 'isFile' },
+        {
+          expand: true,
+          flatten: true,
+          src: [
+            'node_modules/jquery/dist/jquery.js',
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/jquery/dist/jquery.min.map'
+          ],
+          dest: 'dist/js/',
+          filter: 'isFile'
+        },
+        {
+          expand: true,
+          flatten: true,
+          src: ['node_modules/d3/build/d3.js'],
+          dest: 'dist/js/',
+          filter: 'isFile'
+        },
         { expand: true, flatten: true, src: ['components/locale/cultures/*.*'], dest: 'dist/js/cultures/', filter: 'isFile' },
         { expand: true, flatten: true, src: ['components/empty-widgets/svg-empty.html'], dest: 'dist/svg/', filter: 'isFile' },
         { expand: true, flatten: true, src: ['components/charts/svg-patterns.html'], dest: 'dist/svg/', filter: 'isFile' },
