94f0b05551e543c1413260f22c1acb5a839523cc
Dave Hall <daveghall@gmail.com>
2018-02-07 12:29:08 -0500

    Add Browserstack configs, reporting over REST API

--- components/dropdown/e2e/dropdown.e2e-spec.js
+++ components/dropdown/e2e/dropdown.e2e-spec.js
@@ -1,10 +1,30 @@
+const r2 = require('r2');
+let sessionId;
+
 describe('Dropdown tests', () => {
-  it('Should open dropdown list on click', () => {
-    const dropdownEl = element(by.css('div[aria-controls=dropdown-list]'));
-    browser.waitForAngularEnabled(false);
-    browser.get('http://localhost:4000/components/dropdown/example-index');
-    browser.wait(protractor.ExpectedConditions.presenceOf(dropdownEl), 10000);
-    dropdownEl.click();
-    expect(element(by.className('is-open')).isDisplayed()).toBe(true);
+  it('Should open dropdown list on click', async (done) => {
+    try {
+      session = await browser.driver.getSession();
+      await browser.waitForAngularEnabled(false);
+      await browser.driver.get('http://localhost:4000/components/dropdown/example-index');
+      const dropdownEl = await element(by.css('div[aria-controls=dropdown-list]'));
+      await browser.driver.wait(protractor.ExpectedConditions.presenceOf(dropdownEl), 5000);
+      await dropdownEl.click();
+      expect(await element(by.className('is-open')).isDisplayed()).toBe(true);
+      done();
+    } catch(error) {
+      sessionId = session.getId();
+      console.log(sessionId);
+      const username = process.env.BROWSER_STACK_USERNAME;
+      const accessKey = process.env.BROWSER_STACK_ACCESS_KEY;
+      const url = `https://${username}:${accessKey}@api.browserstack.com/automate/sessions/${sessionId}.json`
+      const obj = {
+        status: 'error',
+        reason: error.name
+      }
+      let res = await r2.put(url, {json: obj}).json;
+      console.log(res);
+      done.fail(error);
+    }
   });
 });
--- protractor.browserstack.conf.js
+++ protractor.browserstack.conf.js
@@ -2,45 +2,28 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 const browserstack = require('browserstack-local');
 
 exports.config = {
-  allScriptsTimeout: 20000,
+  allScriptsTimeout: 30000,
   specs: [
-    '**/e2e/*.e2e-spec.js'
+    '**/e2e/*.e2e-spec.js',
+    'async_await.js'
   ],
   seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
-  commonCapabilities: {
+  SELENIUM_PROMISE_MANAGER: false,
+  capabilities: {
     'browserstack.user': process.env.BROWSER_STACK_USERNAME,
     'browserstack.key': process.env.BROWSER_STACK_ACCESS_KEY,
     'browserstack.debug': true,
     'browserstack.local': true,
     build: 'protractor-browserstack',
     name: 'e2e test',
+    browserName: 'Safari',
+    browser_version: '11.0',
+    os_version: 'High Sierra'
   },
-  'multiCapabilities': [
-    {
-      browserName: 'Chrome',
-      browser_version: '63.0'
-    },
-    {
-      browserName: 'Safari',
-      browser_version: '11.0',
-      os_version: 'High Sierra'
-    },
-    {
-      browserName: 'Firefox',
-      browser_version: '57.0'
-    },
-    {
-      browserName: 'IE',
-      browser_version: '11.0'
-    },
-    {
-      browserName: 'Edge'
-    }
-  ],
   beforeLaunch: () => {
     return new Promise((resolve, reject) => {
       exports.bs_local = new browserstack.Local();
-      exports.bs_local.start({'key': exports.config.commonCapabilities['browserstack.key'] }, (error) => {
+      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, (error) => {
         if (error) {
           return reject(error);
         };
@@ -56,8 +39,8 @@ exports.config = {
   }
 };
 
-exports.config.multiCapabilities.forEach(caps => {
-  for (const i in exports.config.commonCapabilities) {
-    caps[i] = caps[i] || exports.config.commonCapabilities[i];
-  }
-});
+//exports.config.multiCapabilities.forEach(caps => {
+  //for (const i in exports.config.commonCapabilities) {
+    //caps[i] = caps[i] || exports.config.commonCapabilities[i];
+  //}
+//});
