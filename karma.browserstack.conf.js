597cb48191b67a70489450319a31a28571dacf85
Dave Hall <daveghall@gmail.com>
2018-03-28 09:39:14 -0400

    Adjust windows version

--- karma.browserstack.conf.js
+++ karma.browserstack.conf.js
@@ -4,7 +4,6 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
     files: [
       'dist/css/light-theme.css',
-      'dist/css/light-theme.css.map',
       'dist/js/jquery-3.1.1.js',
       'dist/js/d3.v4.js',
       'dist/js/sohoxi.js',
