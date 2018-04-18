8f92b83cd22dad67da406632cc2a3ae6d3f672d2
Dave Hall <daveghall@gmail.com>
2018-04-18 11:15:04 -0400

    Add FAQ to TESTING.md, prettied default axe options

--- test/helpers/default-axe-options.js
+++ test/helpers/default-axe-options.js
@@ -1,25 +1,24 @@
-const axeRules =
-  [
-    {
-      id: 'aria-allowed-attr',
-      enabled: false
-    },
-    {
-      id: 'aria-required-children',
-      enabled: false
-    },
-    {
-      id: 'aria-valid-attr-value',
-      enabled: false
-    },
-    {
-      id: 'color-contrast',
-      enabled: false
-    },
-    {
-      id: 'region',
-      enabled: false
-    }
-  ];
+const axeRules = [
+  {
+    id: 'aria-allowed-attr',
+    enabled: false
+  },
+  {
+    id: 'aria-required-children',
+    enabled: false
+  },
+  {
+    id: 'aria-valid-attr-value',
+    enabled: false
+  },
+  {
+    id: 'color-contrast',
+    enabled: false
+  },
+  {
+    id: 'region',
+    enabled: false
+  }
+];
 
 module.exports.axeRules = axeRules;
