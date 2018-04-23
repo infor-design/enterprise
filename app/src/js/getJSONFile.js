b5dfae37b920ad35eaa5cc7eeb76daf6947294b7
Ed Coyle <Ed.Coyle@infor.com>
2018-04-23 10:35:55 -0400

    Fixed bad paths for Dropdown junk data SOHO-7830 #4 #6

--- app/src/js/getJSONFile.js
+++ app/src/js/getJSONFile.js
@@ -1,19 +1,19 @@
 // Small tool that retrieves JSON data from a file and turns it into objects for use in the demo app.
 // NOTE: Synchronous for now.
-const fs = require('fs'),
-      path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 function readJsonFileSync(filepath, encoding) {
-  if (typeof encoding === 'undefined'){
+  if (typeof encoding === 'undefined') {
     encoding = 'utf8';
   }
 
-  let file = fs.readFileSync(filepath, encoding);
+  const file = fs.readFileSync(filepath, encoding);
   return JSON.parse(file);
 }
 
 function getJSONFile(file) {
-  let filepath = path.resolve( __dirname, file );
+  const filepath = path.resolve(__dirname, file);
   return readJsonFileSync(filepath);
 }
 
