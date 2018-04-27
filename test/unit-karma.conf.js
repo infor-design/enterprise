5f4fdf9dbb262629c04455eae7bf2cbd5bcf39d7
Christopher Lepore <chris.lepore@infor.com>
2018-04-27 18:25:03 -0400

    SAVEPOINT

--- test/unit-karma.conf.js
+++ test/unit-karma.conf.js
@@ -1,27 +1,24 @@
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
+    basePath: '..',
     frameworks: ['jasmine'],
     files: [
       'dist/js/jquery-3.3.1.js',
       'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
+      'src/components/locale/locale.js',
       'test/components/locale/locale.spec.js'
     ],
     exclude: [
       'node_modules'
     ],
     preprocessors: {
-      'src/components/locale/locale.js': ['coverage'],
       'test/components/locale/locale.spec.js': ['webpack'],
     },
     webpack: {
       module: {
         rules: [
-          {
-            test: /\.html$/,
-            use: [{
-              loader: 'html-loader'
-            }],
-          },
           {
             test: /\.js$/,
             use: [{
@@ -31,6 +28,11 @@ module.exports = function (config) {
               }
             }],
             exclude: /node_modules/
+          },
+          {
+            test: /\locale.js$/,
+            use: { loader: 'istanbul-instrumenter-loader' },
+            include: path.resolve('src/components')
           }
         ]
       }
