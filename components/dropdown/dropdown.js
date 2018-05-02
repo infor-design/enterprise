1d16a5501684659ccdb3ec1f55d6d81cc7df90dd
Ed Coyle <Ed.Coyle@infor.com>
2018-05-02 13:08:00 -0400

    SOHO-7901
    - Fixed issues with Dropdown's typeahead feature on `no-search`-configurations where it wasn't possible to cycle through all possible selections.

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1196,14 +1196,14 @@ Dropdown.prototype = {
     }
 
     self.initialFilter = true;
-    self.filterTerm += $.actualChar(e);
+    self.filterTerm += e.key;
+
+    if (self.settings.noSearch) {
+      self.selectStartsWith(self.filterTerm);
+      return;
+    }
 
     this.timer = setTimeout(() => {
-      if (self.settings.noSearch) {
-        self.selectStartsWith(self.filterTerm);
-        return;
-      }
-
       if (!self.isOpen()) {
         self.searchInput.val(self.filterTerm);
         self.toggleList();
@@ -2131,36 +2131,54 @@ Dropdown.prototype = {
    * @param {string} char - The starting letter to match for. (Case insensitive)
    */
   selectStartsWith(char) {
-    if (typeof char === 'string') {
-      const elem = this.element[0];
-      const selectedIndex = elem.selectedIndex;
-      this.filterTerm = '';
+    if (typeof char !== 'string') {
+      return;
+    }
 
-      let newIdx = -1;
+    const elem = this.element[0];
+    this.filterTerm = '';
 
-      for (let i = 0; i < elem.options.length; i++) {
-        const option = elem.options[i];
-        // Check if its a match (Case insensitive)
-        const isMatch = option.innerText.toLowerCase().indexOf(char) === 0;
+    let newIdx = -1;
+    let totalMatches = 0;
 
-        // If already a selected item that starts with it, find the next.
-        if (i === selectedIndex) {
-          continue;
-        }
+    // Log search matches
+    if (!this.searchMatches) {
+      this.searchMatches = {};
+    }
+    if (!this.searchMatches[char]) {
+      this.searchMatches[char] = [];
+    }
 
-        if (isMatch) {
+    for (let i = 0; i < elem.options.length; i++) {
+      const option = elem.options[i];
+      // Check if its a match (Case insensitive)
+      const isMatch = option.innerText.toLowerCase().indexOf(char) === 0;
+
+      if (isMatch) {
+        if (this.searchMatches[char].indexOf(i) === -1) {
+          this.searchMatches[char].push(i);
           newIdx = i;
           break;
         }
-      }
-
-      // Set the selected element
-      if (newIdx > -1) {
-        elem.selectedIndex = newIdx;
-        this.updated();
-        this.element.trigger('change');
+        totalMatches++;
+        continue;
       }
     }
+
+    if (newIdx === -1) {
+      if (!this.searchMatches[char].length) {
+        return;
+      }
+
+      if (totalMatches === this.searchMatches[char].length) {
+        newIdx = this.searchMatches[char][0];
+        this.searchMatches[char].length = 1; // reset
+      }
+    }
+
+    elem.selectedIndex = newIdx;
+    this.updated();
+    this.element.trigger('change');
   },
 
   /**
