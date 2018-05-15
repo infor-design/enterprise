4f96f84b88ce043f16a961654350bf1a0d11b1d6
Nick Wynja <nick.wynja@infor.com>
2018-05-15 19:03:19 -0400

    Publish from script

--- scripts/publish-nightly.sh
+++ scripts/publish-nightly.sh
@@ -3,5 +3,9 @@
 PKG_VERSION=$(node -p "require('./publish/package.json').version")
 DATE=$(date +%Y%m%d)
 
+echo "//registry.npmjs.org/:_authToken=\${NPM_AUTH_TOKEN}" > .npmrc
+
+npx gulp publish
 cd publish
 npm version $PKG_VERSION.$DATE
+npm publish --tag dev
