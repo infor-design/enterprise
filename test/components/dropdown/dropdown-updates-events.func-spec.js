88fdb83bfeadee8311ea7dfe50a30d362693ab2e
Larry Amisola <lamisola@phmamlamisola2.infor.com>
2018-05-16 12:38:24 +0800

    SOHO-7799 fix dropdown failed test build

--- test/components/dropdown/dropdown-updates-events.func-spec.js
+++ test/components/dropdown/dropdown-updates-events.func-spec.js
@@ -42,7 +42,8 @@ describe('Dropdown updates, events', () => {
       reloadSourceOnOpen: false,
       showEmptyGroupHeaders: false,
       showSelectAll: false,
-      sourceArguments: {}
+      sourceArguments: {},
+      enableTooltip: false
     };
 
     expect(dropdownObj.settings).toEqual(settings);
@@ -63,7 +64,8 @@ describe('Dropdown updates, events', () => {
       reloadSourceOnOpen: false,
       showEmptyGroupHeaders: false,
       showSelectAll: false,
-      sourceArguments: {}
+      sourceArguments: {},
+      enableTooltip: false
     };
 
     dropdownObj.updated();
@@ -88,7 +90,8 @@ describe('Dropdown updates, events', () => {
       reloadSourceOnOpen: false,
       showEmptyGroupHeaders: false,
       showSelectAll: false,
-      sourceArguments: {}
+      sourceArguments: {},
+      enableTooltip: false
     };
     dropdownObj.updated(settings);
 
