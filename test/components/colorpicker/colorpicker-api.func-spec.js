04cc46e7af2275fec6b31f6875d21ab03404a2f4
Deepak Verma <deepak.verma@infor.com>
2018-06-19 09:57:04 -0400

    SOHO-8078 - Added functional and e2e tests for Colorpicker

---
+++ test/components/colorpicker/colorpicker-api.func-spec.js
@@ -0,0 +1,75 @@
+import { ColorPicker } from '../../../src/components/colorpicker/colorpicker';
+
+const colorpickerHTML = require('../../../app/views/components/colorpicker/example-index.html');
+const svg = require('../../../src/components/icons/svg.html');
+
+let colorpickerEl;
+let svgEl;
+let colorpickerObj;
+
+describe('ColorPicker API', () => {
+  beforeEach(() => {
+    colorpickerEl = null;
+    svgEl = null;
+    colorpickerObj = null;
+    document.body.insertAdjacentHTML('afterbegin', svg);
+    document.body.insertAdjacentHTML('afterbegin', colorpickerHTML);
+    colorpickerEl = document.getElementById('background-color');
+    svgEl = document.body.querySelector('.svg-icons');
+    colorpickerEl.classList.add('no-init');
+    colorpickerObj = new ColorPicker(colorpickerEl);
+  });
+
+  afterEach(() => {
+    colorpickerObj.destroy();
+    colorpickerEl.parentNode.removeChild(colorpickerEl);
+    svgEl.parentNode.removeChild(svgEl);
+  });
+
+  it('Should be defined on jQuery object', () => {
+    expect(colorpickerObj).toEqual(jasmine.any(Object));
+  });
+
+  it('Should open colorpicker', (done) => {
+    colorpickerObj.toggleList();
+
+    setTimeout(() => {
+      expect(colorpickerObj.isPickerOpen).toBeTruthy();
+      expect(document.body.querySelector('.colorpicker.is-open')).toBeTruthy();
+      done();
+    }, 300);
+  });
+
+  it('Should destroy colorpicker', () => {
+    colorpickerObj.destroy();
+
+    expect(colorpickerEl.parentNode.classList.contains('colorpicker-container')).toBeFalsy();
+  });
+
+  it('Should disable colorpicker', () => {
+    colorpickerObj.disable();
+
+    expect(colorpickerEl.parentNode.classList.contains('is-disabled')).toBeTruthy();
+    expect(colorpickerObj.isDisabled()).toBeTruthy();
+  });
+
+  it('Should enable colorpicker', () => {
+    colorpickerObj.disable();
+
+    expect(colorpickerEl.parentNode.classList.contains('is-disabled')).toBeTruthy();
+    expect(colorpickerObj.isDisabled()).toBeTruthy();
+
+    colorpickerObj.enable();
+
+    expect(colorpickerEl.parentNode.classList.contains('is-disabled')).toBeFalsy();
+    expect(colorpickerObj.isDisabled()).toBeFalsy();
+  });
+
+  it('Should render colorpicker readonly', () => {
+    colorpickerObj.readonly();
+
+    expect(document.body.querySelector('.colorpicker[readonly]')).toBeTruthy();
+    expect(colorpickerEl.parentNode.classList.contains('is-readonly')).toBeTruthy();
+    expect(colorpickerObj.isDisabled()).toBeFalsy();
+  });
+});
---
+++ test/components/colorpicker/colorpicker.e2e-spec.js
@@ -0,0 +1,206 @@
+const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const config = requireHelper('e2e-config');
+const utils = requireHelper('e2e-utils');
+requireHelper('rejection');
+
+jasmine.getEnv().addReporter(browserStackErrorReporter);
+
+describe('Colorpicker example-index tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/example-index');
+  });
+
+  it('Should open popup on icon click', async () => {
+    await element(by.css('#background-color + .trigger .icon')).click();
+
+    expect(await element(by.css('#background-color.is-open')).isDisplayed()).toBe(true);
+  });
+
+  it('Should pick color from picker', async () => {
+    await element(by.css('#background-color + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('background-color')).getAttribute('value')).toEqual('#1A1A1A');
+  });
+
+  it('Should pick clear color from picker', async () => {
+    await element(by.css('#background-color + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:last-child a:first-child')).click();
+
+    expect(await element(by.id('background-color')).getAttribute('value')).toEqual('');
+    expect(await element(by.css('.swatch.is-invalid')).isDisplayed()).toBe(true);
+  });
+
+  it('Should open popup on keypress(arrow-down)', async () => {
+    await element(by.id('background-color')).sendKeys(protractor.Key.ARROW_DOWN);
+
+    expect(await element(by.css('#background-color.is-open')).isDisplayed()).toBe(true);
+  });
+
+  it('Should pick color from picker with keypress', async () => {
+    await element(by.id('background-color')).sendKeys(protractor.Key.ARROW_DOWN);
+
+    expect(await element(by.css('#background-color.is-open')).isDisplayed()).toBe(true);
+
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).sendKeys(protractor.Key.SPACE);
+
+    expect(await element(by.id('background-color')).getAttribute('value')).toEqual('#1A1A1A');
+  });
+
+  it('Should pick clear color from picker with keypress', async () => {
+    await element(by.id('background-color')).sendKeys(protractor.Key.ARROW_DOWN);
+
+    expect(await element(by.css('#background-color.is-open')).isDisplayed()).toBe(true);
+
+    await element(by.css('#colorpicker-menu li:last-child a:first-child')).sendKeys(protractor.Key.SPACE);
+
+    expect(await element(by.id('background-color')).getAttribute('value')).toEqual('');
+    expect(await element(by.css('.swatch.is-invalid')).isDisplayed()).toBe(true);
+  });
+
+  it('Should set clear swatch', async () => {
+    await element(by.id('background-color')).sendKeys(protractor.Key.BACK_SPACE);
+    await element(by.css('body')).click();
+
+    expect(await element(by.id('background-color')).getAttribute('value')).toEqual('#B94E4');
+    expect(await element(by.css('.swatch.is-invalid')).isDisplayed()).toBe(true);
+  });
+});
+
+describe('Colorpicker show label tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/example-show-label');
+  });
+
+  it('Should pick color from picker and set label', async () => {
+    await element(by.css('#show-label + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('show-label')).getAttribute('value')).toEqual('Slate10');
+  });
+
+  it('Should pick clear color from picker and set label', async () => {
+    await element(by.css('#show-label + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:last-child a:first-child')).click();
+
+    expect(await element(by.id('show-label')).getAttribute('value')).toEqual('None');
+  });
+});
+
+describe('Colorpicker custom label tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/example-custom-labels');
+  });
+
+  it('Should pick color from picker as custom color and label', async () => {
+    await element(by.css('#custom-color + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('custom-color')).getAttribute('value')).toEqual('#2578a9');
+
+    await element(by.css('#custom-label + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('custom-label')).getAttribute('value')).toEqual('Grape10');
+  });
+});
+
+describe('Colorpicker case types tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/test-case-types');
+  });
+
+  it('Should pick color from picker and use upper case', async () => {
+    await element(by.css('#colorpicker-uc + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('colorpicker-uc')).getAttribute('value')).toEqual('#1A1A1A');
+  });
+
+  it('Should pick color from picker and use lower case', async () => {
+    await element(by.css('#colorpicker-lc + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('colorpicker-lc')).getAttribute('value')).toEqual('#1a1a1a');
+  });
+});
+
+describe('Colorpicker clearable tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/test-clearable');
+  });
+
+  it('Should show clearable option', async () => {
+    await element(by.css('#cp-clearable-true + .trigger .icon')).click();
+
+    expect(await element(by.css('#colorpicker-menu li:last-child .swatch')).getAttribute('class')).toContain('is-empty');
+  });
+
+  it('Should not show clearable option', async () => {
+    await element(by.css('#cp-clearable-false + .trigger .icon')).click();
+
+    expect(await element(by.css('#colorpicker-menu li:last-child .swatch')).getAttribute('class')).not.toContain('is-empty');
+  });
+});
+
+describe('Colorpicker states tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/colorpicker/test-states');
+  });
+
+  it('Should check for state Non-Editable', async () => {
+    await element(by.css('#non-editable + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.id('non-editable')).getAttribute('value')).toEqual('#1A1A1A');
+
+    await element(by.id('non-editable')).sendKeys(protractor.Key.BACK_SPACE);
+    await element(by.css('body')).click();
+
+    expect(await element(by.id('non-editable')).getAttribute('value')).toEqual('#1A1A1A');
+  });
+
+  it('Should check for state Disabled', async () => {
+    await element(by.css('#foreground-color + .trigger .icon')).click();
+
+    expect(await element(by.css('#foreground-color.is-open')).isPresent()).toBe(false);
+    expect(await element(by.css('#foreground-color')).isEnabled()).toBeFalsy();
+  });
+
+  it('Should check for state Readonly', async () => {
+    await element(by.css('#readonly-color + .trigger .icon')).click();
+
+    expect(await element(by.css('#readonly-color.is-open')).isPresent()).toBe(false);
+    expect(await element(by.css('#readonly-color[readonly]')).isDisplayed()).toBe(true);
+  });
+
+  it('Should check for Dirty Tracking', async () => {
+    await element(by.css('#dirty-color + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:first-child a:first-child')).click();
+
+    expect(await element(by.css('.icon-dirty')).isPresent()).toBe(true);
+
+    await element(by.css('#dirty-color + .trigger .icon')).click();
+    await element(by.css('#colorpicker-menu li:last-child a:first-child')).click();
+
+    expect(await element(by.css('.icon-dirty')).isPresent()).toBe(false);
+  });
+
+  it('Should check for Validation required rule', async () => {
+    const colorpickerEl = await element(by.id('required-color'));
+    await colorpickerEl.clear();
+
+    expect(await colorpickerEl.getAttribute('value')).toEqual('');
+
+    await colorpickerEl.sendKeys(protractor.Key.TAB);
+    await browser.driver.sleep(config.sleep);
+
+    expect(await element(by.css('.message-text')).getText()).toBe('Required');
+    expect(await element(by.css('.icon-error')).isPresent()).toBe(true);
+    expect(await colorpickerEl.getAttribute('class')).toContain('error');
+  });
+
+  it('Should check for api option Just Color', async () => {
+    expect(await element(by.id('color-only')).getCssValue('width')).toBe('10px');
+  });
+});
