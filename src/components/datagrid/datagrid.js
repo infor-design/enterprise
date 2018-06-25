99ef8715969f0323b3ded457b41b8df83d7be681
Terrence Marion Castro <TerrenceMarion.Castro@infor.com>
2018-06-25 13:32:04 +0800

    SOHO-8128 Added onKeyDown setting

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -6138,7 +6138,16 @@ Datagrid.prototype = {
       // Tab, Left and Right arrow keys.
       if ([9, 37, 39].indexOf(key) !== -1) {
         if (key === 9 && !self.settings.actionableMode) {
-          return;
+          if (self.settings.onKeyDown) {
+            const ret = self.settings.onKeyDown(e);
+            if (ret === false) {
+              e.stopPropagation();
+              e.preventDefault();
+              return;
+            }
+          } else {
+            return;
+          }
         }
 
         if (key !== 9 && e.altKey) {
