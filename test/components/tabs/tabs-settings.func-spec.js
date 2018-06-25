d94952970ddd91eba3db9a91f94b56b02516bd03
Dave Hall <dave.hall@infor.com>
2018-06-25 16:31:14 -0400

    Add Settings Tabs functional tests

---
+++ test/components/tabs/tabs-settings.func-spec.js
@@ -0,0 +1,74 @@
+import { Tabs } from '../../../src/components/tabs/tabs';
+
+const tabsHTML = require('../../../app/views/components/tabs/example-index.html');
+const svg = require('../../../src/components/icons/svg.html');
+
+let tabsEl;
+let tabsPanelEl;
+let svgEl;
+let tabsObj;
+
+describe('Tabs Settings', () => {
+  beforeEach(() => {
+    tabsEl = null;
+    tabsPanelEl = null;
+    svgEl = null;
+    tabsObj = null;
+    document.body.insertAdjacentHTML('afterbegin', svg);
+    document.body.insertAdjacentHTML('afterbegin', tabsHTML);
+    tabsEl = document.body.querySelector('.tab-container');
+    tabsPanelEl = document.body.querySelector('.tab-panel-container');
+    svgEl = document.body.querySelector('.svg-icons');
+    tabsEl.classList.add('no-init');
+    tabsObj = new Tabs(tabsEl);
+  });
+
+  afterEach(() => {
+    tabsObj.destroy();
+    tabsEl.parentNode.removeChild(tabsEl);
+    tabsPanelEl.parentNode.removeChild(tabsPanelEl);
+    svgEl.parentNode.removeChild(svgEl);
+  });
+
+  it('Should set settings', () => {
+    const settings = {
+      addTabButton: false,
+      addTabButtonCallback: null,
+      appMenuTrigger: false,
+      ajaxOptions: null,
+      containerElement: null,
+      changeTabOnHashChange: false,
+      hashChangeCallback: null,
+      lazyLoad: true,
+      moduleTabsTooltips: false,
+      source: null,
+      sourceArguments: {},
+      tabCounts: false,
+      verticalResponsive: false
+    };
+    tabsObj.updated();
+
+    expect(tabsObj.settings).toEqual(settings);
+  });
+
+  it('Should update set settings via parameter', () => {
+    const settings = {
+      addTabButton: true,
+      addTabButtonCallback: null,
+      appMenuTrigger: false,
+      ajaxOptions: null,
+      containerElement: null,
+      changeTabOnHashChange: true,
+      hashChangeCallback: null,
+      lazyLoad: true,
+      moduleTabsTooltips: false,
+      source: null,
+      sourceArguments: {},
+      tabCounts: false,
+      verticalResponsive: false
+    };
+    tabsObj.updated(settings);
+
+    expect(tabsObj.settings).toEqual(settings);
+  });
+});
