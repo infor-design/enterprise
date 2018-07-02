3f93f63dd00933b45032dfe8c2d61da19f783de6
Dave Hall <dave.hall@infor.com>
2018-07-02 11:46:53 -0400

    Add spinbox visual regression baselines

--- test/components/spinbox/spinbox.e2e-spec.js
+++ test/components/spinbox/spinbox.e2e-spec.js
@@ -1,7 +1,5 @@
-// TODO: Write WCAG tests
-// TODO: Write Visual Regression tests
-
 const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const utils = requireHelper('e2e-utils');
 const config = requireHelper('e2e-config');
 requireHelper('rejection');
 
@@ -24,6 +22,23 @@ describe('Spinbox example-index tests', () => {
     await browser.driver
       .wait(protractor.ExpectedConditions.presenceOf(element(by.id(spinboxId))), config.waitsFor);
   });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visual regress', async () => {
+      const spinboxElWrapper = element(by.className('spinbox-wrapper'));
+      await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(spinboxElWrapper), config.waitsFor);
+      await browser.driver.sleep(config.waitsFor);
+
+      expect(await browser.protractorImageComparison.checkElement(spinboxElWrapper, 'spinbox-init')).toEqual(0);
+      await spinboxEl.sendKeys(protractor.Key.ARROW_UP);
+      await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(element(by.css('.spinbox-wrapper.is-focused'))), config.waitsFor);
+      await browser.driver.sleep(config.waitsFor);
+
+      expect(await browser.protractorImageComparison.checkElement(spinboxElWrapper, 'spinbox-clicked')).toEqual(0);
+    });
+  }
 
   it('Should be set with down arrow', async () => {
     await spinboxEl.sendKeys(protractor.Key.ARROW_DOWN);
