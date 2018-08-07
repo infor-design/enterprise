624db530b3e964e0ab47fe6ef32291b801fe4993
James Allen <tjamesallen15@yahoo.com>
2018-08-07 09:15:42 +0800

    Accordion Functional and E2E Tests

---
+++ test/components/accordion/accordion-api.func-spec.js
@@ -0,0 +1,52 @@
+import { Accordion } from '../../../src/components/accordion/accordion';
+
+const accordionHTML = require('../../../app/views/components/accordion/example-index.html');
+const svg = require('../../../src/components/icons/svg.html');
+
+let accordionEl;
+let svgEl;
+let accordionObj;
+
+describe('Accordion API', () => {
+  beforeEach(() => {
+    accordionEl = null;
+    svgEl = null;
+    accordionObj = null;
+    document.body.insertAdjacentHTML('afterbegin', svg);
+    document.body.insertAdjacentHTML('afterbegin', accordionHTML);
+    accordionEl = document.body.querySelector('.accordion');
+    svgEl = document.body.querySelector('.svg-icons');
+    accordionObj = new Accordion(accordionEl);
+  });
+
+  afterEach(() => {
+    accordionObj.destroy();
+    svgEl.parentNode.removeChild(svgEl);
+  });
+
+  it('Should be defined', () => {
+    expect(accordionObj).toEqual(jasmine.any(Object));
+  });
+
+  it('Should be visible', () => {
+    expect(document.body.querySelector('.accordion')).toBeTruthy();
+  });
+
+  it('Should be able to destroy it', () => {
+    accordionObj.destroy();
+
+    expect(document.body.querySelector('.btn.hide-focus')).toBeFalsy();
+  });
+
+  it('Should be able to enable it', () => {
+    accordionObj.enable();
+
+    expect(document.body.querySelector('.accordion.is-disabled')).toBeFalsy();
+  });
+
+  it('Should be able to disable it', () => {
+    accordionObj.disable();
+
+    expect(document.body.querySelector('.accordion.is-disabled')).toBeTruthy();
+  });
+});
---
+++ test/components/accordion/accordion.e2e-spec.js
@@ -0,0 +1,118 @@
+const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const utils = requireHelper('e2e-utils');
+
+requireHelper('rejection');
+
+jasmine.getEnv().addReporter(browserStackErrorReporter);
+
+describe('Accordion example-accordion-click-event tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-accordion-click-event');
+  });
+
+  it('Should click accordion be displayed', async () => {
+    expect(await element(by.className('accordion'))).toBeTruthy();
+  });
+
+  it('Should click accordion be expanded', async () => {
+    const buttonEl = await element.all(by.tagName('button')).first();
+    await buttonEl.click();
+
+    expect(await element(by.xpath('//a[@aria-expanded="true"]'))).toBeTruthy();
+  });
+
+  it('Should accordion click event be shown', async () => {
+    const accordionEl = await element.all(by.className('accordion-header')).first();
+
+    await browser.actions().mouseMove(accordionEl).perform();
+    await browser.actions().click(accordionEl).perform();
+
+    expect(await element(by.className('toast'))).toBeTruthy();
+  });
+});
+
+describe('Accordion example-accordion-lazy-loading tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-accordion-lazy-loading');
+  });
+
+  it('Should lazy accordion be displayed', async () => {
+    expect(await element(by.className('accordion'))).toBeTruthy();
+  });
+
+  it('Should lazy accordion be expanded', async () => {
+    const buttonEl = await element.all(by.tagName('button')).first();
+    await buttonEl.click();
+
+    expect(await element(by.xpath('//a[@aria-expanded="true"]'))).toBeTruthy();
+  });
+});
+
+describe('Accordion example-accordion-panels tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-accordion-panels');
+  });
+
+  it('Should panel accordion be displayed', async () => {
+    expect(await element(by.className('accordion'))).toBeTruthy();
+  });
+
+  it('Should accordion have panels', async () => {
+    expect(await element(by.className('accordion panel'))).toBeTruthy();
+  });
+
+  it('Should panel accordion be expanded', async () => {
+    const buttonEl = await element.all(by.tagName('button')).first();
+    await buttonEl.click();
+
+    expect(await element(by.xpath('//a[@aria-expanded="true"]'))).toBeTruthy();
+  });
+});
+
+describe('Accordion example-ajax tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-ajax');
+  });
+
+  it('Should ajax accordion be displayed', async () => {
+    expect(await element(by.className('accordion'))).toBeTruthy();
+  });
+
+  it('Should ajax accordion be expanded', async () => {
+    const buttonEl = await element.all(by.tagName('button')).first();
+    await buttonEl.click();
+
+    expect(await element(by.xpath('//a[@aria-expanded="true"]'))).toBeTruthy();
+  });
+});
+
+describe('Accordion example-disabled tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-disabled');
+  });
+
+  it('Should accordion be disabled', async () => {
+    expect(await element(by.className('is-disabled'))).toBeTruthy();
+  });
+
+  it('Should disabled accordion be displayed', async () => {
+    expect(await element(by.className('accordion is-disabled'))).toBeTruthy();
+  });
+});
+
+describe('Accordion example-index tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/accordion/example-index');
+  });
+
+  it('Should accordion be displayed', async () => {
+    expect(await element(by.className('accordion'))).toBeTruthy();
+  });
+
+  it('Should accordion can be expanded', async () => {
+    const buttonEl = await element.all(by.tagName('button')).first();
+    await buttonEl.click();
+
+    expect(await element(by.xpath('//a[@aria-expanded="true"]'))).toBeTruthy();
+  });
+});
