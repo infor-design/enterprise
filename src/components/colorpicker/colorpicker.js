b993b2916ebe880188c3b6ffa9d55fa56e87ad25
Enrico Angeles <enrico.angeles@infor.com>
2018-08-17 11:15:23 +0800

    revert the value of azure02

--- src/components/colorpicker/colorpicker.js
+++ src/components/colorpicker/colorpicker.js
@@ -114,7 +114,7 @@ const COLORPICKER_DEFAULTS = {
     { label: 'Azure', number: '05', value: '4EA0D1' },
     { label: 'Azure', number: '04', value: '69B5DD' },
     { label: 'Azure', number: '03', value: '8DC9E6' },
-    { label: 'Azure', number: '02', value: 'ADDBEB' },
+    { label: 'Azure', number: '02', value: 'ADD8EB' },
     { label: 'Azure', number: '01', value: 'C8E9F4' }
   ],
   placeIn: null, // null|'editor'
