5602fcfeac253bfa552217f3b8ab5085e52251b3
Tim McConechy <tim.mcconechy@infor.com>
2018-08-24 14:23:27 -0400

    Fix test again

--- test/components/dropdown/dropdown.e2e-spec.js
+++ test/components/dropdown/dropdown.e2e-spec.js
@@ -361,7 +361,7 @@ describe('Dropdown example-no-search tests', () => {
     await dropdownPseudoEl.sendKeys('z');
     await browser.driver
       .wait(protractor.ExpectedConditions.textToBePresentInElement(await element.all(by.css('.dropdown span')).first(), ''), config.waitsFor);
-    const dropdownHTML = await browser.executeScript('return document.querySelector("div[aria-controls=\'dropdown-list\']").inner');
+    const dropdownHTML = await browser.executeScript('return document.querySelector("div[aria-controls=\'dropdown-list\']").innerHTML');
 
     expect(dropdownHTML).toEqual('<span></span>');
   });
