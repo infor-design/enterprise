7b49f3685a9df540c27c46d1a181eb7568ad24c8
Enrico Angeles <eangeles@PHMAMEANGELES1.infor.com>
2018-09-06 09:31:02 +0800

    revert trackdirty.js

--- src/components/trackdirty/trackdirty.js
+++ src/components/trackdirty/trackdirty.js
@@ -184,10 +184,10 @@ Trackdirty.prototype = {
         if (!d.icon.is('.icon-dirty')) {
           if (input.is('[type="checkbox"]')) {
             d.rect = this.getAbsolutePosition(label);
-            d.style = `left:${d.rect.left}px; top:${d.rect.top}px`;
+            d.style = `left:${d.rect.left}px; top:${d.rect.top}px;`;
           } else if (input.is('.colorpicker') && !Locale.isRTL()) {
             d.rect = this.getAbsolutePosition(input);
-            d.style = `left:${d.rect.left}px; top:${d.rect.top}px`;
+            d.style = `left:${d.rect.left}px; top:${d.rect.top}px;`;
           }
           d.icon = `<span class="icon-dirty${d.class}"></span>`;
           d.msg = Locale.translate('MsgDirty') || '';
