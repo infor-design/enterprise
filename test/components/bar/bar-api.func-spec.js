a73f5e1dc596a7a68d1ab8dcb6a96bd815e4ecce
James Allen <JamesAllen.Tadique@infor.com>
2018-10-09 14:46:57 +0800

    Updates

--- test/components/bar/bar-api.func-spec.js
+++ test/components/bar/bar-api.func-spec.js
@@ -1,4 +1,5 @@
 import { Bar } from '../../../src/components/bar/bar';
+import { Locale } from '../../../src/components/locale/locale';
 
 const barHTML = require('../../../app/views/components/bar/example-index.html');
 const svg = require('../../../src/components/icons/svg.html');
@@ -40,6 +41,12 @@ describe('Bar API', () => {
     document.body.insertAdjacentHTML('afterbegin', barHTML);
     barEl = document.body.querySelector('.chart-container');
     svgEl = document.body.querySelector('.svg-icons');
+
+    Locale.addCulture('ar-SA', Soho.Locale.cultures['ar-SA']);
+    Locale.addCulture('en-US', Soho.Locale.cultures['en-US']);
+    Locale.set('en-US');
+    Soho.Locale.set('en-US');
+
     barObj = new Bar(barEl, settings);
   });
 
