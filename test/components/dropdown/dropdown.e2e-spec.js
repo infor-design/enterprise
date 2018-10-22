9ee638d7aceb63372585ef010269d2737d9a7a81
Tim McConechy <tim.mcconechy@infor.com>
2018-10-22 16:53:51 -0400

    Remove Fit

--- test/components/dropdown/dropdown.e2e-spec.js
+++ test/components/dropdown/dropdown.e2e-spec.js
@@ -108,7 +108,7 @@ describe('Dropdown example-index tests', () => {
   }
 
   if (utils.isChrome() && utils.isCI()) {
-    fit('Should not visual regress', async () => {
+    it('Should not visual regress', async () => {
       const dropdownEl = element(by.css('div[aria-controls="dropdown-list"]'));
       const dropdownElList = element(by.id('dropdown-list'));
       await browser.driver
