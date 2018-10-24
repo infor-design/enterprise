01f5d4e994d19b5cc6a901ea8a0d755b3603dde9
Tim McConechy <tim.mcconechy@infor.com>
2018-10-24 16:15:03 -0400

    Added fix for error on lookup. Added updateDataset function. Removed timeout. Added tests

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -5436,6 +5436,11 @@ Datagrid.prototype = {
    */
   highlightSearchRows(term) {
     const self = this;
+
+    if (!term || term === '') {
+      return;
+    }
+
     // Move across all visible cells and rows, highlighting
     this.tableBody.find('tr').each(function () {
       let found = false;
