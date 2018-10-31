27006070f2178881bceafa135ec5e209806f2bc0
David Berg <dberg2@USROMDBERG01.local>
2018-10-31 13:40:10 -0400

    Improve test.

--- test/components/datagrid/datagrid.e2e-spec.js
+++ test/components/datagrid/datagrid.e2e-spec.js
@@ -702,6 +702,24 @@ describe('Datagrid paging serverside single select tests', () => {
   });
 });
 
+describe('Datagrid Paging with Summary Row test', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/datagrid/example-paging-with-summary-row');
+
+    const datagridEl = await element(by.id('datagrid'));
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(datagridEl), config.waitsFor);
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  it('Should display summary row', async () => {
+    expect(await element.all(by.css('tr.datagrid-summary-row')).count()).toEqual(1);
+  });
+});
+
 describe('Datagrid select and focus row', () => {
   beforeEach(async () => {
     await utils.setPage('/components/datagrid/test-select-and-focus-row');
@@ -976,23 +994,5 @@ describe('Datagrid Row Indeterminate Activation tests', () => {
     row = await element(by.css('tbody tr[aria-rowindex="2"]'));
 
     expect(await row.getAttribute('class')).toContain('is-rowactivated');
-  });
-});
-
-describe('Datagrid multiselect with no selection checkbox', () => {
-  beforeEach(async () => {
-    await utils.setPage('/components/datagrid/example-paging-with-summary-row');
-
-    const datagridEl = await element(by.id('datagrid'));
-    await browser.driver
-      .wait(protractor.ExpectedConditions.presenceOf(datagridEl), config.waitsFor);
-  });
-
-  it('Should not have errors', async () => {
-    await utils.checkForErrors();
-  });
-
-  it('Should display summary row', async () => {
-    expect(await element(by.css('tr.datagrid-summary-row').count()).toEqual(1));
   });
 });
