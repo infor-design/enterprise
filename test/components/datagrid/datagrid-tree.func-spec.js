18c2976fcaa0708413bf60c01474ccc247045cb9
Deepak Verma <deepak.verma@infor.com>
2018-11-06 17:47:19 -0500

    477 - Added functional tests for dirty cells with datagrid

--- test/components/datagrid/datagrid-tree.func-spec.js
+++ test/components/datagrid/datagrid-tree.func-spec.js
@@ -1,6 +1,6 @@
 import { Datagrid } from '../../../src/components/datagrid/datagrid';
 import { Formatters } from '../../../src/components/datagrid/datagrid.formatters';
-// import { Editors } from '../../../src/components/datagrid/datagrid.editors';
+import { Editors } from '../../../src/components/datagrid/datagrid.editors';
 
 const datagridHTML = require('../../../app/views/components/datagrid/example-tree.html');
 const svg = require('../../../src/components/icons/svg.html');
@@ -17,7 +17,7 @@ let datagridObj;
 const columns = [];
 columns.push({ id: 'taskName', name: 'Task', field: 'taskName', expanded: 'expanded', formatter: Formatters.Tree });
 columns.push({ id: 'id', name: 'Id', field: 'id' });
-columns.push({ id: 'desc', name: 'Description', field: 'desc' });
+columns.push({ id: 'desc', name: 'Description', field: 'desc', editor: Editors.Input });
 columns.push({ id: 'comments', name: 'Comments', field: 'comments', formatter: Formatters.Hyperlink });
 columns.push({ id: 'time', name: 'Time', field: 'time' });
 
@@ -79,5 +79,86 @@ describe('Datagrid Tree', () => {
 
     expect(spyEventRowActivated).toHaveBeenTriggered();
     $(datagridEl).off('rowactivated');
+  });
+
+  it('Should be able to track dirty cells with treeGrid', () => {
+    datagridObj.destroy();
+    datagridObj = new Datagrid(datagridEl, { dataset: data, columns, treeGrid: true, editable: true, showDirty: true }); // eslint-disable-line max-len
+
+    expect(document.body.querySelectorAll('tbody tr.datagrid-tree-parent').length).toEqual(7);
+    expect(document.body.querySelectorAll('tbody tr.datagrid-tree-child').length).toEqual(13);
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(0);
+
+    const cell1 = document.querySelector('tr:nth-child(5) td:nth-child(3)');
+    const cell2 = document.querySelector('tr:nth-child(5) td:nth-child(2)');
+
+    cell1.click();
+    let input = cell1.querySelector('input');
+    const originalVal = input.value;
+    input.value = 'Cell test value';
+    cell2.click();
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(1);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeTruthy();
+
+    cell1.click();
+    input = cell1.querySelector('input');
+    input.value = originalVal;
+    cell2.click();
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(0);
+  });
+
+  it('Should be able to track dirty cells with sort column and treeGrid', () => {
+    datagridObj.destroy();
+    datagridObj = new Datagrid(datagridEl, { dataset: data, columns, treeGrid: true, editable: true, showDirty: true }); // eslint-disable-line max-len
+
+    expect(document.body.querySelectorAll('tbody tr.datagrid-tree-parent').length).toEqual(7);
+    expect(document.body.querySelectorAll('tbody tr.datagrid-tree-child').length).toEqual(13);
+
+    let cell1 = document.querySelector('tr:nth-child(5) td:nth-child(3)');
+    let cell2 = document.querySelector('tr:nth-child(5) td:nth-child(2)');
+    let input;
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(0);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeFalsy();
+    expect(document.querySelector('tr:nth-child(5) td:nth-child(2)').innerText.substr(0, 10).trim()).toEqual('5');
+
+    cell1.click();
+    input = cell1.querySelector('input');
+    const originalVal = input.value;
+    input.value = 'Cell test value';
+    cell2.click();
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(1);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeTruthy();
+
+    datagridObj.setSortColumn('id', false);
+
+    expect(document.querySelector('tr:nth-child(5) td:nth-child(2)').innerText.substr(0, 10).trim()).toEqual('19');
+
+    cell1 = document.querySelector('tr:nth-child(18) td:nth-child(3)');
+    cell2 = document.querySelector('tr:nth-child(18) td:nth-child(2)');
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(1);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeTruthy();
+
+    datagridObj.setSortColumn('taskName', true);
+
+    expect(document.querySelector('tr:nth-child(5) td:nth-child(2)').innerText.substr(0, 10).trim()).toEqual('4');
+
+    cell1 = document.querySelector('tr:nth-child(22) td:nth-child(3)');
+    cell2 = document.querySelector('tr:nth-child(22) td:nth-child(2)');
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(1);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeTruthy();
+
+    cell1.click();
+    input = cell1.querySelector('input');
+    input.value = originalVal;
+    cell2.click();
+
+    expect(document.querySelectorAll('.is-dirty-cell').length).toEqual(0);
+    expect(cell1.classList.contains('is-dirty-cell')).toBeFalsy();
   });
 });
