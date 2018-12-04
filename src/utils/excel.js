d2da7819db80595b1587318cd5ec4f0bb1783813
Tim McConechy <tim.mcconechy@infor.com>
2018-12-04 09:49:10 -0500

    Adjustmented for 4.12x settings

--- src/utils/excel.js
+++ src/utils/excel.js
@@ -306,11 +306,11 @@ excel.exportToExcel = function (fileName, worksheetName, customDs, self) {
  * Export the grid contents to csv
  * @param {string} fileName The desired export filename in the download.
  * @param {string} customDs An optional customized version of the data to use.
- * @param {string} self The grid api to use (if customDs is not used)
  * @param {string} separator (optional) If user's machine is configured for a locale with alternate default seperator.
+ * @param {string} self The grid api to use (if customDs is not used)
  * @returns {void}
  */
-excel.exportToCsv = function (fileName, customDs, self, separator = 'sep=,') {
+excel.exportToCsv = function (fileName, customDs, separator = 'sep=,', self) {
   const formatCsv = function (table) {
     const csv = [];
     const rows = [].slice.call(table[0].querySelectorAll('tr'));
