651acf206438536236321079082163e3ab8ced1a
Tim McConechy <tim.mcconechy@infor.com>
2018-12-12 12:31:04 -0500

    Fix lint error

--- test/components/datepicker/datepicker.e2e-spec.js
+++ test/components/datepicker/datepicker.e2e-spec.js
@@ -870,6 +870,7 @@ describe('Datepicker set first day of week tests', () => {
     expect(await element(by.css('.is-focused'))).toBeTruthy();
 
     const testEl = await triggerEl.all(by.tagName('th')).get(0);
+
     expect(await testEl.getText()).toEqual('M');
   });
 });
