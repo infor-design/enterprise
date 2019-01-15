8da43986a3a490219412112b95b2a703bcec64c0
Tim McConechy <tim.mcconechy@infor.com>
2019-01-15 10:16:02 -0500

    Cleanup bubble to checkboxes

--- test/components/button/button.e2e-spec.js
+++ test/components/button/button.e2e-spec.js
@@ -293,3 +293,24 @@ describe('Button example-100-percent tests', () => {
     });
   }
 });
+
+describe('Button secondary border tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/button/example-index');
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visual regress', async () => {
+      const blockgridEl = await element(by.css('div[role=main]'));
+      await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(blockgridEl), config.waitsFor);
+      await browser.driver.sleep(config.sleep);
+
+      expect(await browser.protractorImageComparison.checkScreen('button-secondary-border')).toEqual(0);
+    });
+  }
+});
