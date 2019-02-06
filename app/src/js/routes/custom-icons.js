081e935f3233ae3f49dbd25331e5712f10311b61
Christopher Lepore <chris.lepore@infor.com>
2019-02-06 14:18:23 -0500

    Add empty state class for widget icons

--- app/src/js/routes/custom-icons.js
+++ app/src/js/routes/custom-icons.js
@@ -27,12 +27,12 @@ const hbsTemplate = `
   <div class="row top-padding">
     <div class="twelve columns demo">
       {{#each cat.icons as |iconName iconKey|}}
-      <button type="button" class="btn-icon" title="{{iconName}}">
-        <svg class="icon" focusable="false" aria-hidden="true" role="presentation">
-          <use xlink:href="{{iconName}}"></use>
-        </svg>
-        <span class="audible">{{iconName}}</span>
-      </button>
+        <div class="demo-svg" title="{{iconName}}">
+          <svg class="icon {{../../additionalClass}}" focusable="false" aria-hidden="true" role="presentation">
+            <use xlink:href="{{iconName}}"></use>
+          </svg>
+          <span class="audible">{{iconName}}</span>
+        </div>
       {{/each}}
     </div>
   </div>
@@ -47,6 +47,11 @@ module.exports = url => {
   const fileName = path.basename(url, '.html');
   const iconSet  = pageMap[fileName];
   const meta = JSON.parse(fs.readFileSync(`node_modules/ids-identity/dist/theme-soho/icons/${iconSet}/metadata.json`, 'utf-8').toString());
+
+  if (iconSet === 'empty') {
+    meta.additionalClass = 'icon-empty-state';
+  }
+
   const html = template(meta);
   return html;
 };
