8a9dad825a5533afa19634bb490db8f77751c351
Ed Coyle <Ed.Coyle@infor.com>
2019-02-27 10:58:41 -0500

    Added e2e tests for Pager inside of List/Detail pattern

---
+++ test/components/pager/pager-listdetail.e2e-spec.js
@@ -0,0 +1,67 @@
+const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const utils = requireHelper('e2e-utils');
+const config = requireHelper('e2e-config');
+requireHelper('rejection');
+
+jasmine.getEnv().addReporter(browserStackErrorReporter);
+
+describe('Pager (List/Detail, no page size selector)', () => {
+  beforeEach(async () => {
+    await utils.setPage('/patterns/list-detail-paging.html');
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(await element(by.css('.pager-toolbar'))), config.waitsFor);
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  it('Should have two navigation buttons', async () => {
+    expect(await element.all(by.css('.pager-toolbar li')).count()).toEqual(2);
+  });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visually regress', async () => {
+      const pagerToolbar = await element(by.css('.pager-toolbar'));
+
+      expect(await browser.protractorImageComparison.checkElement(pagerToolbar, 'pager-listdetail')).toEqual(0);
+    });
+  }
+});
+
+describe('Pager (List/Detail, with page size selector)', () => {
+  beforeEach(async () => {
+    await utils.setPage('/patterns/list-detail-paging-pagesize-selector.html');
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(await element(by.css('.pager-toolbar'))), config.waitsFor);
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  it('Should have four navigation buttons and a small page size selector', async () => {
+    expect(await element.all(by.css('.pager-toolbar > li')).count()).toEqual(5);
+    expect(await element.all(by.css('.pager-toolbar > li.pager-pagesize')).isPresent()).toBeTruthy();
+
+    await element(by.css('.pager-toolbar li.pager-pagesize button')).click();
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(await element(by.css('#popupmenu-1'))), config.waitsFor);
+
+    expect(await element.all(by.css('#popupmenu-1 li')).count()).toEqual(6);
+  });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visually regress', async () => {
+      const pagerToolbar = await element(by.css('.pager-toolbar'));
+
+      expect(await browser.protractorImageComparison.checkElement(pagerToolbar, 'pager-listdetail-pagesize-closed')).toEqual(0);
+
+      await element(by.css('.pager-toolbar li.pager-pagesize button')).click();
+      await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(await element(by.css('#popupmenu-1'))), config.waitsFor);
+
+      expect(await browser.protractorImageComparison.checkElement(pagerToolbar, 'pager-listdetail-pagesize-open')).toEqual(0);
+    });
+  }
+});
