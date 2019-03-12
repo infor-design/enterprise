1072352b4096078ce2b2c61bd444c29e061dde4d
Ed Coyle <Ed.Coyle@infor.com>
2019-03-12 13:21:55 -0400

    Removed the `font` query parameter from the demoapp

--- app/src/js/middleware/option-handler.js
+++ app/src/js/middleware/option-handler.js
@@ -50,15 +50,6 @@ module.exports = function (app, defaults) {
       logger('info', 'Using the minified version of "sohoxi.js"');
     }
 
-    if ((req.query.font && req.query.font.length > 0) || res.opts.theme === 'uplift-alpha') {
-      res.opts.font = req.query.font;
-      logger('info', `Using the ${req.query.font} font`);
-    }
-
-    if (res.opts.theme === 'uplift-alpha') {
-      res.opts.font = true;
-    }
-
     let useLiveReload = false;
     process.argv.forEach((val) => {
       if (val === '--livereload') {
