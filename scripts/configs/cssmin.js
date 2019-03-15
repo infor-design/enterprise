6f9478aad0b68c3a416fb796bfa60f7ae6aba942
Nick Wynja <nick@nickwynja.com>
2019-03-15 11:36:48 -0400

    Renames uplift theme and updates references

--- scripts/configs/cssmin.js
+++ scripts/configs/cssmin.js
@@ -10,7 +10,7 @@ module.exports = {
         'dist/css/high-contrast-theme.min.css': ['dist/css/high-contrast-theme.css'],
         'dist/css/dark-theme.min.css': ['dist/css/dark-theme.css'],
         'dist/css/light-theme.min.css': ['dist/css/light-theme.css'],
-        'dist/css/uplift-alpha-theme.min.css': ['dist/css/uplift-alpha-theme.css'],
+        'dist/css/uplift-theme.min.css': ['dist/css/uplift-theme.css'],
       }
     },
   }
