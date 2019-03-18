b0b214edf85e1a16f464d4f2b1355c644944eb1a
Tim McConechy <tim.mcconechy@infor.com>
2019-03-18 09:50:14 -0400

    More test fixes

--- test/components/datagrid/datagrid.e2e-spec.js
+++ test/components/datagrid/datagrid.e2e-spec.js
@@ -1172,7 +1172,7 @@ describe('Datagrid paging multiselect across pages', () => {
   beforeEach(async () => {
     await utils.setPage('/components/datagrid/test-paging-multiselect-select-across-page');
 
-    const datagridEl = await element(by.id('datagrid'));
+    const datagridEl = await element(by.css('#datagrid .datagrid-body tbody tr:nth-child(2)'));
     await browser.driver
       .wait(protractor.ExpectedConditions.presenceOf(datagridEl), config.waitsFor);
   });
