ea1f2a0787d089c9dca02b3a22ff43ace5956871
Christopher Lepore <chris.lepore@infor.com>
2019-04-02 14:58:45 -0400

    Set layouts with common safe fn

--- app/src/js/custom-route-options.js
+++ app/src/js/custom-route-options.js
@@ -1,6 +1,4 @@
 const extend = require('extend');
-const fs = require('fs');
-const path = require('path');
 const utils = require('./utils');
 
 // Object with settings that gets stringify
@@ -8,19 +6,26 @@ const SohoConfig = {};
 
 // Augments the current set of options for a specific route's needs
 module.exports = function customRouteOptions(req, res) {
-  if (!utils.canChangeLayout(req, res)) {
-    return res.opts;
-  }
-
   const customOpts = {};
   const url = req.originalUrl;
+
+  /**
+   * Set the layout if permissions allow
+   * @param {string} layoutName - The string name of the layout
+   */
+  const setCustomLayout = function(layoutName) {
+    if (utils.canChangeLayout(req, res)) {
+      customOpts.layout = layoutName;
+      customOpts.forceLayout = true;
+    }
+  }
 
   // All patterns will use the "empty" layout
   if (url.match(/patterns\//)) {
     if (url.includes('tree-detail') || url.includes('master-detail')) {
-      customOpts.layout = 'layout';
+      setCustomLayout('layout');
     } else if (!url.endsWith('/list')) {
-      customOpts.layout = 'layout-empty';
+      setCustomLayout('layout-empty');
     }
   }
 
@@ -39,25 +44,25 @@ module.exports = function customRouteOptions(req, res) {
   // Contextual Action Panel
   if (url.match(/components\/contextualactionpanel/)) {
     if (url.match(/partial-/)) {
-      customOpts.layout = 'layout-nofrills'; // No layout for this one on purpose.
+      setCustomLayout('layout-nofrills'); // No layout for this one on purpose).
     }
   }
 
   // Datagrid
   if (url.match(/datagrid-fixed-header/)) {
-    customOpts.layout = 'tests/layout-noscroll';
+    setCustomLayout('tests/layout-noscroll');
   }
 
   // Distribution (AMD)
   if (url.match(/tests\/distribution/)) {
     customOpts.amd = true;
-    customOpts.layout = 'layout-nofrills';
+    setCustomLayout('layout-nofrills');
     customOpts.subtitle = 'AMD Tests';
   }
 
   // Form Compact's List/Detail Example
   if (url.match(/components\/form-compact\/example-list-detail/)) {
-    customOpts.layout = 'layout-empty';
+    setCustomLayout('layout-empty');
   }
 
   // Icons
@@ -67,10 +72,10 @@ module.exports = function customRouteOptions(req, res) {
 
   // Placement Logic
   if (url.match(/place\/test-container-is-body/)) {
-    customOpts.layout = 'components/place/layout-body';
+    setCustomLayout('components/place/layout-body');
   }
   if (url.match(/place\/test-container-is-nested/)) {
-    customOpts.layout = 'components/place/layout-nested';
+    setCustomLayout('components/place/layout-nested');
   }
 
   // RenderLoop
@@ -81,23 +86,18 @@ module.exports = function customRouteOptions(req, res) {
 
   // Searchfield in Headers (needs to load the Header layout)
   if (url.match(/searchfield\/example-header/)) {
-    customOpts.layout = 'components/header/layout';
+    setCustomLayout('components/header/layout');
   }
 
   // Sign-in Dialog
   if (url.match(/tests\/signin/)) {
-    customOpts.layout = 'tests/layout-noheader';
+    setCustomLayout('tests/layout-noheader');
   }
 
   // If there have been properties added to SohoConfig,
   // stringify and pass it to the view options
   if (Object.keys(SohoConfig).length) {
     customOpts.SohoConfig = JSON.stringify(SohoConfig);
-  }
-
-  // Set `forceLayout` to true if there has been a layout defined.
-  if (customOpts.layout) {
-    customOpts.forceLayout = true;
   }
 
   return extend({}, res.opts, customOpts);
