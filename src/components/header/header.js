098f146032292d69dcd69c525c23b64d90588905
Nick Wynja <nick@nickwynja.com>
2019-05-15 17:42:00 -0400

    Push theme state to URL when switching from header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -521,6 +521,20 @@ Header.prototype = {
       const themeAttr = link.attr('data-theme');
       if (themeAttr) {
         personalization.setTheme(themeAttr);
+
+        // Update the app url using pushState so theme
+        // persists through a refresh
+        var currentTheme = theme.currentTheme;
+        var urlParams = new URLSearchParams(location.search);
+        var match = currentTheme.id.match(/theme-(\w+)-(\w+)/);
+
+        if (urlParams.has("theme")) urlParams.delete("theme");
+        if (urlParams.has("variant")) urlParams.delete("variant");
+        urlParams.append("theme", match[1]);
+        urlParams.append("variant", match[2]);
+        var params = urlParams.toString();
+
+        history.pushState({ id: 'themeChange' }, 'IDS Enterprise', `${location.origin}${location.pathname}?${params}`);
         return;
       }
 
