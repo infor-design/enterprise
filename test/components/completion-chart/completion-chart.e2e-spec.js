baabd01c4c5c6b3ba191f43ad7a5dc079a56b799
Tim McConechy <tim.mcconechy@infor.com>
2019-05-23 15:58:01 -0400

    Fx some alignment issues in normal and uplift mode

--- test/components/completion-chart/completion-chart.e2e-spec.js
+++ test/components/completion-chart/completion-chart.e2e-spec.js
@@ -5,7 +5,7 @@ requireHelper('rejection');
 
 jasmine.getEnv().addReporter(browserStackErrorReporter);
 
-describe('Completion Chart variations tests', () => {
+fdescribe('Completion Chart variations tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/completion-chart/example-variations?layout=nofrills');
   });
@@ -26,7 +26,7 @@ describe('Completion Chart variations tests', () => {
   }
 });
 
-describe('Completion Chart color tests', () => {
+fdescribe('Completion Chart color tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/completion-chart/example-colors?layout=nofrills');
   });
--- test/components/targeted-achievement/targeted-achievement.e2e-spec.js
+++ test/components/targeted-achievement/targeted-achievement.e2e-spec.js
@@ -5,7 +5,7 @@ requireHelper('rejection');
 
 jasmine.getEnv().addReporter(browserStackErrorReporter);
 
-describe('Targeted Achievement example-index tests', () => {
+fdescribe('Targeted Achievement example-index tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/targeted-achievement/example-index?layout=nofrills');
   });
@@ -26,7 +26,7 @@ describe('Targeted Achievement example-index tests', () => {
   }
 });
 
-describe('Targeted Achievement icons and links tests', () => {
+fdescribe('Targeted Achievement icons and links tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/targeted-achievement/example-links-icons?layout=nofrills');
   });
