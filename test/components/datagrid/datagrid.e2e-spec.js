ab80c09220f69bab26cc90734781f4d9c646e813
Tim McConechy <tim.mcconechy@infor.com>
2019-05-30 09:21:04 -0400

    Fix tooltip tests

--- test/components/datagrid/datagrid.e2e-spec.js
+++ test/components/datagrid/datagrid.e2e-spec.js
@@ -359,10 +359,10 @@ fdescribe('Datagrid grouping headers and filter tests', () => {
 
   if (utils.isChrome() && utils.isCI()) {
     it('Should not visual regress', async () => {
-      const datagridEl = await element(by.id('datagrid'));
+      const containerEl = await element(by.className('container'));
       await browser.driver.sleep(config.sleep);
 
-      expect(await browser.protractorImageComparison.checkElement(datagridEl, 'datagrid-grouping')).toEqual(0);
+      expect(await browser.protractorImageComparison.checkElement(containerEl, 'datagrid-grouping')).toEqual(0);
     });
   }
 });
