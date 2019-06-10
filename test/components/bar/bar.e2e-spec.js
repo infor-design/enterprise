72249316e937d2d093d67d44e6b7b89d2d2a0d21
tmcconechy <tim.mcconechy@infor.com>
2019-06-10 14:03:58 -0400

    Add new baseline

--- test/components/bar/bar.e2e-spec.js
+++ test/components/bar/bar.e2e-spec.js
@@ -98,9 +98,9 @@ describe('Bar Chart example-hide-legend tests', () => {
   });
 });
 
-describe('Bar Chart example-colors', () => {
+fdescribe('Bar Chart example-colors', () => {
   beforeEach(async () => {
-    await utils.setPage('/components/bar/example-colors?layout=nofrill');
+    await utils.setPage('/components/bar/example-colors?layout=nofrills');
   });
 
   it('Should not have errors', async () => {
