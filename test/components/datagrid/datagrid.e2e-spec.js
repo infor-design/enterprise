de09ceaad5ebdfbd5add3563838e9c2c13125591
Tim McConechy <tim.mcconechy@infor.com>
2019-07-09 10:07:03 -0400

    Fix a test failure due to incorrect click logic

--- test/components/datagrid/datagrid.e2e-spec.js
+++ test/components/datagrid/datagrid.e2e-spec.js
@@ -1324,9 +1324,11 @@ describe('Datagrid filter lookup custom click function tests', () => {
     await lookup.click();
     await browser.driver.sleep(config.sleep);
     await element.all(by.cssContainingText('#lookup-datagrid td', 'I Love Compressors')).first().click();
-    await element(by.css('#test-filter-lookup-click-function-datagrid-1-header-filter-1')).click();
+    await browser.driver.sleep(config.sleep);
 
     expect(await element(by.css('#test-filter-lookup-click-function-datagrid-1-header-2 input')).getAttribute('value')).toEqual('I Love Compressors');
+    await browser.driver.sleep(config.sleep);
+    await element(by.css('#test-filter-lookup-click-function-datagrid-1-header-filter-1')).click();
     if (utils.isChrome() && utils.isCI()) {
       const containerEl = await element(by.className('container'));
 
