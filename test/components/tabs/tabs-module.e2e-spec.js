b2de05bc42f80587d180b239c9437edd721cd31d
Tim McConechy <tim.mcconechy@infor.com>
2019-08-01 15:36:11 -0400

    Remove unused variables

--- test/components/tabs/tabs-module.e2e-spec.js
+++ test/components/tabs/tabs-module.e2e-spec.js
@@ -2,19 +2,8 @@ const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter
 const utils = requireHelper('e2e-utils');
 const config = requireHelper('e2e-config');
 requireHelper('rejection');
-const axePageObjects = requireHelper('axe-page-objects');
 
 jasmine.getEnv().addReporter(browserStackErrorReporter);
-
-const clickTabTest = async (index) => {
-  const tabElTrigger = await element.all(by.className('tab')).get(index);
-  await tabElTrigger.click();
-  await browser.driver
-    .wait(protractor.ExpectedConditions.visibilityOf(element.all(by.className('tab-panel')).get(index)), config.waitsFor);
-
-  expect(await element.all(by.className('tab-panel')).get(index).getAttribute('class')).toContain('can-show');
-  expect(await element.all(by.className('tab')).get(index).getAttribute('class')).toContain('is-selected');
-};
 
 fdescribe('Tabs Module Toolbar tests', () => { //eslint-disable-line
   beforeEach(async () => {
