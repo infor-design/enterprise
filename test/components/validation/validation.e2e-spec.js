56e3526012e25e87303407caf5553908a02fda5b
Tim McConechy <tim.mcconechy@infor.com>
2019-09-30 11:18:17 -0400

    Add more fixes

--- test/components/validation/validation.e2e-spec.js
+++ test/components/validation/validation.e2e-spec.js
@@ -506,21 +506,24 @@ describe('Validation mask tests', () => {
     const timeField = await element(by.id('time-input'));
 
     await timeField.sendKeys(protractor.Key.TAB);
-    await browser.driver.sleep(config.sleepShort);
+    await browser.driver.sleep(config.sleep);
 
     expect(await element(by.css('.error-message')).isPresent()).toBe(false);
     await timeField.clear();
     await timeField.sendKeys('12:01 AM');
     await timeField.sendKeys(protractor.Key.TAB);
-    await browser.driver.sleep(config.sleepShort);
+    await browser.driver.sleep(config.sleep);
 
     expect(await element(by.css('.error-message')).isPresent()).toBe(false);
     await timeField.clear();
     await timeField.sendKeys('99:99');
     await timeField.sendKeys(protractor.Key.TAB);
-    await browser.driver.sleep(config.sleepShort);
+    await browser.driver.sleep(config.sleep);
 
     expect(await element(by.css('.error-message')).isPresent()).toBe(true);
+    const messageList = element.all(by.css('.message-text'));
+
+    expect(await messageList.get(0).getText()).toBe('Invalid Time');
   });
 
   it('Should be able to validate 24 hour time', async () => {
