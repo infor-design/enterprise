ec25cfd553aace266a7f47569e320edca7c444e6
Tim McConechy <tim.mcconechy@infor.com>
2019-10-15 17:12:09 -0400

    Add better test fixes and lint fixes

--- test/components/locale/locale.func-spec.js
+++ test/components/locale/locale.func-spec.js
@@ -1449,7 +1449,7 @@ describe('Locale API', () => {
     });
   });
 
-  fit('Should be able get translations in a non current locale', (done) => {
+  it('Should be able get translations in a non current locale', (done) => {
     Locale.set('fi-FI');
     Locale.setLanguage('sv');
     Locale.getLocale('de-DE');
