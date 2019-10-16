ee5f60a687291ea042cf5899fcc162e040c3bf5f
Ed Coyle <Ed.Coyle@infor.com>
2019-10-16 15:30:33 -0400

    Separate ESLint rules into environment-specific configs

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,68 +1,26 @@
-// ======================================================
-// NOTE: Depends on airbnb's basic eslint settings, which need to be installed
-// via npm install. To install specific versions and update the package.json
-// se
-// some pretty specific commands per your environment.
-// See: https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb
-// ======================================================
-
+/**
+ * Source Code Lint Rules
+ * NOTE: Remember that this cascades on top of the rules from the project root folder.
+ */
 module.exports = {
-  // Only import some air bnb rules
-  // https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb-base/rules
-  'extends': [
-    'airbnb-base',
-    'plugin:jasmine/recommended',
-    'plugin:jasmine-jquery/recommended'
-  ],
-  'parserOptions': {
-    'ecmaVersion': 8,
-    'sourceType': 'module'
-  },
   'plugins': [
-    'jasmine',
-    'jasmine-jquery',
     'compat'
   ],
+
   'env': {
     'browser': true,
-    'jquery': true,
-    'node': true,
-    'jasmine': true,
-    'protractor': true
+    'jquery': true
   },
+
+  'globals': {
+    'd3': true,
+    'document': true,
+    'window': true
+  },
+
   'rules': {
     // Browser compatibility
-    "compat/compat": "error",
-
-    // require function expressions to have a name
-    // https://eslint.org/docs/rules/func-names
-    'func-names': 'off',
-
-    // require trailing commas in multiline object literals
-    'comma-dangle': ['off', {
-      'arrays': 'never',
-      'objects': 'never',
-      'imports': 'never',
-      'exports': 'never',
-      'functions': 'never'
-    }],
-
-    // enforce line breaks between braces
-    // https://eslint.org/docs/rules/object-curly-newline
-    'object-curly-newline': ['error', {
-      ObjectExpression: { minProperties: 8, multiline: true, consistent: true },
-      ObjectPattern: { minProperties: 8, multiline: true, consistent: true }
-    }],
-
-    // disallow use of unary operators, ++ and --
-    // https://eslint.org/docs/rules/no-plusplus
-    'no-plusplus': 'off',
-
-    // disallow multiple empty lines and only one newline at the end
-    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
-
-    // set "no-trailing-spaces" to only a warning (this shouldn't fail builds)
-    'no-trailing-spaces': ['warn'],
+    'compat/compat': 'error',
 
     // ensure JSDoc comments are valid
     // https://eslint.org/docs/rules/valid-jsdoc
@@ -76,59 +34,15 @@ module.exports = {
       'requireReturn': false
     }],
 
-    // disallow use of chained assignment expressions
-    // https://eslint.org/docs/rules/no-multi-assign
-    'no-multi-assign': ['warn'],
-
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    // rule: https://eslint.org/docs/rules/no-param-reassign.html
-    'no-param-reassign': ['off', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'acc', // for reduce accumulators
-        'e', // for e.returnvalue
-        'ctx', // for Koa routing
-        'req', // for Express requests
-        'request', // for Express requests
-        'res', // for Express responses
-        'response', // for Express responses
-        '$scope', // for Angular 1 scopes
-      ]
-    }],
-
-    // Prefer destructuring from arrays and objects
-    // https://eslint.org/docs/rules/prefer-destructuring
-    'prefer-destructuring': ['off', {
-      VariableDeclarator: {
-        array: false,
-        object: true,
-      },
-      AssignmentExpression: {
-        array: true,
-        object: true,
-      },
-    }, {
-      enforceForRenamedProperties: false,
-    }],
-
-    // Allow warns for deprecation messages
-    // https://eslint.org/docs/rules/no-console
-    'no-console': ['warn', { allow: ['warn'] }],
-
-    // don't enforce arrow functions
-    // https://eslint.org/docs/rules/prefer-arrow-callback
-    'prefer-arrow-callback': ['warn', {}],
-
-    // don't enforce disallowing of mixed operators.
+    // Don't enforce disallowing of mixed operators.
     // We use lots of algebra for positioning/etc that gets flagged by this.
     // https://eslint.org/docs/rules/no-mixed-operators
     'no-mixed-operators': ['off'],
 
-    // Require modules with a single export to use a default export
-    // Will ignore because of https://medium.com/@timoxley/named-exports-as-the-default-export-api-670b1b554f65
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
-    'import/prefer-default-export': 'off',
+    // Don't allow paramter reassignment
+    'no-param-reassign': ['off', {
+      props: true,
+    }],
 
     // Ignore certain globals:
     // - IsNaN - due to our previous assumptions in code about how it sometimes will return 'false', and because `Number.isNaN` has no native IE11 support.
@@ -136,47 +50,10 @@ module.exports = {
     // https://eslint.org/docs/rules/no-restricted-globals
     'no-restricted-globals': ['off', 'isNaN'],
 
-    // Ignore usage of certain properties:
-    // - Math.pow (not really sure why AirBNB doesn't allow this)
-    //
-    'no-restricted-properties': ['off', {
-      'object': 'Math',
-      'property': 'pow'
-    }],
-
     // Ignore errors for continue statements, currently used in:
     // - Mask
     // - Validation
     // https://eslint.org/docs/rules/no-continue
-    'no-continue': ['off'],
-
-    // Forbid the use of extraneous packages
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
-    // paths are treated both as absolute paths, and relative to process.cwd()
-    'import/no-extraneous-dependencies': ['off'],
-
-    // specify the maximum length of a line in your program
-    // https://eslint.org/docs/rules/max-len
-    'max-len': ['error', {
-      code: 100,
-      comments: 350,
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-    }],
-    // Disallow use of disabled tests (Jasmine)
-    'jasmine/no-disabled-tests': ['off'],
-    // Allow same name in different describes
-    "jasmine/no-spec-dupes": [1, "branch"],
-    "jasmine/no-suite-dupes": [1, "branch"],
-    // Allow toHaveBeenCalledWith
-    "jasmine/prefer-toHaveBeenCalledWith": ['off']
-  },
-  'globals': {
-    'd3': false,
-    'spyOnEvent': true,
-    'requireHelper': true
+    'no-continue': ['off']
   }
 };
