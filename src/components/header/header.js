ee072eab762f28a54d1cd5cd116f6d832862b7f9
Ed Coyle <Ed.Coyle@infor.com>
2019-10-17 14:45:12 -0400

    Fix an iOS bug in Popupmenu where the theme could not be switched.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -522,8 +522,10 @@ Header.prototype = {
       const themeNameAttr = link.attr('data-theme-name');
       const themeVariantAttr = link.attr('data-theme-variant');
       if (themeNameAttr || themeVariantAttr) {
-        const name = changer.next().find('.is-checked a[data-theme-name]').attr('data-theme-name');
-        const variant = changer.next().find('.is-checked a[data-theme-variant]').attr('data-theme-variant');
+        const api = changer.data('popupmenu');
+        const menu = api.menu;
+        const name = menu.find('.is-checked a[data-theme-name]').attr('data-theme-name');
+        const variant = menu.find('.is-checked a[data-theme-variant]').attr('data-theme-variant');
         if (name && variant) {
           personalization.setTheme(`${name}-${variant}`);
         }
