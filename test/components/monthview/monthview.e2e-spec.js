3deb869cd6f724a9f6d5e5dcf6f110d14b85656d
Tim McConechy <tim.mcconechy@infor.com>
2019-12-03 14:51:52 -0500

    One more fix

--- test/components/monthview/monthview.e2e-spec.js
+++ test/components/monthview/monthview.e2e-spec.js
@@ -295,7 +295,7 @@ describe('MonthView specific language tests', () => {
   });
 
   it('Should render a specific locale and language', async () => {
-    await browser.driver.sleep(config.sleep);
+    await browser.driver.sleep(config.sleepLonger);
 
     expect(await element(by.id('monthview-datepicker-field')).getText()).toEqual('oktober 2019');
     expect(await element(by.css('.hyperlink.today')).getText()).toEqual('Heute');
