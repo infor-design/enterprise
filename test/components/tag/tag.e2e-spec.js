bf43f52d039529d5bf78fbb6d74d752490e62185
Eric Angeles <enrico.angeles@infor.com>
2020-01-02 19:40:25 +0800

    Added linkable tag style

---
+++ test/components/tag/tag.e2e-spec.js
@@ -0,0 +1,28 @@
+import { element, browser } from 'protractor';
+
+const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const utils = requireHelper('e2e-utils');
+const config = requireHelper('e2e-config');
+requireHelper('rejection');
+
+jasmine.getEnv().addReporter(browserStackErrorReporter);
+
+describe('Tag example-linkable tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/tag/example-linkable');
+    const tagEl = await element(by.id('linkable-tags'));
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(tagEl), config.waitsFor);
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  it('Should display caret right icon correctly', async () => {
+    await browser.driver
+      .wait(protractor.ExpectedConditions.visibilityOf(await element.all(by.css('.tag-list .is-linkable')).get(0)), config.waitsFor);
+    
+    expect(await element.all(by.css('.tag-list .is-linkable')).get(0).isDisplayed()).toBeTruthy();
+  });
+});
