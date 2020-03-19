22fcead3289619c76fcdb62255e1476dc1c48cf7
tmcconechy <tim.mcconechy@infor.com>
2020-03-19 15:46:44 -0400

    Remove focus since they all worked

--- test/components/editor/editor.e2e-spec.js
+++ test/components/editor/editor.e2e-spec.js
@@ -5,7 +5,7 @@ requireHelper('rejection');
 
 jasmine.getEnv().addReporter(browserStackErrorReporter);
 
-fdescribe('Editor example-index tests', () => {
+describe('Editor example-index tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/example-index?layout=nofrills');
   });
@@ -79,7 +79,7 @@ fdescribe('Editor example-index tests', () => {
   });
 });
 
-fdescribe('Editor visual regression tests', () => {
+describe('Editor visual regression tests', () => {
   if (utils.isChrome() && utils.isCI()) {
     it('Should not visually regress on subtle light', async () => {
       await utils.setPage('/components/editor/example-index?theme=soho&variant=light&layout=nofrills');
@@ -131,7 +131,7 @@ fdescribe('Editor visual regression tests', () => {
   }
 });
 
-fdescribe('Editor preview mode tests', () => {
+describe('Editor preview mode tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/example-preview?layout=nofrills');
   });
@@ -156,7 +156,7 @@ fdescribe('Editor preview mode tests', () => {
   });
 });
 
-fdescribe('Editor dirty tracking tests', () => {
+describe('Editor dirty tracking tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/example-dirty-tracking?layout=nofrills');
   });
@@ -192,7 +192,7 @@ fdescribe('Editor dirty tracking tests', () => {
   });
 });
 
-fdescribe('Editor empty tests', () => {
+describe('Editor empty tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/test-empty?layout=nofrills');
   });
@@ -211,7 +211,7 @@ fdescribe('Editor empty tests', () => {
   });
 });
 
-fdescribe('Editor placeholder tests', () => {
+describe('Editor placeholder tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/test-placeholder?layout=nofrills');
   });
@@ -229,7 +229,7 @@ fdescribe('Editor placeholder tests', () => {
   }
 });
 
-fdescribe('Editor modal tests', () => {
+describe('Editor modal tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/editor/test-modal?layout=nofrills');
   });
