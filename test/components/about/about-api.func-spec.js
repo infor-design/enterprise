1d0be03be226849d938312730a840aada25123c6
Ed Coyle <Ed.Coyle@infor.com>
2020-04-13 12:10:30 -0400

    handle lifecycle better in About API tests

--- test/components/about/about-api.func-spec.js
+++ test/components/about/about-api.func-spec.js
@@ -1,36 +1,26 @@
-import { About } from '../../../src/components/about/about';
 import { cleanup } from '../../helpers/func-utils';
 
 const aboutHTML = require('../../../app/views/components/about/example-index.html');
 const svg = require('../../../src/components/icons/svg.html');
 
-let aboutEl;
 let aboutObj;
 
 describe('About API', () => {
   const Locale = window.Soho.Locale;
 
   beforeEach(() => {
-    aboutEl = null;
     aboutObj = null;
     document.body.insertAdjacentHTML('afterbegin', svg);
     document.body.insertAdjacentHTML('afterbegin', aboutHTML);
-    aboutEl = document.body.querySelector('.about');
 
     Locale.addCulture('en-US', Soho.Locale.cultures['en-US'], Soho.Locale.languages['en']); //eslint-disable-line
     Locale.set('en-US');
-
-    aboutObj = new About(aboutEl);
   });
 
   afterEach((done) => {
     Locale.set('en-US');
     if (aboutObj) {
       aboutObj.destroy();
-    }
-    const modalAPI = $('body').data('modal');
-    if (modalAPI && typeof modalAPI.destroy === 'function') {
-      modalAPI.destroy();
     }
 
     cleanup([
@@ -41,7 +31,7 @@ describe('About API', () => {
     ]);
     setTimeout(() => {
       done();
-    }, 500);
+    }, 650);
   });
 
   it('Should show on page via API', (done) => {
@@ -51,6 +41,7 @@ describe('About API', () => {
       version: 'ver. {{version}}',
       content: '<p>Fashionable components for fashionable applications.</p>'
     });
+    aboutObj = $('body').data('about');
 
     setTimeout(() => {
       expect(document.body.querySelector('#about-modal').classList.contains('is-visible')).toBeTruthy();
@@ -65,6 +56,7 @@ describe('About API', () => {
       version: 'ver. {{version}}',
       content: '<p>Fashionable components for fashionable applications.</p>'
     });
+    aboutObj = $('body').data('about');
 
     const spyEvent = spyOnEvent('body', 'close');
 
