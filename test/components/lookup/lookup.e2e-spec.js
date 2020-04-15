a2b340070a73149db70b6b6392f38d76d92c2d2d
Eric Angeles <enrico.angeles@infor.com>
2020-04-15 20:23:04 +0800

    final fix

--- test/components/lookup/lookup.e2e-spec.js
+++ test/components/lookup/lookup.e2e-spec.js
@@ -474,7 +474,7 @@ describe('Lookup minWidth tests', () => {
   });
 
   if (utils.isChrome() && utils.isCI()) {
-    fit('Should not visual regress', async () => {
+    it('Should not visual regress', async () => {
       const buttonEl = await element.all(by.className('trigger')).last();
       await buttonEl.click();
 
