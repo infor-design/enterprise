2caacc54e87fe585b5c1328e14da97b1f149ab41
Eric Angeles <enrico.angeles@infor.com>
2020-04-29 00:09:53 +0800

    Add e2e spec

---
+++ test/components/error-page/error-page.e2e-spec.js
@@ -0,0 +1,27 @@
+const { browserStackErrorReporter } = requireHelper('browserstack-error-reporter');
+const utils = requireHelper('e2e-utils');
+const config = requireHelper('e2e-config');
+requireHelper('rejection');
+
+jasmine.getEnv().addReporter(browserStackErrorReporter);
+
+describe('Error page example-index tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/error-page/example-index?layout=nofrills');
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visual regress on example-index', async () => {
+      const mainEl = await Element(by.css('div[role=main]'));
+      await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(mainEl), config.waitsFor);
+      await browser.driver.sleep(config.sleep);
+
+      expect(await browser.protractorImageComparison.checkScreen('error-page')).toEqual(0);
+    });
+  }
+});
