73558b28502a3b4e37d9160ff6730af058bb9552
Tim McConechy <tim.mcconechy@infor.com>
2020-05-28 10:57:25 -0400

    Changed result text to lower case

--- src/components/message/message.js
+++ src/components/message/message.js
@@ -14,7 +14,7 @@ const COMPONENT_NAME = 'message';
  * @class Message
  * @param {object} element The component element.
  * @param {object} [settings] The component settings.
- * @param {string} [settings.title='Message Title']  Title text or content shown in the message
+ * @param {string} [settings.title='Message Title']  Title text or content shown in the message. An HTML string containing the follow tags may also be used `<div><span><a><small><img><svg><i><b><use><br><strong><em>`.
  * @param {string} [settings.status='']  Pass a status to style icon and title color ('error', 'alert', 'success')
  * @param {string} [settings.message='Message Summary']  The message content or text
  * @param {number} [settings.width='auto']  Pass a specific with or defaults to auto
