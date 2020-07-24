c22c1ceb23e92c186ea28800f2a7e2e00cb59abd
John Macaluso <John.Macaluso@infor.com>
2020-07-24 12:04:56 -0400

    Add more descriptive comment

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -8303,7 +8303,7 @@ Datagrid.prototype = {
       const key = e.which || e.keyCode || e.charCode || 0;
       let handled = false;
 
-      // Trigger the dropdown on first keydown
+      // Make sure the first keydown gets captured and Trigger the dropdown
       setTimeout(() => {
         self.activeCell.node.find('select.dropdown').each(function () {
           const dropdown = $(this);
