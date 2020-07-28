b857b490e110e3975e4347efdd98c1769a81c668
Deepak Verma <deepak.verma@infor.com>
2020-07-28 14:56:17 -0400

    Fix overlapping icons with popupmenu

--- src/components/popupmenu/popupmenu.js
+++ src/components/popupmenu/popupmenu.js
@@ -2168,7 +2168,7 @@ PopupMenu.prototype = {
     const returnObj = [anchor];
 
     // If the entire menu is "selectable", place the checkmark where it's supposed to go.
-    if (parent.hasClass('is-selectable') || singleMenu || singleSection) {
+    if ((parent.hasClass('is-selectable') && !anchor.find('.icon').length) || singleMenu || singleSection) {
       parent.prevUntil('.heading, .separator').add(parent.nextUntil('.heading, .separator')).removeClass('is-checked');
       parent.addClass('is-checked');
       returnObj.push('selected');
