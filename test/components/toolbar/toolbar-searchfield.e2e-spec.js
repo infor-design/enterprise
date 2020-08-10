cf94c281c2610a834dfefe1c048a69568229728b
Eric Angeles <enrico.angeles@infor.com>
2020-08-10 18:57:12 +0800

    Add visual regress

--- test/components/toolbar/toolbar-searchfield.e2e-spec.js
+++ test/components/toolbar/toolbar-searchfield.e2e-spec.js
@@ -23,3 +23,28 @@ describe('Toolbar Searchfield (no-reinvoke)', () => {
     await utils.checkForErrors();
   });
 });
+
+fdescribe('Searchfield with Toolbar alignment tests', () => {
+  beforeEach(async () => {
+    await utils.setPage('/components/toolbarsearchfield/example-flex-toolbar-align-with-searchfield?layout=nofrills');
+  });
+
+  it('Should not have errors', async () => {
+    await utils.checkForErrors();
+  });
+
+  if (utils.isChrome() && utils.isCI()) {
+    it('Should not visual regress on example-flex-toolbar-align-with-searchfield', async () => {
+      const searchfieldInput = element(by.id('toolbar-searchfield-01'));
+      const searchfieldToolbarContainer = await element(by.id('maincontent'));
+      await searchfieldInput.click();
+
+      await browser.driver
+        .wait(protractor.ExpectedConditions
+          .presenceOf(element(by.id('maincontent'))), config.waitsFor);
+      await browser.driver.sleep(config.sleep);
+
+      expect(await browser.imageComparison.checkElement(searchfieldToolbarContainer, 'searchfield-toolbar-alignment')).toEqual(0);
+    });
+  }
+});
