93ac51d53b29d65ac69d3d7a3ad0bde587b8531d
Tim McConechy <tim.mcconechy@infor.com>
2020-09-28 10:11:49 -0400

    Update examples

--- scripts/after-release.sh
+++ scripts/after-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ./scripts/after-release.sh latest 4.12.1
-# ./scripts/after-release.sh beta 4.13.0-beta.0
+# ./scripts/after-release.sh latest 4.32.0
+# ./scripts/after-release.sh beta 4.33.0-beta.0
 
 RELEASE_TAG=$1
 RELEASE_VERSION=$2
