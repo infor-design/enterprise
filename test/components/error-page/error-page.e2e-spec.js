110f59afc2a010d024ca0ed54d51c525f1df2af4
Eric Angeles <enrico.angeles@infor.com>
2020-10-20 19:33:00 +0800

    Add e2e for error-page

--- test/components/error-page/error-page.e2e-spec.js
+++ test/components/error-page/error-page.e2e-spec.js
@@ -24,4 +24,14 @@ describe('Error page example-index tests', () => {
       expect(await browser.imageComparison.checkScreen('error-page')).toEqual(0);
     });
   }
+
+  fit('Should be able to set id/automations', async () => {
+    await browser.driver.sleep(config.sleep);
+
+    expect(await element(by.id('error-page-content-id-1')).getAttribute('id')).toEqual('error-page-content-id-1');
+    expect(await element(by.id('error-page-content-id-1')).getAttribute('data-automation-id')).toEqual('automation-id-errorpage-1');
+
+    expect(await element(by.id('error-page-btn-id-1')).getAttribute('id')).toEqual('error-page-btn-id-1');
+    expect(await element(by.id('error-page-btn-id-1')).getAttribute('data-automation-id')).toEqual('automation-id-errorpage-btn-1');
+  });
 });
