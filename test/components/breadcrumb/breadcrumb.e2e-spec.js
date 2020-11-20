517ae04dcc45e7d3c94be3a91a798f0954b5c07a
Ed Coyle <Ed.Coyle@infor.com>
2020-11-20 14:51:02 -0500

    remove `fit()`

--- test/components/breadcrumb/breadcrumb.e2e-spec.js
+++ test/components/breadcrumb/breadcrumb.e2e-spec.js
@@ -59,7 +59,7 @@ describe('Breadcrumb navigation alternate tests', () => {
   });
 
   if (utils.isChrome() && utils.isCI()) {
-    fit('Should not visual regress', async () => {
+    it('Should not visual regress', async () => {
       const containerEl = await element(by.css('div[role=main]'));
       await browser.driver
         .wait(protractor.ExpectedConditions.presenceOf(containerEl), config.waitsFor);
