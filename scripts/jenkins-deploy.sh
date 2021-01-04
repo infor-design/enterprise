327b2fa5fdcf86b1d006449a81d5ed7db63741be
Tim McConechy <tim.mcconechy@infor.com>
2021-01-04 13:16:02 -0500

    Cleanup script

--- scripts/jenkins-deploy.sh
+++ scripts/jenkins-deploy.sh
@@ -7,8 +7,6 @@ JENKINS_URL="http://$JENKINS_USER:$JENKINS_API_TOKEN@$JENKINS_DOMAIN"
 
 BUILD_FROM="master"
 BUILD_AS_LATEST=false
-WATCH_FOR_BUILD_STATUS=false
-QUEUE_BUILD=false
 
 while getopts "b:lwq" opt; do
     case $opt in
@@ -19,14 +17,6 @@ while getopts "b:lwq" opt; do
         l)
             # latest? publishes to `latest-enterprise` demo server
             BUILD_AS_LATEST=true
-            ;;
-        q)
-            # Add to queue instead of cancelling existing build
-            QUEUE_BUILD=true
-            ;;
-        w)
-            # watch build status to break build
-            WATCH_FOR_BUILD_STATUS=true
             ;;
         *)
             exit 1
@@ -48,12 +38,6 @@ _check_credentials() {
         echo $(curl -s $JENKINS_URL/job/$JENKINS_JOB/lastBuild/api/json)
         exit 1
     fi
-}
-
-check_status () {
-    job_status=$(curl -s $JENKINS_URL/job/$JENKINS_JOB/lastBuild/api/json | \
-        python -c "import sys, json; print json.load(sys.stdin)['result']")
-    echo $job_status
 }
 
 get_build_number () {
@@ -88,26 +72,7 @@ _check_credentials
 build_number_url=$(echo $BUILD_FROM | sed -e 's/\.//g')
 
 echo "Building $BUILD_FROM as $([ "$BUILD_AS_LATEST" = true ] && echo 'latest-enterprise' || echo $build_number_url-enterprise)..."
-
-CURRENT_JOB_STATUS=`check_status`
-
-if [ "$CURRENT_JOB_STATUS" = "None" ]; then
-    if [ "$QUEUE_BUILD" = false ]; then
-        CURRENT_BUILD_NUMBER=`get_build_number`
-        echo "Job #$CURRENT_BUILD_NUMBER is already running. Aborting that job now..."
-        RESP=`stop_jenkins_build`
-        if [ "$RESP" = "302" ]; then
-            echo "Successfully aborted job #$CURRENT_BUILD_NUMBER"
-        else
-            exit 1
-            echo "ERROR: Request to Jenkins returned $RESP"
-        fi
-    else
-        echo "- adding Jenkins build to queue..."
-    fi
-else
-    echo "- adding Jenkins build to queue..."
-fi
+echo "- adding Jenkins build to queue..."
 
 RESP=`queue_jenkins_build`
 
@@ -116,39 +81,4 @@ if [ "$RESP" = "201" ]; then
 else
     exit 1
     echo "ERROR: Request to Jenkins returned $RESP"
-fi
-
-if [ "$WATCH_FOR_BUILD_STATUS" = true ]; then
-    INITIAL_STATUS=`check_status`
-    if [ -n "$INITIAL_STATUS" ]; then
-        BUILD_STATUS=`check_status`
-        CURRENT_BUILD_NUMBER=`get_build_number`
-        echo -n "Watching build #$CURRENT_BUILD_NUMBER to report on status..."
-        while [ "$BUILD_STATUS" = "None" ]; do
-            sleep 10
-            printf "."
-            BUILD_STATUS=`check_status`
-        done
-        if [ "$BUILD_STATUS" = "SUCCESS" ]; then
-            echo "" # new line
-            echo "DEPLOY to http://$build_number_url-enterprise.demo.design.infor.com SUCCESSFUL."
-            exit
-        elif [ "$BUILD_STATUS" = "ABORTED" ]; then
-            echo "" # new line
-            echo "Build was $BUILD_STATUS. Exiting with 0 since this was likely intentional."
-            exit 0
-        else
-            echo "" # new line
-            echo "$BUILD_STATUS! See log for details:"
-            echo "" # new line
-            while IFS='\n' read -r line; do
-                printf "\t%s\n" "$line"
-            done
-            echo "Build was ended with status: $BUILD_STATUS!"
-            exit 1
-        fi
-    else
-      echo "BUILD FAILURE: Other build is unsuccessful or status could not be obtained."
-      exit 1
-    fi
 fi
