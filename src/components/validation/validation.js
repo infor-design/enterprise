be80ba4f619d3aedb48e7173090e08bc8307893a
Daniel Ortiz <dortiz@inforadmins-MBP.lan1>
2021-03-11 17:00:24 -0500

    439 - update to line 110 to this.message = Locale.translate(dateTimeClass ? 'InvalidDateTime' : 'InvalidDate'); - issue 4888

--- src/components/validation/validation.js
+++ src/components/validation/validation.js
@@ -107,7 +107,7 @@ function ValidationRules() {
       check(value, field, gridInfo) {
         const dateTimeClass = field[0].classList.contains('datetime');
 
-        dateTimeClass ? this.message = Locale.translate('InvalidDateTime') : this.message = Locale.translate('InvalidDate');
+        this.message = Locale.translate(dateTimeClass ? 'InvalidDateTime' : 'InvalidDate');
 
         if (gridInfo && gridInfo.column) {
           const gridValue = gridInfo.column.formatter(gridInfo.row, gridInfo.cell, value, gridInfo.column, true);
