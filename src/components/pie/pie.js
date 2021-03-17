cb463cc9aa87a0178a35a2a057537e28e02f6723
Daniel Ortiz <dortiz@inforadmins-MacBook-Pro.local>
2021-03-17 14:20:59 -0400

    #3144 - removed console.log()

--- src/components/pie/pie.js
+++ src/components/pie/pie.js
@@ -11,7 +11,6 @@ import '../emptymessage/emptymessage.jquery';
 
 // Settings and Options
 const COMPONENT_NAME = 'pie';
-console.log('pie.js')
 /**
  * A pie chart (or a circle chart) is a circular statistical graphic which is divided
   into slices to illustrate numerical proportion. In a pie chart, the arc length of each slice is proportional to the quantity it represents.
