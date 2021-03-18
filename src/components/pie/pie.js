ca67d9c67bab4d620894689e27a21f35bb677752
Daniel Ortiz <dortiz@inforadmins-MBP.lan1>
2021-03-18 10:56:00 -0400

    3144 - Created destory method to reset class names based on the given chart

--- src/components/pie/pie.js
+++ src/components/pie/pie.js
@@ -11,6 +11,7 @@ import '../emptymessage/emptymessage.jquery';
 
 // Settings and Options
 const COMPONENT_NAME = 'pie';
+
 /**
  * A pie chart (or a circle chart) is a circular statistical graphic which is divided
   into slices to illustrate numerical proportion. In a pie chart, the arc length of each slice is proportional to the quantity it represents.
