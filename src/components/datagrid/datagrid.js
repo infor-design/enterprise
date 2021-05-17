46306482340716a7b8cd9467078388e453329f9e
Tim McConechy <tim.mcconechy@infor.com>
2021-05-17 13:49:52 -0400

    Add alternate fix

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -1608,24 +1608,13 @@ Datagrid.prototype = {
     let activeMenu = null;
 
     // Attach Keyboard support
-    this.element.off('click.datagrid-filter').on('click.datagrid-filter', '.btn-filter', function (event) {
+    this.element.off('click.datagrid-filter').on('click.datagrid-filter', '.btn-filter', function () {
       const filterBtn = $(this);
       const popupOpts = { trigger: 'immediate', offset: { y: 15 }, placementOpts: { strategies: ['flip', 'nudge'] } };
       const popupmenu = filterBtn.data('popupmenu');
-      const parentElem = $(event.target).parent();
-      const popupmenuElem = parentElem.find('.popupmenu');
 
       if (popupmenu) {
         popupmenu.close(true, true);
-
-        // If the menu is missing in the Datagrid filter wrapper,
-        // it will re-attach to the parent in the correct position.
-        if (!popupmenuElem.length) {
-          if (popupmenu.wrapperPlace) {
-            popupmenu.wrapper.off().remove();
-          }
-          popupmenu.menu.insertAfter(filterBtn);
-        }
       } else {
         filterBtn.off('beforeopen.datagrid-filter').on('beforeopen.datagrid-filter', (e, menu, api) => {
           self.hideTooltip();
@@ -1684,6 +1673,15 @@ Datagrid.prototype = {
 
     this.element.off('focus.datagrid-filter-focus')
       .on('focus.datagrid-filter-focus', '.datagrid-filter-wrapper input', () => {
+        activeMenu?.close();
+        const dropdownId = $('#dropdown-list').attr('data-element-id');
+        const dropdown = $(`#${dropdownId}`).data('dropdown');
+        if (dropdown && dropdown.isOpen()) {
+          dropdown.closeList('click');
+        }
+      });
+    this.element.off('click.datagrid-filter-click')
+      .on('click.datagrid-filter-click', 'th', () => {
         activeMenu?.close();
         const dropdownId = $('#dropdown-list').attr('data-element-id');
         const dropdown = $(`#${dropdownId}`).data('dropdown');
@@ -4827,7 +4825,8 @@ Datagrid.prototype = {
         if (diff < colWidth) {
           this.stretchColumnWidth = colWidth;
         } else {
-          this.stretchColumnDiff = colWidth = diff;
+          this.stretchColumnDiff = colWidth;
+          this.stretchColumnDiff = diff;
         }
       }
 
