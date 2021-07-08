f7ba6d899855356b22023d8bf562ceb588f8f8a2
Deepak Verma <deepak.verma@infor.com>
2021-07-08 15:51:09 -0400

    Update unwanded removed code

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -5697,7 +5697,12 @@ Datagrid.prototype = {
     this.settings.columns[idx].hidden = true;
     this.headerNodeCheckbox = this.headerNodes().eq(idx);
     if (!this.settings?.frozenColumns?.left.length) this.headerNodes().eq(idx).addClass('is-hidden');
-    this.headerNodes().eq(idx).off().remove();
+    if (idx === 0 && id === 'selectionCheckbox') {
+      this.headerNodes().eq(idx).off().remove();
+    } else {
+      this.headerNodes().eq(idx).addClass('is-hidden');
+    }
+
     this.colGroupNodes().eq(idx).addClass('is-hidden');
 
     const frozenLeft = this.settings?.frozenColumns?.left.length || 0;
