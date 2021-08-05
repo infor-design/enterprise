3f0852f41248a4f748ec089b36a856216a17634e
Yohannah Bautista <yohannah.bautista@infor.com>
2021-08-05 14:47:10 +0800

    5472. Added tooltip in fontpicker

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1020,7 +1020,7 @@ Editor.prototype = {
 
       unorderedlist: `<button type="button" class="btn btn-editor" title="${Locale.translate('UnorderedList')}" data-action="insertunorderedlist" data-element="ul">${buttonLabels.unorderedlist}</button>`,
 
-      fontPicker: `<button type="button" class="btn btn-editor fontpicker" data-action="fontStyle" data-init="false"><span>${'FontPicker'}</span></button>`,
+      fontPicker: `<button type="button" class="btn btn-editor fontpicker" title="${Locale.translate('ChooseFont')}" data-action="fontStyle" data-init="false"><span>${'FontPicker'}</span></button>`,
 
       justifyLeft: `<button type="button" class="btn btn-editor" title="${Locale.translate('JustifyLeft')}" data-action="justifyLeft" >${buttonLabels.justifyLeft}</button>`,
 
