1ee7d11741c2c002d0cb71310439fb0190891ff6
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 14:44:11 +0800

    Test

--- test/components/input/input.puppeteer-spec.js
+++ test/components/input/input.puppeteer-spec.js
@@ -1,15 +1,15 @@
 describe('Input Puppeteer Tests', () => {
     describe('Index Tests', () => {
-      //const url = 'http://localhost:4000/components/input/example-index.html';
-      const puppeteer = require('puppeteer');
+      const url = 'http://localhost:4000/components/input/example-index.html';
+      //const puppeteer = require('puppeteer');
 
       (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('http://localhost:4000/components/input/example-index.html');
+        await page.goto(url);
         await page.screenshot({ path: 'input.png' });
       
-        await jestPuppeteer.debug()
+        await jestPuppeteer.debug();
         await browser.close();
       })();
     
