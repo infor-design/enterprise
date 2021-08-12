1d28159cdd8b18a2bb0d67f7afd4dcdb67c3da26
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 17:50:08 +0800

    test

--- test/components/input/input.puppeteer-spec.js
+++ test/components/input/input.puppeteer-spec.js
@@ -10,16 +10,26 @@ describe('Input Puppeteer Tests', () => {
       it('should show the title', async () => {
         await expect(page.title()).resolves.toMatch('IDS Enterprise');  
         await page.click('input[name="first-name"]');
-        await page.type('input[name="first-name"]', 'Juan Dela Cruz');
+        await page.type('input[name="first-name"]', 'Juan');
 
-        await page.click('input[name="first-name"]');
         await page.click('input[name="last-name"]');
-        let lastNameValidation = await page.$eval('input[name="first-name"]', 
+        await page.type('input[name="last-name"]', 'Dela Cruz');
+        
+
+        await page.click('input[name="email-address"]');
+        await page.type('input[name="email-address"]', 'jaundelacruz@infor.com');
+
+        await page.click('input[name="email-address-ok"]');
+        await page.click('input[name="department-code-trackdirty"]');
+        
+        
+        
+        /*let lastNameValidation = await page.$eval('input[name="first-name"]', 
         (input) => input.lastNameValidation
         );
 
         expect(lastNameValidation.toBe('invalid'));
-        await browser.close();
+        await browser.close();*/
         
         //await page.$eval('input[name="first-name"]', el => el.value = 'Juan Dela Cruz');
        // await page.screenshot({ path: 'input.png' });
