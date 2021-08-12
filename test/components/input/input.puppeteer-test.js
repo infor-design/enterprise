92304c7e1247b75bcaad287b164284146a090c04
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 20:02:01 +0800

    test

--- test/components/input/input.puppeteer-test.js
+++ test/components/input/input.puppeteer-test.js
@@ -34,7 +34,7 @@ describe('Input Puppeteer Tests', () => {
         await page.click('input[name="email-address-ok"]');
         await page.type('input[name="email-address-ok"]', 'jaundelacruz2@infor.com');
         await page.waitForSelector('input.required.error', {
-          visible: false,
+          visible: true,
         })
 
 
