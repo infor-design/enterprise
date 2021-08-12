a8c7ac6ff026851ab2a70d2453b19dba499ca414
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 16:00:54 +0800

    test

--- test/components/input/input.puppeteer-spec.js
+++ test/components/input/input.puppeteer-spec.js
@@ -12,6 +12,14 @@ describe('Input Puppeteer Tests', () => {
         await expect(page.title()).resolves.toMatch('IDS Enterprise');  
         await page.waitForSelector('input[name="first-name"]');   
         await page.$eval('input[name="first-name"]', el => el.value = 'Juan Dela Cruz');
+        await page.screenshot({ path: 'input.png' });
+
+       /* const text = await page.evaluate(() => {
+            const anchor = document.querySelector('#mw-content-text');
+            return anchor.textContent;
+        });
+        console.log(text);
+        await browser.close();*/
 
       });
     
