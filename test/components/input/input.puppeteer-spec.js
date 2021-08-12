069d53cd27881f46fba514addf5b98b4a25cc7a2
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 14:31:53 +0800

    Test Input

---
+++ test/components/input/input.puppeteer-spec.js
@@ -0,0 +1,16 @@
+describe('Input Puppeteer Tests', () => {
+    describe('Index Tests', () => {
+      //const url = 'http://localhost:4000/components/input/example-index.html';
+      const puppeteer = require('puppeteer');
+
+      (async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:4000/components/input/example-index.html');
+        await page.screenshot({ path: 'input.png' });
+      
+        await browser.close();
+      })();
+    
+    });
+});
