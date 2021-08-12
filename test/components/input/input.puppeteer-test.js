93900206cb1c2ab0fea29c768834ae5177ea2c75
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 19:59:16 +0800

    test

--- test/components/input/input.puppeteer-test.js
+++ test/components/input/input.puppeteer-test.js
@@ -10,14 +10,14 @@ describe('Input Puppeteer Tests', () => {
       it('should show the title', async () => {
         await expect(page.title()).resolves.toMatch('IDS Enterprise');  
         await page.click('input[name="first-name"]');
-        await page.keyboard.type('input[name="first-name"]', 'Juan');
+        await page.type('input[name="first-name"]', 'Juan');
 
         await page.click('input[name="last-name"]');
         await page.type('input[name="last-name"]', 'Dela Cruz');
         
 
         await page.click('input[name="email-address"]');
-        await page.keyboard.type('input[name="email-address"]', 'jaundelacruz@infor.com');
+        await page.type('input[name="email-address"]', 'jaundelacruz@infor.com');
 
         await page.click('input[name="email-address-ok"]');
         await page.click('input[name="department-code-trackdirty"]');
@@ -27,11 +27,36 @@ describe('Input Puppeteer Tests', () => {
           visible: true,
         })
 
+
         await page.click('input[name="email-address-ok"]');
-        await page.keyboard.type('input[name="email-address-ok"]', 'jaundelacruz2@infor.com');
+        await page.type('input[name="email-address-ok"]', 'jaundelacruz2@infor.com');
         await page.waitForSelector('input.required.error', {
-          visible: true,
+          visible: false,
         })
+
+
+/* 
+       let emailVal = await page.$eval('input.required.error', 
+        (input) => input.emailVal
+        );
+        expect(emailVal.toBe(true));
+*/
+        
+        /*const text = await page.evaluate(() => {
+            const anchor = document.querySelector('#required-error');
+            return anchor.textContent;
+        });
+        console.log(text);
+        await browser.close();*/
+        
+        /*let lastNameValidation = await page.$eval('input[name="first-name"]', 
+        (input) => input.lastNameValidation
+        );
+
+        expect(lastNameValidation.toBe('invalid'));
+        await browser.close();*/
+        
+       // await page.screenshot({ path: 'input.png' });
 
       });
     
