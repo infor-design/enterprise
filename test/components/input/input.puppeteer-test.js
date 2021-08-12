45f22b7f0a6cc9a885406655411e1a0aee21c93d
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-12 20:07:13 +0800

    test

--- test/components/input/input.puppeteer-test.js
+++ test/components/input/input.puppeteer-test.js
@@ -26,16 +26,19 @@ describe('Input Puppeteer Tests', () => {
         await page.click('input[name="department-code-trackdirty"]');
 
         //validate
-        await page.waitForSelector('input.required.error', {
+        await page.waitForSelector('input#email-addres-ok.required.error', {
           visible: true,
         })
 
-
+/*
         await page.click('input[name="email-address-ok"]');
         await page.type('input[name="email-address-ok"]', 'jaundelacruz2@infor.com');
         await page.waitForSelector('input.required.error', {
           visible: true,
         })
+
+*/
+
 
 
 /* 
