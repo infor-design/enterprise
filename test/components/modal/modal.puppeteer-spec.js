d12b512630a3c3a53ed47934188baa47cf597f13
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-13 21:48:20 +0800

    Test Modal Tooltip

---
+++ test/components/modal/modal.puppeteer-spec.js
@@ -0,0 +1,33 @@
+describe('Modal Puppeteer Tests', () => {
+    describe('Modal Tooltip Test', () => {
+
+      const url = 'http://localhost:4000/components/modal/test-custom-tooltip-close-btn.html';
+
+      beforeAll(async () => {
+        await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+      });
+
+      it('should test modal tooltip', async () => {
+        await expect(page.title()).resolves.toMatch('IDS Enterprise');  
+        await page.click('#add-context');
+        await page.waitForTimeout(200);
+
+        await page.waitForSelector('#add-context-modal');
+
+        await page.hover('#add-context-modal-btn-close');
+        await page.waitForTimeout(200);
+
+        await page.waitForSelector('.has-open-tooltip')
+        let element = await page.evaluate(() => {
+          return !!document.querySelector('.has-open-tooltip') //converts anything to boolean
+        })
+        console.log(element);
+
+        if(element == false){
+          throw console.error('TOOLTIP NOT TRIGGERED');
+        }else{
+          console.log('TOOLTIP TRIGGERED');
+        }
+      });
+    });
+});
