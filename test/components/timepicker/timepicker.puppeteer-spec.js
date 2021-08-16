af74e3897c303316c7a8f361fb17f22eb65f2c92
Eric Angeles <enrico.angeles@infor.com>
2021-08-16 19:50:06 +0800

    add puppeteer tests

---
+++ test/components/timepicker/timepicker.puppeteer-spec.js
@@ -0,0 +1,61 @@
+describe('Timepicker Puppeteer Tests', () => {
+  describe('Index Tests', () => {
+    const url = 'http://localhost:4000/components/timepicker/example-index';
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    it('should show the title', async () => {
+      await expect(page.title()).resolves.toMatch('IDS Enterprise');
+    });
+
+    it('should pass Axe accessibility tests', async () => {
+      await page.setBypassCSP(true);
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+      await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport'] });
+    });
+
+    it('should not have errors', async () => {
+      await page.on('error', function (err) {
+        const theTempValue = err.toString();
+        console.log(`Error: ${theTempValue}`);
+      });
+    });
+
+    it('should open popup on icon click and show value', async () => {
+      const timepickerEl = await page.$('#timepicker-id-1');
+
+      await page.click('#timepicker-id-1-trigger');
+      await page.click('.set-time');
+
+      expect(await page.evaluate(el => el.value, timepickerEl)).toEqual('1:00 AM');
+    });
+  });
+
+  describe('Timepicker Chinese Localization Tests', () => {
+    const url = 'http://localhost:4000/components/timepicker/example-index.html?locale=zh-CN';
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    it('should not have errors', async () => {
+      await page.on('error', function (err) {
+        const theTempValue = err.toString();
+        console.log(`Error: ${theTempValue}`);
+      });
+    });
+
+    it('should set the time and period without any error message', async () => {
+      const timepickerEl = await page.$('#timepicker-id-1');
+      const errorMessage = await page.$('#timepicker-id-1-error');
+
+      await page.click('#timepicker-id-1-trigger');
+      await page.click('.set-time');
+
+      expect(await page.evaluate(el => el.value, timepickerEl)).toEqual('1:00 上午');
+      expect(errorMessage === null).toBeTruthy(); // Error message should not be shown/presented.
+    });
+  });
+});
