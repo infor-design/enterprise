7977e33b05d11e07240f889117eec8da925e0110
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-08-24 16:52:46 +0800

    Reduced & changed timeout to selector  visibsle: true.

--- test/components/modal/modal.puppeteer-spec.js
+++ test/components/modal/modal.puppeteer-spec.js
@@ -8,14 +8,14 @@ describe('Modal Puppeteer Test', () => {
     await page.on('error', function (err) {
       const theTempValue = err.toString();
       console.log(`Error: ${theTempValue}`);
-      });
+    }); 
   });
 
   it('Should open modal on tab, and enter', async () => {
     await page.keyboard.press('Tab');
     await page.keyboard.press('Tab');
     await page.keyboard.press('Enter');
-    const visibleModal = await page.waitForSelector('.modal.is-visible.is-active' , { visible:true });
+    const visibleModal = await page.waitForSelector('.modal.is-visible.is-active', { visible: true });
     expect(visibleModal).toBeTruthy();
   });
 
@@ -24,11 +24,11 @@ describe('Modal Puppeteer Test', () => {
     await page.keyboard.press('Tab');
     await page.keyboard.press('Tab');
     await page.keyboard.press('Enter');
-    await page.waitForSelector('.modal.is-visible.is-active' , { visible:true });
+    await page.waitForSelector('.modal.is-visible.is-active', { visible:true });
     await page.keyboard.press('Escape');
     const closeModal = await page.$('.modal.is-visible.is-active');
     expect(closeModal).toBeFalsy();
-  }); 
+  });
 
 });
 describe('Modal example-validation-editor tests', () => {
@@ -37,19 +37,18 @@ describe('Modal example-validation-editor tests', () => {
     await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
   });
 
-it('Should enable submit after add text to all fields', async () => {
-
+  it('Should enable submit after add text to all fields', async () => {
     await page.click('.btn-secondary');
     await page.waitForSelector('#submit');
-    await page.waitForSelector('.modal div.dropdown' , { visible:true });
+    await page.waitForSelector('.modal div.dropdown', { visible: true });
     await page.click('.modal div.dropdown');
-    await page.waitForSelector('#dropdown-search' , { visible:true });
+    await page.waitForSelector('#dropdown-search', { visible: true });
     await page.keyboard.press('ArrowDown');
     await page.keyboard.press('ArrowDown');
     await page.keyboard.press('Enter');
     await page.type('#context-name', 'test@test.com');
     await page.type('#context-desc', 'test description');
-    await page.type('.editor','test description!^');
+    await page.type('.editor', 'test description!^');
     const element = await page.waitForSelector('#submit');
     expect(element).toBeTruthy();
     await page.click('#submit');
@@ -64,7 +63,7 @@ describe('Modal Tooltip Test', () => {
   it('should test modal tooltip', async () => {
     await page.waitForSelector('#add-context');
     await page.click('#add-context');
-    await page.waitForSelector('#add-context-modal');
+    await page.waitForSelector('#add-context-modal', {visible: true});
     await page.waitForTimeout(200);
     await page.hover('#add-context-modal-btn-close');
     await page.waitForSelector('#add-context-modal-btn-close');
