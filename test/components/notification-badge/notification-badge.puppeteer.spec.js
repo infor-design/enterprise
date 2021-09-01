961eaaf23a6d0152aa1a90f449831ec419d0cda3
Cheno Barcena <johnedwincheno.barcena@infor.com>
2021-09-01 20:25:47 +0800

    “Scripts for Enhancement 5344”

--- test/components/notification-badge/notification-badge.puppeteer.spec.js
+++ test/components/notification-badge/notification-badge.puppeteer.spec.js
@@ -5,101 +5,124 @@ describe('notification-badge Puppeteer Tests', () => {
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
     });
 
-    it('should show the title', async () => {
+    it(' should show the title', async () => {
       await expect(page.title()).resolves.toMatch('IDS Enterprise');
     });
 
-    it('should have six notification badges with different colors on different dot placements', async () => {
-      const elHandleArray = await page.$$('.container-spacer');
-      // eslint-disable-next-line no-unused-vars
-      elHandleArray.forEach(async (el, index) => {
-        await page.waitForSelector('.notification-badge-container', { visible: true });
-        const notifbadge = await page.$eval(`#notification-badge-id-${index + 1}-container`, element => element.innerHTML);
-        try {
-          switch (index) {
-            case 0:
-              expect(notifbadge).toContain('notification-dot-upper-left');
-              break;
-            case 1:
-              expect(notifbadge).toContain('notification-dot-upper-right123');
-              break;
-            case 2:
-              expect(notifbadge).toContain('notification-dot-lower-left');
-              break;
-            case 3:
-              expect(notifbadge).toContain('notification-dot-lower-right123');
-              break;
-            case 4:
-              expect(notifbadge).toContain('notification-dot-upper-left');
-              break;
-            case 5:
-              expect(notifbadge).toContain('notification-dot-upper-right');
-              break;
-            default:
-              console.error('No Value Found');
+    it(' should have six notification badges with different colors on different dot placements', async () => {
+      let hasFailed = false;
+      const checkBadgePlacement = async () => {
+        const elHandleArray = await page.$$('.container-spacer');
+        let index = 0;
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const eL of elHandleArray) {
+          await eL.click();
+          await page.waitForSelector('.notification-badge-container', { visible: true });
+          const notifbadge = await page.$eval(`#notification-badge-id-${index + 1}-container`, element => element.innerHTML);
+
+          try {
+            switch (index) {
+              case 0:
+                expect(notifbadge).toContain('notification-dot-upper-left');
+                break;
+              case 1:
+                expect(notifbadge).toContain('notification-dot-upper-right');
+                break;
+              case 2:
+                expect(notifbadge).toContain('notification-dot-lower-left');
+                break;
+              case 3:
+                expect(notifbadge).toContain('notification-dot-lower-right');
+                break;
+              case 4:
+                expect(notifbadge).toContain('notification-dot-upper-left');
+                break;
+              case 5:
+                expect(notifbadge).toContain('notification-dot-upper-right');
+                break;
+              default:
+
+                hasFailed = true;
+            }
+          } catch (error) {
+            hasFailed = true;
           }
-        } catch (error) {
-          console.error(error);
+
+          index += 1;
+          // });
         }
-        index += 1;
-      });
+        return hasFailed;
+      };
+
+      expect(await checkBadgePlacement()).not.toBeTruthy();
       // }
     });
-
-    async function getWidth(index, iconWidth, dotWidth) {
-      const iconEl = await page.$eval(`#notification-badge-id-${index + 1}-icon`, e => getComputedStyle(e).width);
-      const dotEl = await page.$eval(`#notification-badge-id-${index + 1}-dot`, e => getComputedStyle(e).width);
-      expect(iconEl).toBe(iconWidth);
-      expect(dotEl).toBe(dotWidth);
+    async function getWidth(index, el, width) {
+      const elem = await page.$eval(`#notification-badge-id-${index + 1}-${el}`, e => getComputedStyle(e).width);
+      expect(elem).toBe(width);
     }
-    it('should have Icons 18x18 pixels, and the dot should be 6x6', async () => {
-      const elHandleArray = await page.$$('.container-spacer');
-      // let x = 0;
-      // eslint-disable-next-line no-unused-vars
-      elHandleArray.map(async (el, index) => {
-      // for (const el of elHandleArray) {
-        try {
-          switch (index) {
-            case 0:
-              getWidth(index, '18px', '6px');
-              break;
-            case 1:
-              getWidth(index, '18px', '6px123');
-              break;
-            case 2:
-              getWidth(index, '18px', '6px');
-              break;
-            case 3:
-              getWidth(index, '18px', '6px');
-              break;
-            case 4:
-              getWidth(index, '18px', '6px123');
-              break;
-            case 5:
-              getWidth(index, '18px', '6px');
-              break;
-            default:
-              console.error('No Value Found');
+    it(' should have Icons 18x18 pixels, and the dot should be 6x6', async () => {
+      let hasFailed = false;
+      const checkElementsSize = async (el, size) => {
+        const elHandleArray = await page.$$('.container-spacer');
+        let index = 0;
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const eL of elHandleArray) {
+          await eL.click();
+          try {
+            switch (index) {
+              case 0:
+                await getWidth(index, el, size);
+                break;
+              case 1:
+                await getWidth(index, el, size);
+                break;
+              case 2:
+                await getWidth(index, el, size);
+                break;
+              case 3:
+                await getWidth(index, el, size);
+                break;
+              case 4:
+                await getWidth(index, el, size);
+                break;
+              case 5:
+                await getWidth(index, el, size);
+                break;
+              default:
+
+                hasFailed = true;
+            }
+          } catch (err) {
+            hasFailed = true;
           }
-        } catch (error) {
-          console.error(error);
+          index += 1;
         }
-        index += 1;
-      });
+        return hasFailed;
+      };
+      await checkElementsSize('icon', '18px');
+      await checkElementsSize('dot', '6px');
+      expect(hasFailed).toBe(false);
     });
-    const getIDandCompare = async (el, val) => {
-      const elemHandle = await page.$(el);
-      const elemID = await page.evaluate(elem => elem.getAttribute('data-automation-id'), elemHandle);
-      expect(elemID).toEqual(val);
-      return elemID;
-    };
-    it('should be able to set id/automation id', async () => {
+
+    it(' should be able to set id/automation id', async () => {
+      let isFailed = false;
+      const getIDandCompare = async (el, val) => {
+        try {
+          const elemHandle = await page.$(el);
+          const elemID = await page.evaluate(elem => elem.getAttribute('data-automation-id'), elemHandle);
+          expect(elemID).toEqual(val);
+        } catch (error) {
+          isFailed = true;
+        }
+      };
       await getIDandCompare('#notification-badge-id-1-icon', 'notification-badge-automation-id-1-icon');
       await getIDandCompare('#notification-badge-id-2-icon', 'notification-badge-automation-id-2-icon');
       await getIDandCompare('#notification-badge-id-3-icon', 'notification-badge-automation-id-3-icon');
       await getIDandCompare('#notification-badge-id-4-icon', 'notification-badge-automation-id-4-icon');
       await getIDandCompare('#notification-badge-id-5-icon', 'notification-badge-automation-id-5-icon');
       await getIDandCompare('#notification-badge-id-6-icon', 'notification-badge-automation-id-6-icon');
+      expect(isFailed).toBe(false);
     });
   });
 });
