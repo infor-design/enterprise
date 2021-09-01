1c94bb57077a32c612e40fa8fc4a85c2f15a09c5
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-09-01 19:26:38 +0800

    Removed should have no error test, fixed spacing & added scripts for enhancement ticket #5391

--- test/components/modal/modal.puppeteer-spec.js
+++ test/components/modal/modal.puppeteer-spec.js
@@ -1,14 +1,7 @@
-describe('Modal Puppeteer Test', () => {
+describe('Modal Puppeteer init example-modal tests', () => {
   const url = 'http://localhost:4000/components/modal/example-index';
   beforeAll(async () => {
     await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
-  });
-
-  it('should not have error', async () => {
-    await page.on('error', function (err) {
-      const theTempValue = err.toString();
-      console.log(`Error: ${theTempValue}`);
-    }); 
   });
 
   it('Should open modal on tab, and enter', async () => {
@@ -30,6 +23,72 @@ describe('Modal Puppeteer Test', () => {
     expect(closeModal).toBeFalsy();
   });
 });
+
+describe('Modal open example-modal tests on click', () => {
+  const url = 'http://localhost:4000/components/modal/example-index?theme=classic';
+  beforeEach(async () => {
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    await page.waitForSelector('#add-context');
+    await page.click('#add-context');
+    await page.waitForSelector('.overlay');
+  });
+
+  it('Should open modal on click', async () => {
+    expect(await page.waitForSelector('.modal, is-visible')).toBeTruthy();
+  });
+});
+
+describe('Modal example-close-btn tests', () => {
+  const url = 'http://localhost:4000/components/modal/example-close-btn';
+  beforeEach(async () => {
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+  });
+
+  it('Should close the modal via the x close icon', async () => {
+    await page.click('#add-context');
+
+    await page.waitForSelector('.modal-engaged', { visible: true });
+    await page.waitForTimeout(200);
+    const button = await page.waitForSelector('#add-context-modal-btn-close');
+    await button.hover();
+    await button.click();
+    await page.waitForSelector('.modal-engaged', { hidden: true });
+  });
+
+  it('Should be able to set id/automation id', async () => {
+    await page.click('#add-context');
+
+    await page.waitForSelector('.modal-engaged', { visible: true });
+    await page.waitForTimeout(200);
+
+    const title = await page.$eval('#add-context-modal-title',
+      element => element.getAttribute('id'));
+    const text = await page.$eval('#add-context-modal-text',
+      element => element.getAttribute('id'));
+    const modal = await page.$eval('#add-context-modal',
+      element => element.getAttribute('id'));
+
+    if (title !== 'add-context-modal-title' || text !== 'add-context-modal-text' || modal !== 'add-context-modal') {
+      throw console.error('ERROR');
+    }
+  }); 
+});
+
+describe('Modal example-validation tests', () => {
+  const url = 'http://localhost:4000/components/modal/example-validation';
+  beforeEach(async () => {
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    await page.click('.btn-secondary');
+  });
+
+  it('Should focus on first focusable item in modal', async () => {
+    const dropdown = await page.evaluateHandle(() => document.activeElement);
+    await dropdown.type('div.dropdown');
+  });
+});
+
 describe('Modal example-validation-editor tests', () => {
   const url = 'http://localhost:4000/components/modal/test-validation-editor';
   beforeAll(async () => {
@@ -50,5 +109,31 @@ describe('Modal example-validation-editor tests', () => {
     const element = await page.waitForSelector('#submit');
     expect(element).toBeTruthy();
     await page.click('#submit');
+  });
+});
+
+describe('Modal test-custom-tooltip-close-btn tests', () => {
+  const url = 'http://localhost:4000/components/modal/test-custom-tooltip-close-btn.html';
+  beforeAll(async () => {
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+  });
+
+  it('it should open the tooltip', async () => {
+    await page.click('#add-context');
+    await page.waitForSelector('.modal-engaged', { visible: true });
+    await page.hover('#add-context-modal-btn-close');
+    const tooltip = await page.waitForSelector('.has-open-tooltip, .has-tooltip', { visible: true });
+    expect(tooltip).toBeTruthy();
+  });
+
+  it('it should have custom attribute tabIndex:-1', async () => {
+    await page.click('#add-context');
+    await page.waitForSelector('.modal-engaged', { visible: true });
+    
+    const tabIndex = await page.$eval('#add-context-modal-btn-close',
+      element => element.getAttribute('tabindex'));
+    if (tabIndex !== '-1') {
+      throw console.error('Tab Index value changed');
+    }
   });
 });
