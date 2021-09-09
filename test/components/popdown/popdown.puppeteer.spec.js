6520e8b63451637b77f073ba37e1603b961f94af
Cheno Barcena <johnedwincheno.barcena@infor.com>
2021-09-09 21:38:41 +0800

    :hammer: Added Popdown (with Dropdown) Tests :hammer: Added Popdown first last tab Tests :heavy_check_mark: Reorder Outside Event Tests and removed waitForTimeout

--- test/components/popdown/popdown.puppeteer.spec.js
+++ test/components/popdown/popdown.puppeteer.spec.js
@@ -35,6 +35,119 @@ describe('Popdown Puppeteer Tests', () => {
     });
   });
 
+  describe('Popdown (with Dropdown) Tests', () => {
+    const url = 'http://localhost:4000/components/popdown/test-contains-dropdown?layout=nofrills';
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
+    });
+
+    it('should show the title', async () => {
+      await expect(page.title()).resolves.toMatch('IDS Enterprise');
+    });
+
+    it('should keep the Popdown open while focused on an inline-Dropdown component\'s list', async () => {
+      // Open the Popdown
+      await page.click('#popdown-example-trigger');
+      await page.waitForSelector('.popdown', { visible: true });
+
+      // Open the Dropdown List
+      await page.click('.popdown div.dropdown');
+      await page.waitForSelector('.dropdown-list', { visible: true });
+
+      // Test that the Popdown remained open
+      const popdown = await page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
+      expect(popdown).toBeTruthy();
+
+      // Choose an option from the Dropdown, which will close it.
+      await page.click('li[data-val="1"]');
+      await page.waitForTimeout(400);
+
+      // Test that the Popdown remained open
+      expect(popdown).toBeTruthy();
+    });
+  });
+
+  describe('Popdown first last tab Tests', () => {
+    const url = 'http://localhost:4000/components/popdown/test-first-last-tab?layout=nofrills';
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
+    });
+
+    it('should show the title', async () => {
+      await expect(page.title()).resolves.toMatch('IDS Enterprise');
+    });
+
+    // 1. On open first input should be focused.
+    // 2. On first input (Shift + Tab) should close and focus to previous.
+    // 3. On last input Tab should close and focus to next.
+    it('should let close the popdown and if available focus to prev/next', async () => {
+      // Tab on date field
+      const dateField = await page.$('#date-field-normal');
+      const dateIcon = await page.$('.btn-icon');
+      dateField.click();
+      await dateField.press('Tab', { delay: 100 });
+      await dateIcon.press('Tab', { delay: 100 });
+      await page.waitForSelector('.popdown', { visible: true });
+
+      // Popdown should open and first input should be focused.
+      const focusedId = () => page.evaluate(() => document.activeElement.getAttribute('id'));
+      const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
+      // const firstNameField = () => page.evaluate(() => !!document.querySelector('#first-name'));
+      // const lastNameField = () => page.evaluate(() => !!document.querySelector('#last-name'));
+      const firstName = await page.$('#first-name');
+      const lastName = await page.$('#last-name');
+      expect(await popdown()).toBe(true);
+      // expect(firstName).toBeTruthy();
+      // expect(lastName).toBeTruthy();
+      expect(await focusedId()).toEqual('first-name');
+
+      // Tab on first input
+      await firstName.press('Tab');
+
+      // Last input should be focused in popdown.
+      expect(await focusedId()).toEqual('last-name');
+      await page.waitForTimeout(380);
+      // Tab on last input in popdown
+      await lastName.press('Tab');
+
+      // Popdown should close and next input (another-field) should be focused.
+      expect(await popdown()).toBe(false);
+      // expect(await firstNameField()).toBeFalsy();
+      // expect(await lastNameField()).toBeFalsy();
+      expect(await focusedId()).toEqual('another-field');
+
+      // Shift + Tab on this next to popdown input (another-field)
+      await page.waitForTimeout(500);
+      const anotherField = await page.$('#another-field');
+      await page.keyboard.down('Shift');
+      await anotherField.press('Tab');
+      await page.keyboard.up('Shift');
+
+      // Popdown should open again and first input should be focused.
+      expect(await popdown()).toBe(true);
+      // expect(await firstNameField()).toBeTruthy();
+      // expect(await lastNameField()).toBeTruthy();
+      expect(await focusedId()).toEqual('first-name');
+
+      // Shift + Tab on first input in popdown
+      await page.waitForTimeout(380);
+      await page.keyboard.down('Shift');
+      await firstName.press('Tab');
+      await page.keyboard.up('Shift');
+
+      await page.waitForTimeout(100);
+      await page.keyboard.down('Shift');
+      await page.keyboard.press('Tab', { delay: 100 });
+      await page.keyboard.up('Shift');
+
+      // Popdown should close and previous input (date field) should be focused.
+      expect(await popdown()).toBe(false);
+      // expect(await firstNameField()).toBeFalsy();
+      // expect(await lastNameField()).toBeFalsy();
+      expect(await focusedId()).toEqual('date-field-normal');
+    });
+  });
+
   describe('Outside Event Tests', () => {
     const url = 'http://localhost:4000/components/popdown/test-click-outside.html';
     beforeAll(async () => {
@@ -49,12 +162,6 @@ describe('Popdown Puppeteer Tests', () => {
       // |----------------------------------------------------------|
       // | https://github.com/infor-design/enterprise/issues/3618   |
       // |----------------------------------------------------------|
-      await page.click('#popdown-example-trigger');
-      await page.waitForSelector('#maincontent');
-      await page.click('#maincontent');
-      await page.waitForSelector('[data-automation-id="popover-listview-example-automation-id"]');
-      await page.click('[data-automation-id="popover-listview-example-automation-id"]');
-      await page.waitForTimeout(1000);
       page
         .on('console', (message) => {
           expect(message.text()).toContain('click outside');
@@ -65,7 +172,11 @@ describe('Popdown Puppeteer Tests', () => {
           expect(_text).toBe('click outside JSHandle@object');
           expect(value).toBe('click outside');
         });
+      await page.click('#popdown-example-trigger');
+      await page.waitForSelector('#maincontent');
       await page.click('#maincontent');
+      await page.click('[data-automation-id="popover-listview-example-automation-id"]');
     });
   });
 });
+
