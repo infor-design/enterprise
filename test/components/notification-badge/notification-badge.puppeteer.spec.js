a3489c9378d0fdafb2a54ed3717a6ae4830590aa
Cheno Barcena <johnedwincheno.barcena@infor.com>
2021-10-22 03:57:55 +0800

    ✓ code refactor ✓ Added convenience functions

--- test/components/notification-badge/notification-badge.puppeteer.spec.js
+++ test/components/notification-badge/notification-badge.puppeteer.spec.js
@@ -1,23 +1,31 @@
-const { getWidthandCompare, getIDandCompare, compareInnerHTML } = require('../../helpers/e2e-utils.js');
+const { getComputedStyle, checkDataAutomationID, checkInnerHTMLValue } = require('../../helpers/e2e-utils.js');
 
-describe('notification-badge Puppeteer Tests', () => {
+describe('Notification-Badge Puppeteer Tests', () => {
   describe('Badge Placement Tests', () => {
     const url = 'http://localhost:4000/components/notification-badge/example-badge-placement.html?theme=classic&mode=light&layout=nofrills';
     beforeAll(async () => {
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
     });
 
-    it(' should show the title', async () => {
+    it('should show the title', async () => {
       await expect(page.title()).resolves.toMatch('IDS Enterprise');
     });
 
-    it(' should check the test page with Axe', async () => {
+    it('should check the test page with Axe', async () => {
       await page.setBypassCSP(true);
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport'] });
     });
 
-    it(' should have six notification badges with different colors on different dot placements', async () => {
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea).toMatchObject({
+        name: 'IDS Enterprise',
+        role: 'RootWebArea',
+      });
+    });
+
+    it('should have six notification badges with different colors on different dot placements', async () => {
       let hasFailed = false;
       const checkBadgePlacement = async () => {
         const elHandleArray = await page.$$('.container-spacer');
@@ -27,7 +35,6 @@ describe('notification-badge Puppeteer Tests', () => {
           await eL.click();
           await page.waitForSelector('.notification-badge-container', { visible: true });
           const notifbadge = await page.$eval(`#notification-badge-id-${index + 1}-container`, element => element.innerHTML);
-
           try {
             switch (index) {
               case 0:
@@ -49,7 +56,6 @@ describe('notification-badge Puppeteer Tests', () => {
                 expect(notifbadge).toContain('notification-dot-upper-right');
                 break;
               default:
-
                 hasFailed = true;
             }
           } catch (error) {
@@ -62,38 +68,17 @@ describe('notification-badge Puppeteer Tests', () => {
       expect(await checkBadgePlacement()).not.toBeTruthy();
     });
 
-    it(' should have Icons 18x18 pixels, and the dot should be 6x6', async () => {
+    it('should have Icons 18x18 pixels, and the dot should be 6x6', async () => {
       let hasFailed = false;
-      const checkElementsSize = async (el, size) => {
+      const checkElementSize = async (element, style, value) => {
         const elHandleArray = await page.$$('.container-spacer');
         let index = 0;
         // eslint-disable-next-line no-restricted-syntax
         for await (const eL of elHandleArray) {
           await eL.click();
           try {
-            switch (index) {
-              case 0:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              case 1:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              case 2:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              case 3:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              case 4:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              case 5:
-                await getWidthandCompare(`#notification-badge-id-${index + 1}-${el}`, size);
-                break;
-              default:
-
-                hasFailed = true;
-            }
+            const width = await getComputedStyle(`#notification-badge-id-${index + 1}-${element}`, style);
+            expect(width).toBe(value);
           } catch (err) {
             hasFailed = true;
           }
@@ -101,60 +86,77 @@ describe('notification-badge Puppeteer Tests', () => {
         }
         return hasFailed;
       };
-      await checkElementsSize('icon', '18px');
-      await checkElementsSize('dot', '6px');
+      await checkElementSize('icon', 'width', '18px');
+      await checkElementSize('dot', 'width', '6px');
       expect(hasFailed).toBe(false);
     });
 
-    it(' should be able to set id/automation id', async () => {
+    it('should be able to set id/automation id', async () => {
       const isFailed = [];
-      isFailed.push(await getIDandCompare('#notification-badge-id-1-icon', 'notification-badge-automation-id-1-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-2-icon', 'notification-badge-automation-id-2-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-3-icon', 'notification-badge-automation-id-3-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-4-icon', 'notification-badge-automation-id-4-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-5-icon', 'notification-badge-automation-id-5-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-6-icon', 'notification-badge-automation-id-6-icon'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-1-dot', 'notification-badge-automation-id-1-dot'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-2-dot', 'notification-badge-automation-id-2-dot'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-3-dot', 'notification-badge-automation-id-3-dot'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-4-dot', 'notification-badge-automation-id-4-dot'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-5-dot', 'notification-badge-automation-id-5-dot'));
-      isFailed.push(await getIDandCompare('#notification-badge-id-6-dot', 'notification-badge-automation-id-6-dot'));
+      let index = 0;
+      const elHandleArray = await page.$$('.container-spacer');
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const eL of elHandleArray) {
+        await eL.click();
+        isFailed.push(await checkDataAutomationID(`#notification-badge-id-${index + 1}-container`, `notification-badge-automation-id-${index + 1}-container`));
+        isFailed.push(await checkDataAutomationID(`#notification-badge-id-${index + 1}-icon`, `notification-badge-automation-id-${index + 1}-icon`));
+        isFailed.push(await checkDataAutomationID(`#notification-badge-id-${index + 1}-dot`, `notification-badge-automation-id-${index + 1}-dot`));
+        index += 1;
+      }
       expect(isFailed).not.toContain(true);
     });
   });
 
   describe('Index Tests', () => {
-    const url = 'http://localhost:4000/components/notification-badge/example-index.html?theme=classic&mode=light&layout=nofrills';
+    const url = 'http://localhost:4000/components/notification-badge/example-index.html';
     beforeAll(async () => {
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
     });
 
-    it(' should show the title', async () => {
+    it('should show the title', async () => {
       await expect(page.title()).resolves.toMatch('IDS Enterprise');
     });
 
-    it(' should check the test page with Axe', async () => {
+    it('should check the test page with Axe', async () => {
       await page.setBypassCSP(true);
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport'] });
     });
 
-    it(' should have six notification badges with different colors on different dot placements', async () => {
-      const elHandleArray = await page.$$('.container-spacer');
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea).toMatchObject({
+        name: 'IDS Enterprise',
+        role: 'RootWebArea',
+      });
+      expect(webArea.children[0]).toMatchObject({
+        name: 'Skip to Main Content',
+        role: 'link'
+      });
+      expect(webArea.children[1]).toMatchObject({
+        role: 'heading',
+        name: 'IDS Enterprise',
+        level: 1
+      });
+      expect(webArea.children[2]).toMatchObject({
+        role: 'combobox',
+        name: 'Header More Actions Button',
+        haspopup: 'menu'
+      });
+      expect(webArea.children[3]).toMatchObject({
+        role: 'StaticText',
+        name: 'Notification Badge'
+      });
+    });
+
+    it('should have six notification badges with different colors', async () => {
       const isFailed = [];
-      let index = 0;
-      // eslint-disable-next-line no-restricted-syntax
-      for await (const eL of elHandleArray) {
-        await eL.click();
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        isFailed.push(await compareInnerHTML(`#notification-badge-${index + 1} > .notification-badge-container`, 'notification-dot-upper-right'));
-        index += 1;
-      }
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-1 > .notification-badge-container', 'notification-dot-upper-right'));
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-2 > .notification-badge-container', 'notification-dot-upper-right'));
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-3 > .notification-badge-container', 'notification-dot-upper-right'));
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-4 > .notification-badge-container', 'notification-dot-upper-right'));
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-5 > .notification-badge-container', 'notification-dot-upper-right'));
+      isFailed.push(await checkInnerHTMLValue('#notification-badge-6 > .notification-badge-container', 'notification-dot-upper-right'));
       expect(isFailed).not.toContain(true);
     });
   });
