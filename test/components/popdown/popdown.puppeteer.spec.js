dc43adce4fb4370b82431b3b8c905709bb32a695
Cheno Barcena <johnedwincheno.barcena@infor.com>
2021-10-22 19:53:01 +0800

    Code refactor ✓ Added convenience functions ✓ Remove false postives. Line 128

--- test/components/popdown/popdown.puppeteer.spec.js
+++ test/components/popdown/popdown.puppeteer.spec.js
@@ -1,3 +1,5 @@
+const { checkDataAutomationID, checkIfElementHasFocused, checkIfElementExist } = require('../../helpers/e2e-utils.js');
+
 describe('Popdown Puppeteer Tests', () => {
   describe('Index Tests', () => {
     const url = 'http://localhost:4000/components/popdown/example-index?layout=nofrills';
@@ -15,6 +17,36 @@ describe('Popdown Puppeteer Tests', () => {
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport', 'aria-valid-attr-value', 'region'] });
     });
 
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea.children[0]).toMatchObject({
+        name: 'Component Example Page',
+        role: 'heading',
+        level: 1
+      });
+      expect(webArea.children[1]).toMatchObject({
+        name: 'My Cart',
+        role: 'button'
+      });
+      expect(webArea.children[2]).toMatchObject({
+        level: 2,
+        name: 'My Cart (8 Items)',
+        role: 'heading'
+      });
+      expect(webArea.children[3]).toMatchObject({
+        name: '',
+        role: 'generic'
+      });
+      expect(webArea.children[4]).toMatchObject({
+        name: 'Edit Cart',
+        role: 'button'
+      });
+      expect(webArea.children[5]).toMatchObject({
+        name: 'Checkout',
+        role: 'button'
+      });
+    });
+
     it('should display on click', async () => {
       await page.click('#popdown-example-trigger');
       await page.waitForSelector('.popdown', { visible: true });
@@ -23,21 +55,12 @@ describe('Popdown Puppeteer Tests', () => {
     });
 
     it('should have id/automation ids', async () => {
-      let isFailed = false;
-      const getIDandCompare = async (el, val) => {
-        try {
-          const elemHandle = await page.$(el);
-          const elemID = await page.evaluate(elem => elem.getAttribute('data-automation-id'), elemHandle);
-          expect(elemID).toEqual(val);
-        } catch (error) {
-          isFailed = true;
-        }
-      };
-      await getIDandCompare('#popdown', 'popdown-automation-id');
-      await getIDandCompare('#popover-listview-example', 'popover-listview-example-automation-id');
-      await getIDandCompare('#edit-cart', 'edit-cart-automation-id');
-      await getIDandCompare('#checkout', 'checkout-automation-id');
-      expect(isFailed).toBe(false);
+      const isFailed = [];
+      isFailed.push(await checkDataAutomationID('#popdown', 'popdown-automation-id'));
+      isFailed.push(await checkDataAutomationID('#popover-listview-example', 'popover-listview-example-automation-id'));
+      isFailed.push(await checkDataAutomationID('#edit-cart', 'edit-cart-automation-id'));
+      isFailed.push(await checkDataAutomationID('#checkout', 'checkout-automation-id'));
+      expect(isFailed).not.toContain(true);
     });
   });
 
@@ -57,6 +80,41 @@ describe('Popdown Puppeteer Tests', () => {
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport', 'aria-valid-attr-value', 'region'] });
     });
 
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea.children[0]).toMatchObject({
+        name: 'Component Example Page',
+        role: 'heading',
+        level: 1
+      });
+      expect(webArea.children[1]).toMatchObject({
+        name: 'Choose an option...',
+        role: 'button'
+      });
+      expect(webArea.children[2]).toMatchObject({
+        level: 2,
+        name: 'Dropdown Container',
+        role: 'heading'
+      });
+      expect(webArea.children[3]).toMatchObject({
+        name: 'Option Picker',
+        role: 'StaticText'
+      });
+      expect(webArea.children[4]).toMatchObject({
+        name: 'Option Picker, (please select)',
+        role: 'combobox',
+        haspopup: 'listbox'
+      });
+      expect(webArea.children[5]).toMatchObject({
+        name: 'Clear',
+        role: 'button'
+      });
+      expect(webArea.children[6]).toMatchObject({
+        name: 'Submit',
+        role: 'button'
+      });
+    });
+
     it('should keep the Popdown open while focused on an inline-Dropdown component\'s list', async () => {
       // Open the Popdown
       await page.click('#popdown-example-trigger');
@@ -67,15 +125,15 @@ describe('Popdown Puppeteer Tests', () => {
       await page.waitForSelector('.dropdown-list', { visible: true });
 
       // Test that the Popdown remained open
-      const popdown = await page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
-      expect(popdown).toBeTruthy();
+      const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
+      expect(await popdown()).toBeTruthy();
 
       // Choose an option from the Dropdown, which will close it.
       await page.click('li[data-val="1"]');
       await page.waitForTimeout(400);
 
       // Test that the Popdown remained open
-      expect(popdown).toBeTruthy();
+      expect(await popdown()).toBeTruthy();
     });
   });
 
@@ -95,10 +153,39 @@ describe('Popdown Puppeteer Tests', () => {
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport', 'aria-valid-attr-value', 'region'] });
     });
 
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      const root = {
+        role: 'RootWebArea',
+        name: 'IDS Enterprise',
+        children: [
+          { role: 'heading', name: 'Component Example Page', level: 1 },
+          { role: 'heading', name: 'Popdown Example: Tabbing into and out from a popdown.', level: 2 },
+          { role: 'StaticText', name: '1. When compound field opens, the first input in the popdown should be focused.' },
+          { role: 'StaticText', name: '2. Shift + Tab on first input in the popdown should close the popdown and focus to previous element (Date Field).' },
+          { role: 'StaticText', name: '3. Tab on last input in the popdown should close the popdown and focus to next element (Another Field).' },
+          { role: 'StaticText', name: 'Date Field' },
+          { role: 'StaticText', name: '. Press Down arrow to select' },
+          { role: 'combobox', name: 'Date Field . Press Down arrow to select', haspopup: 'listbox' },
+          { role: 'combobox', name: 'Date Picker Trigger', haspopup: 'dialog' },
+          { role: 'StaticText', name: 'Compound Field, mouse click or tab into' },
+          { role: 'generic', name: 'JoeSmith' },
+          { role: 'StaticText', name: 'Another Field' },
+          { role: 'textbox', name: 'Another Field' },
+          { role: 'StaticText', name: 'First Name' },
+          { role: 'textbox', name: 'First Name' },
+          { role: 'StaticText', name: 'Last Name' },
+          { role: 'textbox', name: 'Last Name' }
+        ]
+      };
+      expect(webArea).toMatchObject(root);
+    });
+
     // 1. On open first input should be focused.
     // 2. On first input (Shift + Tab) should close and focus to previous.
     // 3. On last input Tab should close and focus to next.
     it('should let close the popdown and if available focus to prev/next', async () => {
+      const isFailed = [];
       // Tab on date field
       const dateField = await page.$('#date-field-normal');
       const dateIcon = await page.$('.btn-icon');
@@ -108,31 +195,35 @@ describe('Popdown Puppeteer Tests', () => {
       await page.waitForSelector('.popdown', { visible: true });
 
       // Popdown should open and first input should be focused.
-      const focusedId = () => page.evaluate(() => document.activeElement.getAttribute('id'));
-      const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
-      // const firstNameField = () => page.evaluate(() => !!document.querySelector('#first-name'));
-      // const lastNameField = () => page.evaluate(() => !!document.querySelector('#last-name'));
+
+      /* const focusedId = () => page.evaluate(() => document.activeElement.getAttribute('id'));
+      const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible')); */
       const firstName = await page.$('#first-name');
       const lastName = await page.$('#last-name');
-      expect(await popdown()).toBe(true);
-      // expect(firstName).toBeTruthy();
-      // expect(lastName).toBeTruthy();
-      expect(await focusedId()).toEqual('first-name');
+
+      /* expect(await popdown()).toBe(true);
+      expect(await focusedId()).toEqual('first-name'); */
+      isFailed.push(await checkIfElementExist('.popdown.bottom.visible'));
+      isFailed.push(await checkIfElementHasFocused('#first-name'));
 
       // Tab on first input
       await firstName.press('Tab');
 
       // Last input should be focused in popdown.
-      expect(await focusedId()).toEqual('last-name');
+
+      /* expect(await focusedId()).toEqual('last-name'); */
+      isFailed.push(await checkIfElementHasFocused('#last-name'));
+
       await page.waitForTimeout(380);
       // Tab on last input in popdown
       await lastName.press('Tab');
 
       // Popdown should close and next input (another-field) should be focused.
-      expect(await popdown()).toBe(false);
-      // expect(await firstNameField()).toBeFalsy();
-      // expect(await lastNameField()).toBeFalsy();
-      expect(await focusedId()).toEqual('another-field');
+
+      /* expect(await popdown()).toBe(false);
+      expect(await focusedId()).toEqual('another-field'); */
+      isFailed.push(!await checkIfElementExist('.popdown.bottom.visible'));
+      isFailed.push(await checkIfElementHasFocused('#another-field'));
 
       // Shift + Tab on this next to popdown input (another-field)
       await page.waitForTimeout(500);
@@ -142,10 +233,11 @@ describe('Popdown Puppeteer Tests', () => {
       await page.keyboard.up('Shift');
 
       // Popdown should open again and first input should be focused.
-      expect(await popdown()).toBe(true);
-      // expect(await firstNameField()).toBeTruthy();
-      // expect(await lastNameField()).toBeTruthy();
-      expect(await focusedId()).toEqual('first-name');
+
+      /* expect(await popdown()).toBe(true);
+      expect(await focusedId()).toEqual('first-name'); */
+      isFailed.push(await checkIfElementExist('.popdown.bottom.visible'));
+      isFailed.push(await checkIfElementHasFocused('#first-name'));
 
       // Shift + Tab on first input in popdown
       await page.waitForTimeout(380);
@@ -159,10 +251,12 @@ describe('Popdown Puppeteer Tests', () => {
       await page.keyboard.up('Shift');
 
       // Popdown should close and previous input (date field) should be focused.
-      expect(await popdown()).toBe(false);
-      // expect(await firstNameField()).toBeFalsy();
-      // expect(await lastNameField()).toBeFalsy();
-      expect(await focusedId()).toEqual('date-field-normal');
+
+      /* expect(await popdown()).toBe(false);
+      expect(await focusedId()).toEqual('date-field-normal'); */
+      isFailed.push(!await checkIfElementExist('.popdown.bottom.visible'));
+      isFailed.push(await checkIfElementHasFocused('#date-field-normal'));
+      expect(isFailed).not.toContain(true);
     });
   });
 
@@ -182,21 +276,67 @@ describe('Popdown Puppeteer Tests', () => {
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport', 'aria-valid-attr-value', 'region'] });
     });
 
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea.children[0]).toMatchObject({
+        name: 'Component Example Page',
+        role: 'heading',
+        level: 1
+      });
+      expect(webArea.children[1]).toMatchObject({
+        name: 'Trigger Popdown',
+        role: 'button'
+      });
+      expect(webArea.children[2]).toMatchObject({
+        level: 2,
+        name: 'Look Up Something',
+        role: 'heading'
+      });
+      expect(webArea.children[3]).toMatchObject({
+        name: 'Lookup',
+        role: 'StaticText'
+      });
+      expect(webArea.children[4]).toMatchObject({
+        name: '[Lookup]. [PressDown]',
+        role: 'StaticText'
+      });
+      expect(webArea.children[5]).toMatchObject({
+        name: 'Lookup [Lookup]. [PressDown]',
+        role: 'textbox'
+      });
+      expect(webArea.children[6]).toMatchObject({
+        name: '[LookupTriggerButton]',
+        role: 'button'
+      });
+      expect(webArea.children[7]).toMatchObject({
+        name: 'Reset',
+        role: 'button'
+      });
+      expect(webArea.children[8]).toMatchObject({
+        name: 'Accept',
+        role: 'button'
+      });
+    });
+
     it('Should remain open when an inner Lookup component is opened', async () => {
-    // Open the Popdown
+      const isFailed = [];
+      // Open the Popdown
       await page.click('#popdown-trigger');
+      isFailed.push(await checkIfElementExist('.popdown.bottom.visible'));
       // await page.waitForSelector('.popdown.bottom.visible', { visible: true });
-      const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
-      expect(await popdown()).toBe(true);
+      /*       const popdown = () => page.evaluate(() => !!document.querySelector('.popdown.bottom.visible'));
+      expect(await popdown()).toBe(true); */
 
       // Open the Lookup
       await page.click('.btn-icon');
+      isFailed.push(await checkIfElementExist('.lookup-modal'));
       // await page.waitForSelector('.lookup-modal', { visible: true });
-      const modal = await page.evaluate(() => !!document.querySelector('.lookup-modal'));
-      expect(modal).toBe(true);
+      /*       const modal = await page.evaluate(() => !!document.querySelector('.lookup-modal'));
+      expect(modal).toBe(true); */
 
       // Test that the Popdown remained open
-      expect(await popdown()).toBe(true);
+      isFailed.push(await checkIfElementExist('.popdown.bottom.visible'));
+      /*  expect(await popdown()).toBe(true); */
 
       // Choose an option from the Lookup
       await page.click('#lookup-datagrid > div.datagrid-wrapper.center.scrollable-x.scrollable-y > table > tbody > tr:nth-child(1) > td:nth-child(2) > div > a');
@@ -205,7 +345,9 @@ describe('Popdown Puppeteer Tests', () => {
       expect(input).toBe('2142201');
 
       // Test that the Popdown remained open
-      expect(await popdown()).toBe(true);
+      isFailed.push(await checkIfElementExist('.popdown.bottom.visible'));
+      /*  expect(await popdown()).toBe(true); */
+      expect(isFailed).not.toContain(true);
     });
   });
 
@@ -225,7 +367,47 @@ describe('Popdown Puppeteer Tests', () => {
       await expect(page).toPassAxeTests({ disabledRules: ['meta-viewport', 'aria-valid-attr-value', 'region'] });
     });
 
+    it('should have Accessibility', async () => {
+      const webArea = await page.accessibility.snapshot();
+      expect(webArea.children[0]).toMatchObject({
+        name: 'Skip to Main Content',
+        role: 'link',
+      });
+      expect(webArea.children[1]).toMatchObject({
+        level: 1,
+        name: 'IDS Enterprise',
+        role: 'heading'
+      });
+      expect(webArea.children[2]).toMatchObject({
+        name: 'Header More Actions Button',
+        haspopup: 'menu',
+        role: 'combobox'
+      });
+      expect(webArea.children[3]).toMatchObject({
+        name: 'My Cart',
+        role: 'button'
+      });
+      expect(webArea.children[4]).toMatchObject({
+        name: 'My Cart (8 Items)',
+        level: 2,
+        role: 'heading'
+      });
+      expect(webArea.children[5]).toMatchObject({
+        name: '',
+        role: 'generic'
+      });
+      expect(webArea.children[6]).toMatchObject({
+        name: 'Edit Cart',
+        role: 'button'
+      });
+      expect(webArea.children[7]).toMatchObject({
+        name: 'Checkout',
+        role: 'button'
+      });
+    });
+
     it('should show have outside event', async () => {
+      expect.assertions(8);
       // |----------------------------------------------------------|
       // | https://github.com/infor-design/enterprise/issues/3618   |
       // |----------------------------------------------------------|
@@ -241,8 +423,8 @@ describe('Popdown Puppeteer Tests', () => {
         });
       await page.click('#popdown-example-trigger');
       await page.waitForSelector('#maincontent');
-      await page.click('#maincontent');
-      await page.click('[data-automation-id="popover-listview-example-automation-id"]');
+      await page.click('#maincontent', { delay: 500 });
+      await page.click('[data-automation-id="popover-listview-example-automation-id"]', { delay: 500 });
     });
   });
 });
