125388131131dd817abe3cec4696c92872b7631c
Janah Beatriz Intal <janahbeatriz.intal@infor.com>
2021-12-10 20:27:17 +0800

    5851: Button puppeteer tests

---
+++ test/components/button/button.puppeteer-spec.js
@@ -0,0 +1,17 @@
+describe('Button with hitbox test', () => {
+  const url = 'http://localhost:4000/components/button/example-button-with-hitbox';
+  beforeAll(async () => {
+    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+  });
+  
+  it('should have 44px height, and 44px minimum width in button with hitbox', async () => {
+    const hboxHeight = await page.$eval('span[class="hitbox-area"]', el => getComputedStyle(el).getPropertyValue('height'));
+    expect(hboxHeight).toMatch('44px');
+  });
+
+  it('should have the hitbox area clickable', async () => {
+    await page.click('a.btn-menu span.hitbox-area');
+    await page.waitForTimeout(200);
+    await page.waitForSelector('#popupmenu-2', { visible: true });
+  });
+});
