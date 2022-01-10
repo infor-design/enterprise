b5419aae784b72037320563ff0c588635357b7d5
Eric Angeles <enrico.angeles@infor.com>
2022-01-10 22:35:53 +0800

    add more test steps

--- test/components/searchfield/searchfield.e2e-spec.js
+++ test/components/searchfield/searchfield.e2e-spec.js
@@ -217,15 +217,24 @@ if (utils.isChrome() && utils.isCI()) {
 
   describe('Searchfield close icon placement tests', () => {
     beforeEach(async () => {
-      await utils.setPage('/components/field-options/example-index.html?theme=classic&mode=light&layout=nofrills');
+      await utils.setPage('/components/field-options/example-index?theme=classic&mode=light&layout=nofrills');
+      await browser.driver
+        .wait(protractor.ExpectedConditions
+          .presenceOf(element(by.id('example-searchfield-clearable'))), config.waitsFor);
     });
 
     if (utils.isChrome() && utils.isCI()) {
       it('should not visual regress', async () => {
         const searchfieldClearable = await element(by.id('example-searchfield-clearable'));
+        await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(searchfieldClearable), config.waitsFor);
         const compactButton = await element(by.id('toggle-compact'));
+        await browser.driver
+        .wait(protractor.ExpectedConditions.presenceOf(compactButton), config.waitsFor);
 
         await compactButton.click();
+        await searchfieldClearable.clear();
+        await searchFieldClearable.sendKeys('co');
         await browser.driver
           .wait(protractor.ExpectedConditions.visibilityOf(await element(by.id('example-searchfield-clearable'))), config.waitsFor);
         await browser.driver.sleep(config.sleep);
