0fca8c642f4fc6b618349755352991c5ce0c2c7a
Dave Berg <dave.berg@infor.com>
2022-01-12 16:28:44 -0500

    Use data-automation-id if attributes length is greater than one otherwise default to id.

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1264,7 +1264,9 @@ Editor.prototype = {
             self.createLink($(`[name="em-url-${self.id}"]`, this));
           }
         } else {
-          self.insertImage($(`#${self.settings.attributes[0].value}-editor-modal-input0`).val());
+          self.settings.attributes.length > 1
+            ? self.insertImage($(`[data-automation-id="${self.settings.attributes[self.settings.attributes.length - 1].value}-editor-modal-input0"`).val())
+            : self.insertImage($(`#${self.settings.attributes[0].value}-editor-modal-input0`).val());
         }
       });
 
