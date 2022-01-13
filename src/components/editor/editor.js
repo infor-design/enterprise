3f8350b928b47793f67c557510d3352d99773d1f
Dave Berg <dave.berg@infor.com>
2022-01-12 20:01:09 -0500

    Swap back to ternary to avoid lonely if lint error.

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1264,11 +1264,7 @@ Editor.prototype = {
             self.createLink($(`[name="em-url-${self.id}"]`, this));
           }
         } else {
-          if (self.settings.attributes.length > 1) {
-            self.insertImage($(`[data-automation-id="${self.settings.attributes[self.settings.attributes.length - 1].value}-editor-modal-input0"`).val());
-          } else {
-            self.insertImage($(`#${self.settings.attributes[0].value}-editor-modal-input0`).val());
-          }
+          self.settings.attributes.length > 1 ? self.insertImage($(`[data-automation-id="${self.settings.attributes[self.settings.attributes.length - 1].value}-editor-modal-input0"`).val()) : self.insertImage($(`#${self.settings.attributes[0].value}-editor-modal-input0`).val());
         }
       });
 
