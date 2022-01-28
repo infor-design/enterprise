63ba87136aa0fff196406991bf4e3e208fc6359b
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2022-01-29 00:38:19 +0800

    5988. Changes

--- src/components/monthview/monthview.js
+++ src/components/monthview/monthview.js
@@ -494,12 +494,33 @@ MonthView.prototype = {
   },
 
   /**
+   * Loads legend list to the monthview settings.
+   * @returns {void}
+   */
+  loadLegend(legendList, isReset) {
+    if (legendList === undefined || legendList.length < 1) {
+      return;
+    }
+
+    if (isReset) {
+      this.settings.legend = legendList;
+    } else {
+      // Push new dates into legend list in the monthview settings.
+      legendList.forEach((value) => {
+        this.settings.legend.push(value);
+      });
+    }
+  },
+
+  /**
    * Update the calendar to show the given month and year
    * @param {number} month The zero based month to display
    * @param {number} year The year to display
    * @returns {void}
    */
   showMonth(month, year) {
+    this.element.trigger('beforemonthrendered');
+
     const self = this;
     const s = this.settings;
     const now = new Date();
@@ -507,7 +528,7 @@ MonthView.prototype = {
     now.setHours(0);
     now.setMinutes(0);
     now.setSeconds(0);
-
+    
     let elementDate;
     if (this.isIslamic) {
       elementDate = s.activeDate || Locale.gregorianToUmalqura(now);
@@ -766,6 +787,7 @@ MonthView.prototype = {
     * @property {object} args.api - The MonthView api
     */
     this.element.trigger('monthrendered', { year, month, elem: this.element, api: this });
+    this.element.trigger('aftermonthrendered');
   },
 
   /**
