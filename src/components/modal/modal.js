e20b993cb774c259cab1db85f50a7e2e99d6e849
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2022-02-03 19:31:37 +0800

    6086. Lint

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1047,7 +1047,13 @@ Modal.prototype = {
         const observer = new MutationObserver((mutationList) => {
           mutationList.forEach((mutation) => {
             const mutationTarget = $(mutation.target);
-            mutationTarget.is(':visible') ? mutationTarget.attr('tabindex', '0') : mutationTarget.attr('tabindex', '-1');
+            // mutationTarget.is(':visible') ? mutationTarget.attr('tabindex', '0') : mutationTarget.attr('tabindex', '-1');
+
+            if (mutationTarget.is(':visible')) {
+              mutationTarget.attr('tabindex', '0')
+            } else {
+              mutationTarget.attr('tabindex', '-1');
+            }
           });
         });
 
