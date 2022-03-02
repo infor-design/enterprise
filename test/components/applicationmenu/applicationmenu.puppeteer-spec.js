9f8f599978de4f62dc43d15ce4dbb7625cfc1b01
Eric Angeles <enrico.angeles@infor.com>
2022-03-02 15:51:09 +0800

    fix lint

--- test/components/applicationmenu/applicationmenu.puppeteer-spec.js
+++ test/components/applicationmenu/applicationmenu.puppeteer-spec.js
@@ -349,110 +349,110 @@ describe('Application Menu Puppeteer Test', () => {
       expect(await page.waitForSelector('#application-menu', { visible: true })).toBeTruthy();
     });
   });
-});
 
-describe('Resize', () => {
-  const url = `${baseUrl}/example-resizable-menu`;
+  describe('Resize', () => {
+    const url = `${baseUrl}/example-resizable-menu`;
 
-  /**
-   * Check if application menu is visible
-   * @param {string} selector selector of app menu
-   * @param {boolean} isVisible if app menu should be visible or not
-   */
-  function checkVisibility(selector, isVisible) {
-    return page.waitForSelector(selector, { visible: isVisible, hidden: !isVisible })
-      .then(element => Promise.all([element, element.evaluate(dom => getComputedStyle(dom).getPropertyValue('display'))]))
-      .then((output) => {
-        if (isVisible) {
-          expect(output[1]).not.toEqual('none');
-        } else {
-          expect(output[1]).toEqual('none');
-        }
-        return output[0];
-      });
-  }
+    /**
+     * Check if application menu is visible
+     * @param {string} selector selector of app menu
+     * @param {boolean} isVisible if app menu should be visible or not
+     */
+    function checkVisibility(selector, isVisible) {
+      return page.waitForSelector(selector, { visible: isVisible, hidden: !isVisible })
+        .then(element => Promise.all([element, element.evaluate(dom => getComputedStyle(dom).getPropertyValue('display'))]))
+        .then((output) => {
+          if (isVisible) {
+            expect(output[1]).not.toEqual('none');
+          } else {
+            expect(output[1]).toEqual('none');
+          }
+          return output[0];
+        });
+    }
 
-  beforeEach(async () => {
-    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
-  });
+    beforeEach(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
+    });
 
-  it('should resize at middle of the page', async () => {
-    const windowSize = await page.viewport();
-    const location = [{ y: 0, x: windowSize.width / 2 }];
+    it('should resize at middle of the page', async () => {
+      const windowSize = await page.viewport();
+      const location = [{ y: 0, x: windowSize.width / 2 }];
 
-    await dragAndDrop('.resizer', location);
+      await dragAndDrop('.resizer', location);
 
-    // hamburger icon should be visible
-    let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    expect(hamburger).toBeDefined();
+      // hamburger icon should be visible
+      let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      expect(hamburger).toBeDefined();
 
-    // Menu size after being dragged
-    const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
+      // Menu size after being dragged
+      const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
 
-    hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', false);
+      await checkVisibility('nav#application-menu', false);
 
-    hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', true)
-      .then(element => element.boundingBox())
-      .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
-  });
+      await checkVisibility('nav#application-menu', true)
+        .then(element => element.boundingBox())
+        .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
+    });
 
-  it.skip('should resize at the near end of the page', async () => {
-    const windowSize = await page.viewport();
-    const location = [{ y: 0, x: windowSize.width - (windowSize.width * 0.1) }];
+    it.skip('should resize at the near end of the page', async () => {
+      const windowSize = await page.viewport();
+      const location = [{ y: 0, x: windowSize.width - (windowSize.width * 0.1) }];
 
-    await dragAndDrop('.resizer', location);
+      await dragAndDrop('.resizer', location);
 
-    // hamburger icon should be visible
-    let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    expect(hamburger).toBeDefined();
+      // hamburger icon should be visible
+      let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      expect(hamburger).toBeDefined();
 
-    // Menu size after being dragged
-    const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
+      // Menu size after being dragged
+      const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
 
-    hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', false);
+      await checkVisibility('nav#application-menu', false);
 
-    hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', true)
-      .then(element => element.boundingBox())
-      .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
-  });
+      await checkVisibility('nav#application-menu', true)
+        .then(element => element.boundingBox())
+        .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
+    });
 
-  it.skip('should save last resize', async () => {
-    const windowSize = await page.viewport();
-    const location = [{ y: 0, x: windowSize.width - (windowSize.width * 0.1) }];
+    it.skip('should save last resize', async () => {
+      const windowSize = await page.viewport();
+      const location = [{ y: 0, x: windowSize.width - (windowSize.width * 0.1) }];
 
-    await dragAndDrop('.resizer', location);
+      await dragAndDrop('.resizer', location);
 
-    const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
+      const menuSize = await checkVisibility('nav#application-menu', true).then(element => element.boundingBox());
 
-    let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      let hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', false);
+      await checkVisibility('nav#application-menu', false);
 
-    hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
-    await hamburger.click();
+      hamburger = await page.waitForSelector('button#header-hamburger', { visible: true });
+      await hamburger.click();
 
-    await checkVisibility('nav#application-menu', true)
-      .then(element => element.boundingBox())
-      .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
+      await checkVisibility('nav#application-menu', true)
+        .then(element => element.boundingBox())
+        .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
 
-    // refresh page
-    await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
+      // refresh page
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle2'] });
 
-    await checkVisibility('nav#application-menu', true)
-      .then(element => element.boundingBox())
-      .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
+      await checkVisibility('nav#application-menu', true)
+        .then(element => element.boundingBox())
+        .then(boundingBox => expect(boundingBox.width).toEqual(menuSize.width));
+    });
   });
 });
