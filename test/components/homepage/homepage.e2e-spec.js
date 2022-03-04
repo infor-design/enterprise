d57b16c26b41a7161bb4c7f39f2c00440625c45d
Dave Berg <dave.berg@infor.com>
2022-03-04 10:03:58 -0500

    Remove visual regression tests from protractor file.

--- test/components/homepage/homepage.e2e-spec.js
+++ test/components/homepage/homepage.e2e-spec.js
@@ -11,50 +11,6 @@ function checkLeftAndTop(elem, pos) {
   expect(elemStyle).toContain(`top: ${pos.top};`);
 }
 
-describe('Homepage example hero widget tests', () => {
-  beforeEach(async () => {
-    await utils.setPage('/components/homepage/example-hero-widget.html?theme=classic');
-  });
-
-  it('Should not have errors', async () => {
-    await utils.checkForErrors();
-  });
-
-  if (utils.isChrome() && utils.isCI()) {
-    it('Should not visual regress', async () => {
-      const containerEl = await element(by.css('body'));
-      await browser.driver
-        .wait(protractor.ExpectedConditions.presenceOf(containerEl), config.waitsFor);
-      await browser.driver.sleep(config.sleepLonger);
-
-      expect(await browser.imageComparison.checkScreen('homepage-hero-widget')).toEqual(0);
-    });
-  }
-});
-
-describe('Homepage example editable tests', () => {
-  beforeEach(async () => {
-    await utils.setPage('/components/homepage/example-editable.html?theme=classic');
-  });
-
-  it('Should not have errors', async () => {
-    await utils.checkForErrors();
-  });
-
-  if (utils.isChrome() && utils.isCI()) {
-    it('Should not visual regress', async () => {
-      const containerEl = await element(by.css('body'));
-      await browser.driver
-        .wait(protractor.ExpectedConditions.presenceOf(containerEl), config.waitsFor);
-      await browser.driver.sleep(config.sleep);
-
-      browser.actions().mouseMove(element(by.css('.widget'))).perform();
-
-      expect(await browser.imageComparison.checkScreen('homepage-editable')).toEqual(0);
-    });
-  }
-});
-
 describe('Homepage example five column tests', () => {
   beforeEach(async () => {
     await utils.setPage('/components/homepage/example-five-column.html');
