e18994a2c13d8a567d7643d7bf9a6e204cb75d17
Dave Berg <dave.berg@infor.com>
2022-03-06 10:23:24 -0500

    Add puppeteer visual regression test. Remove protractor visual regression test.

--- test/components/pie/pie.e2e-spec.js
+++ test/components/pie/pie.e2e-spec.js
@@ -20,17 +20,4 @@ describe('Pie Chart tests', () => {
     expect(await element(by.id('item-a-legend-0')).getAttribute('id')).toEqual('item-a-legend-0');
     expect(await element(by.id('item-a-legend-0')).getAttribute('data-automation-id')).toEqual('item-a-automation-id-legend-0');
   });
-
-  if (utils.isChrome() && utils.isCI()) {
-    it('Should not visual regress', async () => {
-      const containerEl = await element(by.className('container'));
-      await browser.driver.sleep(config.sleep);
-
-      await browser.driver
-        .wait(protractor.ExpectedConditions.visibilityOf(containerEl), config.waitsFor);
-      await browser.driver.sleep(config.sleep);
-
-      expect(await browser.imageComparison.checkElement(containerEl, 'pie')).toEqual(0);
-    });
-  }
 });
