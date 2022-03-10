fbc92af5643adf8f408c9a6f29f75a47f72653f5
Cheno Barcena <johnedwincheno.barcena@infor.com>
2022-03-10 16:09:43 +0800

    Create a Puppeteer Script for change Launch Icon

---
+++ test/components/icons/icon.puppeteer-spec.js
@@ -0,0 +1,22 @@
+const { getConfig } = require('../../helpers/e2e-utils.js');
+
+describe('Icons Puppeteer Tests', () => {
+  describe('Index', () => {
+    const url = 'http://localhost:4000/components/icons/example-index.html';
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+      await page.setViewport({ width: 1920, height: 1080 });
+    });
+
+    it('should have new ids identity for launch icon', async () => {
+      // https://github.com/infor-design/enterprise/pull/5596
+      const launchIcon = await page.$eval('div.demo-svg[title=launch]', element => element.innerHTML);
+      expect(launchIcon).toContain('<use href="#icon-launch"></use>');
+
+      const icon = await page.$('div.demo-svg[title=launch]');
+      const image = await icon.screenshot();
+      const config = getConfig('icon-launch');
+      expect(image).toMatchImageSnapshot(config);
+    });
+  });
+});
