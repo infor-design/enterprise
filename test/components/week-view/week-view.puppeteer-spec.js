3a30b2d6a40f0bdf505f149efbff6d4e33e6243a
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2022-05-05 20:26:40 +0800

    6260. Remove Protractor Tests.

--- test/components/week-view/week-view.puppeteer-spec.js
+++ test/components/week-view/week-view.puppeteer-spec.js
@@ -1,3 +1,5 @@
+const { getConfig } = require('../../helpers/e2e-utils.js');
+
 describe('Week View Puppeteer Tests', () => {
   const baseUrl = 'http://localhost:4000/components/week-view';
 
@@ -53,6 +55,55 @@ describe('Week View Puppeteer Tests', () => {
       await page.click('.today');
       expect(await page.$eval('.hidden.year', el => el.innerHTML)).toBe(validateYear);
     });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-index');
+      expect(image).toMatchImageSnapshot(config);
+    });
+  });
+
+  describe('WeekView Events Tests', () => {
+    const url = `${baseUrl}/test-events?theme=classic&layout=nofrills`;
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    it('should show the title', async () => {
+      await expect(page.title()).resolves.toMatch('IDS Enterprise');
+    });
+
+    it('should render without error', async () => {
+      await page.waitForSelector('.calendar-event', { visible: true })
+        .then(element => expect(element).toBeTruthy());
+    });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-events');
+      expect(image).toMatchImageSnapshot(config);
+    });
   });
 
   describe('WeekView Loading Tests', () => {
@@ -69,6 +120,22 @@ describe('Week View Puppeteer Tests', () => {
     it('should render without error', async () => {
       await page.waitForSelector('.calendar-event', { visible: true })
         .then(element => expect(element).toBeTruthy());
+    });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-loading');
+      expect(image).toMatchImageSnapshot(config);
     });
   });
 
@@ -87,6 +154,22 @@ describe('Week View Puppeteer Tests', () => {
       await page.waitForSelector('.week-view-table th', { visible: true })
         .then(element => expect(element).toBeTruthy());
     });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-two-weeks');
+      expect(image).toMatchImageSnapshot(config);
+    });
   });
 
   describe('WeekView One Day Tests', () => {
@@ -104,6 +187,22 @@ describe('Week View Puppeteer Tests', () => {
       await page.waitForSelector('.week-view-table th', { visible: true })
         .then(element => expect(element).toBeTruthy());
     });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-one-day');
+      expect(image).toMatchImageSnapshot(config);
+    });
   });
 
   describe('WeekView Two Day Tests', () => {
@@ -120,6 +219,22 @@ describe('Week View Puppeteer Tests', () => {
     it('should render without error', async () => {
       await page.waitForSelector('.week-view-table th', { visible: true })
         .then(element => expect(element).toBeTruthy());
+    });
+
+    it('should not visual regress', async () => {
+      await page.setViewport({ width: 1200, height: 800 });
+
+      expect(await page.waitForSelector('.week-view')).toBeTruthy();
+
+      // Need a bit of delay to show the modal perfectly
+      await page.waitForTimeout(200);
+
+      // Screenshot of the page
+      const image = await page.screenshot();
+
+      // Set a custom name of the snapshot
+      const config = getConfig('week-view-two-day');
+      expect(image).toMatchImageSnapshot(config);
     });
   });
 });
