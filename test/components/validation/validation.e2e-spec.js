82bd714e560c9aaba0805fbabf78eed3d78eb0ca
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2022-05-21 17:39:00 +0800

    6253. Restore E2E

--- test/components/validation/validation.e2e-spec.js
+++ test/components/validation/validation.e2e-spec.js
@@ -105,10 +105,16 @@ describe('Validation multiple error tests', () => {
 
   it('Should be able to call removeError after addError', async () => {
     await browser.executeScript('$("select.dropdown").removeError().addError({ message: "Dropdown error.", inline: false })');
-    expect(await element(by.css('.icon-error')).isPresent()).toBe(false);
+    await browser.driver
+      .wait(protractor.ExpectedConditions.presenceOf(await element(by.css('.icon-error'))), config.waitsFor);
+
+    expect(await element(by.css('.icon-error'))).toBeTruthy();
 
     await browser.executeScript('$("select.dropdown").removeError()');
-    expect(await element(by.css('.icon-error')).isPresent()).toBe(false);
+    await browser.driver
+      .wait(protractor.ExpectedConditions.stalenessOf(await element(by.css('.icon-error'))), config.waitsFor);
+
+    expect(await element(by.css('.icon-error'))).toBeTruthy();
   });
 });
 
