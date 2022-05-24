843407f053d80565ddd75116c2ff0dcf8fe15516
Artem Golub <artemgolub@gmail.com>
2022-05-24 13:18:11 +0300

    Release CI updates.

--- scripts/docs-publish/run.sh
+++ scripts/docs-publish/run.sh
@@ -8,23 +8,33 @@ _BRANCH=${BRANCH:-}
 
 _NPM_TOKEN=${NPM_TOKEN:-}
 _NPM_COMMAND=${NPM_COMMAND:-}
+_NPM_LATEST=${NPM_LATEST:-}
 _RELEASEIT_FLAGS=${RELEASEIT_FLAGS:-}
+_RELEASE_INCREMENT=${RELEASE_INCREMENT:-}
 
-rm -fr /root/enterprise/*
+rm -fr /root/enterprise
 git clone https://$_GITHUB_ACCESS_TOKEN@github.com/infor-design/enterprise.git /root/enterprise
 cd /root/enterprise
 git remote set-url origin git@github.com:infor-design/enterprise.git
 
-git checkout $_BRANCH && git pull --tags
+git checkout $_BRANCH
 
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 npm install
 npm run build
 
-if [[ -n "$_RELEASEIT_FLAGS" ]];
+if [ -n "$_RELEASEIT_FLAGS" ];
 then
-  release-it $_RELEASEIT_FLAGS --config .release-it.json --ci
+  release-it $_RELEASEIT_FLAGS --config .release-it.json --ci -- $_RELEASE_INCREMENT
 else
   npmcmd=($_NPM_COMMAND)
   "${npmcmd[@]}"
+fi
+
+if [[ "$_RELEASEIT_FLAGS" != *"--dry-run"* ]];
+then
+  if [ -n "$_NPM_LATEST" ];
+  then
+    npm dist-tags add ids-enterprise@$_NPM_LATEST latest
+  fi
 fi
