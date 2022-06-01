42434bcc23a689ddc34360ec6da12dea094d0c3d
James Allen <jamesallen.tadique@infor.com>
2022-06-01 10:00:37 +0800

    Chore. Added snapshots and remove protractor tests.

--- test/components/tooltip/tooltip.puppeteer-spec.js
+++ test/components/tooltip/tooltip.puppeteer-spec.js
@@ -1,5 +1,8 @@
+const { getConfig } = require('../../helpers/e2e-utils.js');
+
 describe('Tooltip Puppeteer Tests', () => {
   const baseUrl = 'http://localhost:4000/components/tooltip';
+
   describe('Tooltips Index Page Tests', () => {
     const url = `${baseUrl}/example-index?theme=classic&layout=nofrills`;
 
@@ -24,6 +27,18 @@ describe('Tooltip Puppeteer Tests', () => {
       await page.waitForSelector('.tooltip.is-open', { visible: true })
         .then(element => expect(element).toBeTruthy());
     });
+
+    it('should not visual regress', async () => {
+      const button = await page.$('#tooltip-btn');
+      button.hover();
+
+      await page.waitForSelector('.tooltip.is-open', { visible: true })
+        .then(element => expect(element).toBeTruthy());
+
+      const image = await page.screenshot();
+      const config = getConfig('tooltip-index');
+      expect(image).toMatchImageSnapshot(config);
+    });
   });
 
   describe('Tooltips on icon tests', () => {
@@ -44,6 +59,18 @@ describe('Tooltip Puppeteer Tests', () => {
       await page.waitForSelector('.tooltip.is-open', { visible: true })
         .then(element => expect(element).toBeTruthy());
     });
+
+    it('should not visual regress', async () => {
+      const button = await page.$('#standalone-delete-icon');
+      button.hover();
+
+      await page.waitForSelector('.tooltip.is-open', { visible: true })
+        .then(element => expect(element).toBeTruthy());
+
+      const image = await page.screenshot();
+      const config = getConfig('tooltip-on-icons');
+      expect(image).toMatchImageSnapshot(config);
+    });
   });
 
   describe('Tooltips icons page tests', () => {
@@ -63,6 +90,18 @@ describe('Tooltip Puppeteer Tests', () => {
 
       await page.waitForSelector('.tooltip.is-open', { visible: true })
         .then(element => expect(element).toBeTruthy());
+    });
+
+    it('should not visual regress', async () => {
+      const button = await page.$('#tooltip-btn');
+      button.hover();
+
+      await page.waitForSelector('.tooltip.is-open', { visible: true })
+        .then(element => expect(element).toBeTruthy());
+
+      const image = await page.screenshot();
+      const config = getConfig('tooltip-with-icon');
+      expect(image).toMatchImageSnapshot(config);
     });
   });
 });
