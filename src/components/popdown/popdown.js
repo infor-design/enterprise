8df95b3d3078b0b43a69c9cea0c05a0af15f3265
Yohannah Bautista <yohannahbautista@gmail.com>
2022-08-02 16:11:31 +0800

    NG1304. Fixed popdown not closing when clicking outside in NG

--- src/components/popdown/popdown.js
+++ src/components/popdown/popdown.js
@@ -141,12 +141,13 @@ Popdown.prototype = {
       })
       .on('updated.popdown', () => {
         self.updated();
-      })
-      .on('clickoutside.popdown', (e) => {
-        if (!self.settings.keepOpen) {
-          self.handleFocusOut(e);
-        }
       });
+
+    $(document).on('click', (e) => {
+      if (!self.element.is($(e.target)) && !self.settings.keepOpen) {
+        self.handleFocusOut(e);
+      }
+    });
 
     // First and last tab
     this.setFirstLastTab();
@@ -408,7 +409,9 @@ Popdown.prototype = {
    */
   handleFocusOut(e) {
     const self = this;
+    console.log('focus')
     if (self.focusableElems.includes(e.target) || self.hasFocus(e.target)) {
+      console.log(3)
       self.keyTarget = e.target;
       return;
     }
@@ -416,6 +419,7 @@ Popdown.prototype = {
     // near the front or back are focused. `keyTarget` detects what was previously clicked
     // and is used as an additional element check in these cases.
     if (e.target.tagName === 'BODY' && self.keyTarget) {
+      console.log(4)
       delete self.keyTarget;
       return;
     }
