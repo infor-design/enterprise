21143668116c849f8cf85c331c9152aab09da970
Cheno Barcena <johnedwincheno.barcena@infor.com>
2022-09-08 12:40:40 +0800

    add pptr test for status border and status color

--- test/components/colors/colors.puppeteer-spec.js
+++ test/components/colors/colors.puppeteer-spec.js
@@ -46,4 +46,59 @@ describe('Colors Puppeteer Tests', () => {
       expect(image).toMatchImageSnapshot(config);
     });
   });
+
+  describe('Status color and border tests', () => {
+    const url = `${baseUrl}/example-index`;
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    let hasFailed = false;
+    const checkElementsClassname = async (selector, element, type) => {
+      const elHandleArray = await page.$$(selector);
+      let index = 1;
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const eL of elHandleArray) {
+        await eL.click();
+        await page.waitForSelector('.palette-label', { visible: true });
+        const elemHandle = await page.$(`${selector}:nth-child(${index})  ${element}`);
+        const elemClass = await page.evaluate(elem => elem.getAttribute('class'), elemHandle);
+        try {
+          switch (index) {
+            case 1:
+              expect(elemClass).toContain(`status-0${index}-${type}`);
+              break;
+            case 2:
+              expect(elemClass).toContain(`status-0${index}-${type}`);
+              break;
+            case 3:
+              expect(elemClass).toContain(`status-0${index}-${type}`);
+              break;
+            case 4:
+              expect(elemClass).toContain(`status-0${index}-${type}`);
+              break;
+            case 5:
+              expect(elemClass).toContain(`status-0${index}-${type}`);
+              break;
+            default:
+              hasFailed = true;
+          }
+        } catch (error) {
+          hasFailed = true;
+        }
+        index += 1;
+      }
+      return hasFailed;
+    };
+    it('should have classes for status color borders', async () => {
+      await checkElementsClassname('#maincontent > div:nth-child(5) > .palette-column', '> div', 'border');
+      expect(await checkElementsClassname()).not.toBeTruthy();
+    });
+
+    it('should have classes for status colors for svg', async () => {
+      await checkElementsClassname('#maincontent > div:nth-child(7) > .palette-column', '> div > svg', 'color');
+      expect(await checkElementsClassname()).not.toBeTruthy();
+    });
+  });
 });
