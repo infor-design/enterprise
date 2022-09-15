c3bc2e19e3417352ed67cae018d60d02e56ca65a
Cheno Barcena <johnedwincheno.barcena@infor.com>
2022-09-15 11:14:38 +0800

    pptr test for show/hide legend

--- test/components/calendar/calendar.puppeteer-spec.js
+++ test/components/calendar/calendar.puppeteer-spec.js
@@ -492,4 +492,25 @@ describe('Calendar', () => {
       })).toBe('');
     });
   });
+
+  describe('show/hide legend', () => {
+    const url = `${baseUrl}/test-hide-legend.html`;
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+      await page.setViewport({ width: 967, height: 783 });
+    });
+
+    it('should hide legend', async () => {
+      await page.click('#show-action');
+      const elem = await page.evaluate(() => !!document.querySelector('.classIAmTryingToFind'));
+      expect(elem).toBe(false);
+    });
+
+    it('should show legend', async () => {
+      await page.click('#show-action');
+      const elem = await page.$eval('.calendar-event-legend', el => el !== null);
+      expect(elem).toBe(true);
+    });
+  });
 });
