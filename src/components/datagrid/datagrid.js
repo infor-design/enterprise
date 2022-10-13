55db31ac433d17a45306bc9fbcc49a236804db41
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2022-10-13 08:46:42 +0800

    6851. Follow-up changes for the comment.

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -3145,6 +3145,10 @@ Datagrid.prototype = {
       handle.on('mousedown.datagrid', (e) => {
         e.preventDefault();
 
+        if (self.editor && self.editor.input) {
+          self.commitCellEdit();
+        }
+
         header.drag({
           clone: true, cloneAppendTo: headers.first().parent().parent(), clonePosIsFixed: true
         })
