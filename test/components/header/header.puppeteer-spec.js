e4907e250eb5cc3e6d05428a0d9194577a76e546
Janah Intal <janahbeatriz.intal@infor.com>
2022-10-26 15:42:35 +0800

    6919-draft

---
+++ test/components/header/header.puppeteer-spec.js
@@ -0,0 +1,51 @@
+const { getConfig } = require('../../helpers/e2e-utils.js');
+
+
+describe('Header', () => {
+  const baseUrl = 'http://localhost:4000/components/header';
+
+  describe('Index', () => {
+    const url = `${baseUrl}/example-index?theme=classic&layout=nofrills`;
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    it('should display header text', async () => {
+      const headerTitle = await page.evaluate(() => document.querySelector('.title h1').textContent);
+      
+      expect(headerTitle).toEqual('Page Title');
+    });
+
+    it('should not visual regress', async () => {
+      expect(await page.waitForSelector('.container', { visible: true })).toBeTruthy();
+      const header = await page.$('.header');
+      const img = await header.screenshot();
+      const config = getConfig('header-title');
+      expect(img).toMatchImageSnapshot(config);
+    });
+  });
+
+  describe('Toolbar Categories', () => {
+    const url = `${baseUrl}/example-toolbar-flex-with-categories.html?theme=classic&mode=light&layout=nofrills`;
+
+    beforeAll(async () => {
+      await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+    });
+
+    it('should display header text', async () => {
+      const headerTitle = await page.evaluate(() => document.querySelector('.title h1').textContent);
+      
+      expect(headerTitle).toEqual('Page Title');
+    });
+
+    it('should not visual regress', async () => {
+      expect(await page.waitForSelector('.container', { visible: true })).toBeTruthy();
+      const header = await page.$('.header');
+      const img = await header.screenshot();
+      const config = getConfig('header-title');
+      expect(img).toMatchImageSnapshot(config);
+    });
+  });
+
+});
