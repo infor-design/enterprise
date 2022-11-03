b7963917e3d877e7729cd58c38ba949e962db4c2
James Allen <jamesallen.tadique@infor.com>
2022-11-03 11:12:30 +0800

    6918. Reverting Changes (3)

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -4764,7 +4764,7 @@ Datagrid.prototype = {
     containerHtml.right = containerHtml.center;
 
     for (j = 0; j < self.settings.columns.length; j++) {
-      const col = self.columnSettings(j);
+      const col = self.settings.columns[j];
       const container = this.getContainer(col.id);
       let cssClass = '';
       const defaultFormatter = col.summaryRowFormatter || col.formatter || self.defaultFormatter;
@@ -4774,7 +4774,7 @@ Datagrid.prototype = {
         dataRowIdx,
         j,
         self.fieldValue(rowData, self.settings.columns[j].field),
-        self.columnSettings(j),
+        self.settings.columns[j],
         rowData,
         self,
         formatLocale
@@ -5849,7 +5849,7 @@ Datagrid.prototype = {
     }
 
     for (let j = 0; j < this.settings.columns.length; j++) {
-      const col = this.columnSettings(j);
+      const col = this.settings.columns[j];
 
       if (col.hidden) {
         continue;
@@ -11111,9 +11111,7 @@ Datagrid.prototype = {
    * @returns {array} The settings array
    */
   columnSettings(idx) {
-    let tempColumn = this.settings.columns;
-
-    const foundColumn = tempColumn[idx];
+    let foundColumn = this.settings.columns[idx];
     return foundColumn || {};
   },
 
