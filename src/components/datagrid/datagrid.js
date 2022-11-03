71fc94dca500eaf344d849fa0d0f594481123764
James Allen <jamesallen.tadique@infor.com>
2022-11-03 11:10:26 +0800

    6918. Changes for the Issue (2).

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -431,7 +431,6 @@ Datagrid.prototype = {
     const self = this;
     this.hasLeftPane = this.settings.frozenColumns.left.length > 0;
     this.hasRightPane = this.settings.frozenColumns.right.length > 0;
-    this.hasFrozenColumns = this.settings.frozenColumns.left.length > 0 || this.settings.frozenColumns.right.length > 0;
 
     if (this.hasLeftPane) {
       self.bodyWrapperLeft = $('<div class="datagrid-wrapper left"></div>');
@@ -533,6 +532,9 @@ Datagrid.prototype = {
   */
   renderRow(data, location) {
     const self = this;
+    let tableHtml = '';
+    let tableHtmlLeft = '';
+    let tableHtmlRight = '';
 
     if (self.emptyMessageContainer) {
       self.emptyMessageContainer.hide();
@@ -543,6 +545,18 @@ Datagrid.prototype = {
     const dataIndex = self.settings.dataset.length - 1;
 
     const rowHtml = self.rowHtml(data, position, dataIndex);
+    if (self.hasLeftPane && rowHtml.left) {
+      tableHtmlLeft += rowHtml.left;
+    }
+
+    if (rowHtml.center) {
+      tableHtml += rowHtml.center;
+    }
+    
+    if (self.hasRightPane && rowHtml.right) {
+      tableHtmlRight += rowHtml.right;
+    }
+
     if (self.settings.groupable) {
       const groups = $('.datagrid-rowgroup-header').find('span:not([class])');
       for (let i = 0; i < groups.length; i++) {
@@ -560,7 +574,15 @@ Datagrid.prototype = {
       const newActivePage = (location === 'bottom' ? self.pagerAPI.pageCount() : 1) + newPage;
 
       if (location !== 'bottom' && self.pagerAPI.activePage === 1) {
-        DOM.prepend(self.tableBody, rowHtml.center, '*');
+        if (self.hasLeftPane) {
+          DOM.prepend(self.tableBodyLeft, tableHtmlLeft, '*');
+        }
+
+        DOM.prepend(self.tableBody, tableHtml, '*');
+
+        if (self.hasRightPane) {
+          DOM.prepend(self.tableBodyRight, tableHtmlRight, '*');
+        }
       } else {
         self.pagerAPI.setActivePage(newActivePage, false, operationType);
         self.pagerAPI.triggerPagingEvents(self.pagerAPI.currentPage);
@@ -569,9 +591,25 @@ Datagrid.prototype = {
 
     if (!self.settings.paging && !self.settings.groupable) {
       if (location !== 'bottom') {
-        DOM.prepend(self.tableBody, rowHtml.center, '*');
+        if (self.hasLeftPane) {
+          DOM.prepend(self.tableBodyLeft, tableHtmlLeft, '*');
+        }
+
+        DOM.prepend(self.tableBody, tableHtml, '*');
+
+        if (self.hasRightPane) {
+          DOM.prepend(self.tableBodyRight, tableHtmlRight, '*');
+        }
       } else {
-        DOM.append(self.tableBody, rowHtml.center, '*');
+        if (self.hasLeftPane) {
+          DOM.append(self.tableBodyLeft, tableHtmlLeft, '*');
+        }
+
+        DOM.append(self.tableBody, tableHtml, '*');
+
+        if (self.hasRightPane) {
+          DOM.append(self.tableBodyRight, tableHtmlRight, '*');
+        }
       }
     }
 
@@ -581,7 +619,6 @@ Datagrid.prototype = {
       }
     }
 
-    self.refreshIndexes();
     self.afterRender();
   },
 
