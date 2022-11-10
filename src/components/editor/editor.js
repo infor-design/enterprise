040e9c7decac4244e8a0c6896272291d751254e4
James Allen <jamesallen.tadique@infor.com>
2022-11-10 09:34:21 +0800

    6892. Fix Lint Error

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1804,11 +1804,11 @@ Editor.prototype = {
 
       function getSanitizedPlainText(toOutput) {
         return toOutput.replace(/\&/g, '&amp;')
-        .replace(/\</g, '&lt;')
-        .replace(/\>/g, '&gt;')
-        .replace(/\"/g, '&quot;')
-        .replace(/\'/g, '&#x27;')
-        .replace(/\//g, '&#x2F;');
+          .replace(/\</g, '&lt;')
+          .replace(/\>/g, '&gt;')
+          .replace(/\"/g, '&quot;')
+          .replace(/\'/g, '&#x27;')
+          .replace(/\//g, '&#x2F;');
       }
 
       if (clipboardData && clipboardData.types) {
