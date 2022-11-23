930191f4115962a00f061fe3e3c1ad020ad6debc
Janah Intal <janahbeatriz.intal@infor.com>
2022-11-23 15:57:51 +0800

    6989 draft

--- test/components/radar/radar.puppeteer-spec.js
+++ test/components/radar/radar.puppeteer-spec.js
@@ -1,84 +1,77 @@
-const { checkClassNameValue } = require('../../helpers/e2e-utils.js');
+//import { getConfig } from "../../helpers/e2e-utils.js";
 
-describe('Radar Puppeteer Tests', () => {
-  describe('Radar Disable Selection  State Tests', () => {
-    const url = 'http://localhost:4000/components/radar/example-disable-selection-state.html';
+describe('Radar', () => {
+  const baseUrl = 'http://localhost:4000/components/radar';
+
+  describe('Index tests', () => {
+    const url = `${baseUrl}/example-index?theme=classic&layout=nofrills`
 
     beforeAll(async () => {
       await page.goto(url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
     });
 
-    it('should not able to tab through the legends', async () => {
-      const legendTabIndex = await page.$$eval('.chart-legend-item', e => e.map(el => el.tabIndex));
-      expect(legendTabIndex).toEqual([-1, -1, -1]); // These are the values of tabindex of all the legends.
+    it('should be able to set id/automation id', async () => {
+      await page.evaluate(() => document.getElementById('radar-iphone-battery-life-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-battery-life-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-battery-life-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-battery-life-circle'));
+
+      await page.evaluate(() => document.getElementById('radar-iphone-brand-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-brand-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-brand-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-brand-circle'));
+
+      await page.evaluate(() => document.getElementById('radar-iphone-cost-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-cost-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-cost-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-cost-circle'));
+
+      await page.evaluate(() => document.getElementById('radar-iphone-design-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-design-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-design-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-design-circle'));
+
+      await page.evaluate(() => document.getElementById('radar-iphone-connectivity-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-connectivity-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-connectivity-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-connectivity-circle'));
+
+      await page.evaluate(() => document.getElementById('radar-iphone-screen-circle').getAttribute('id'))
+        .then(id => expect(id).toEqual('radar-iphone-screen-circle'));
+      await page.evaluate(() => document.getElementById('radar-iphone-screen-circle').getAttribute('data-automation-id'))
+        .then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-screen-circle'));
+
+////
+await page.evaluate(() => document.getElementById('radar-iphone-price-circle').getAttribute('id'))
+.then(id => expect(id).toEqual('radar-iphone-price-circle'));
+await page.evaluate(() => document.getElementById('radar-iphone-price-circle').getAttribute('data-automation-id'))
+.then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-iphone-price-circle'));
+
+await page.evaluate(() => document.getElementById('radar-samsung-battery-life-circle').getAttribute('id'))
+.then(id => expect(id).toEqual('radar-samsung-battery-life-circle'));
+await page.evaluate(() => document.getElementById('radar-samsung-battery-life-circle').getAttribute('data-automation-id'))
+.then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-samsung-battery-life-circle'));
+
+await page.evaluate(() => document.getElementById('radar-samsung-brand-circle').getAttribute('id'))
+.then(id => expect(id).toEqual('radar-samsung-brand-circle'));
+await page.evaluate(() => document.getElementById('radar-samsung-brand-circle').getAttribute('data-automation-id'))
+.then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-samsung-brand-circle'));
+
+await page.evaluate(() => document.getElementById('radar-samsung-cost-circle').getAttribute('id'))
+.then(id => expect(id).toEqual('radar-samsung-cost-circle'));
+await page.evaluate(() => document.getElementById('radar-samsung-cost-circle').getAttribute('data-automation-id'))
+.then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-samsung-cost-circle'));
+
+await page.evaluate(() => document.getElementById('radar-samsung-design-circle').getAttribute('id'))
+.then(id => expect(id).toEqual('radar-samsung-design-circle'));
+await page.evaluate(() => document.getElementById('radar-samsung-design-circle').getAttribute('data-automation-id'))
+.then(dataAutomationId => expect(dataAutomationId).toEqual('automation-id-radar-samsung-design-circle'));
+
+
+
+
     });
 
-    it('should not highlight when selected', async () => {
-      const isFailed = [];
-      const iphone = '#radar-iphone-area';
-      const samsung = '#radar-samsung-area';
-      const nokia = '#radar-nokia-area';
-      await page.click(iphone);
-      await page.waitForTimeout(200);
-      isFailed.push(await checkClassNameValue(iphone, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(samsung, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(nokia, 'chart-radar-area'));
-      await page.click(iphone);
-      await page.waitForTimeout(200);
-
-      await page.click(samsung);
-      await page.waitForTimeout(200);
-      isFailed.push(await checkClassNameValue(iphone, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(samsung, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(nokia, 'chart-radar-area'));
-      await page.click(samsung);
-      await page.waitForTimeout(200);
-
-      await page.click(nokia);
-      await page.waitForTimeout(200);
-      isFailed.push(await checkClassNameValue(iphone, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(samsung, 'chart-radar-area'));
-      isFailed.push(await checkClassNameValue(nokia, 'chart-radar-area'));
-      await page.click(nokia);
-      await page.waitForTimeout(200);
-      expect(isFailed).not.toContain(true);
-    });
-
-    it('should not highlight on click in legends', async () => {
-      const elHandleArray = await page.$$('.chart-radar-wrapper');
-      const isFailed = [];
-      let index = 0;
-      const iphone = '#radar-iphone-area';
-      const samsung = '#radar-samsung-area';
-      const nokia = '#radar-nokia-area';
-      // eslint-disable-next-line no-restricted-syntax
-      for await (const eL of elHandleArray) {
-        let brand = '';
-        await eL.click();
-        switch (index) {
-          case 0: // Line A
-            brand = 'iphone';
-            break;
-          case 1: // Line B
-            brand = 'samsung';
-            break;
-          case 2: // Line C
-            brand = 'nokia';
-            break;
-          default:
-            console.warn('line-group element not found');
-        }
-        await page.click(`#radar-${brand}-legend-${index}`);
-        await page.waitForTimeout(200);
-        isFailed.push(await checkClassNameValue(iphone, 'chart-radar-area'));
-        isFailed.push(await checkClassNameValue(samsung, 'chart-radar-area'));
-        isFailed.push(await checkClassNameValue(nokia, 'chart-radar-area'));
-        await page.click(`#radar-${brand}-legend-${index}`);
-        await page.waitForTimeout(200);
-        index += 1;
-      }
-      expect(isFailed).not.toContain(true);
-    });
   });
-});
 
+}); 
