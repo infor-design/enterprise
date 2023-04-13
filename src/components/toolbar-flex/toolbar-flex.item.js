28ba42b13ca0810f42f24d6494c51b05bb71551e
James Allen <34049950+tjamesallen15@users.noreply.github.com>
2023-04-13 10:30:11 +0800

    Remove aria-disabled

--- src/components/toolbar-flex/toolbar-flex.item.js
+++ src/components/toolbar-flex/toolbar-flex.item.js
@@ -314,13 +314,11 @@ ToolbarFlexItem.prototype = {
   set disabled(boolean) {
     if (boolean) {
       this.element.disabled = true;
-      this.element.setAttribute('aria-disabled', true);
       this.element.readOnly = false;
       return;
     }
 
     this.element.disabled = false;
-    this.element.removeAttribute('aria-disabled');
   },
 
   /**
