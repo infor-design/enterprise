cd42c47c4c36ad951fc79fa62df0cc6a3dd1ada4
corpulent <artemgolub@gmail.com>
2023-06-05 15:38:23 +0300

    normalize service string, minor cleanup

--- scripts/publish-docs/run.sh
+++ scripts/publish-docs/run.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
-
 set -e
 
-export TERM=xterm
+source ./utils.sh
+trap exit_trap EXIT
+
 export NODE_OPTIONS=--max_old_space_size=4096
 
-check_required_vars()
-{
-    var_names=("$@")
-    for var_name in "${var_names[@]}"; do
-        [ -z "${!var_name}" ] && echo "$var_name is unset." && var_unset=true
-    done
-    [ -n "$var_unset" ] && exit 1
-    return 0
+REPOROOT=/root/enterprise
+
+function main() {
+  check_required_vars GITHUB_ACCESS_TOKEN BUILD_FROM SITE APP_REPO
+  clean_clone_repo $GITHUB_ACCESS_TOKEN $APP_REPO $BUILD_FROM $REPOROOT
+  npm install
+  node ./scripts/deploy-documentation.js --site $SITE
 }
 
-check_required_vars \
-  GITHUB_ACCESS_TOKEN \
-  BUILD_FROM \
-  SITE \
-  APP_REPO
-
-rm -rf /root/enterprise/{..?*,.[!.]*,*} 2>/dev/null
-
-git clone https://$GITHUB_ACCESS_TOKEN@github.com/$APP_REPO.git /root/enterprise
-cd /root/enterprise
-git remote set-url origin https://${GITHUB_ACCESS_TOKEN}@github.com/$APP_REPO.git
-
-git fetch --all
-git checkout $BUILD_FROM
-
-npm install
-node ./scripts/deploy-documentation.js --site $SITE
+main "$@"
