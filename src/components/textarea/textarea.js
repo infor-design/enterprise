d8e25d22aa60a8cd3c5716393f190e40ab6d9a3f
Yohannah Bautista <yohannahbautista@gmail.com>
2023-08-25 19:04:14 +0800

    7677. Updated example page and textarea

--- src/components/textarea/textarea.js
+++ src/components/textarea/textarea.js
@@ -67,7 +67,7 @@ Textarea.prototype = {
         this.element.is('.textarea-def') ? 'input-md' : //eslint-disable-line
           this.element.is('.textarea-lg') ? 'input-lg' : ''); //eslint-disable-line
 
-    if (this.settings.characterCounter && this.getMaxLength()) {
+    if (this.settings.characterCounter || this.getMaxLength()) {
       this.counter = this.element.siblings('.textarea-wordcount');
 
       if (this.counter.length === 0) {
@@ -295,7 +295,6 @@ Textarea.prototype = {
     if (this.element.attr('maxlength')) {
       return parseInt(this.element.attr('maxlength'), 10);
     }
-
     return undefined;
   },
 
