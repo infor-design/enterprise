0dea16b45205598bae8c561cdc7c05b50d5a3443
Yohannah Bautista <yohannahbautista@gmail.com>
2023-09-26 18:35:11 +0800

    7739. 24h format and title

--- src/components/locale/locale.js
+++ src/components/locale/locale.js
@@ -655,8 +655,8 @@ const Locale = {  // eslint-disable-line
       ret = ret.replace('h', 12);
     }
 
-    ret = ret.replace('hh', (hours > 12 ? this.pad(hours - 12, 2) : this.pad(hours, 2)));
-    ret = ret.replace('h', (hours > 12 ? hours - 12 : hours));
+    ret = ret.replace('hh', (!options.militaryTime && hours > 12 ? this.pad(hours - 12, 2) : this.pad(hours, 2)));
+    ret = ret.replace('h', (!options.militaryTime && hours > 12 ? hours - 12 : hours));
     ret = ret.replace('HH', this.pad(hours, 2));
     ret = ret.replace('H', hours);
     ret = ret.replace('mm', this.pad(mins, 2));
@@ -756,7 +756,7 @@ const Locale = {  // eslint-disable-line
       date.setMinutes(parts[1] || 0);
     }
 
-    return this.formatDate(date, { date: 'hour' });
+    return this.formatDate(date, { date: 'hour', militaryTime: options.militaryTime });
   },
 
   /**
@@ -778,7 +778,7 @@ const Locale = {  // eslint-disable-line
     }
     let range = `${Locale.formatHour(startHour, options)} - ${Locale.formatHour(endHour, options)}`;
 
-    if ((range.match(/:00/g) || []).length > 1) {
+    if (!options.militaryTime && (range.match(/:00/g) || []).length > 1) {
       range = range.replaceAll(':00', '');
     }
 
@@ -793,6 +793,16 @@ const Locale = {  // eslint-disable-line
 
       if (stringUtils.count(range, dayPeriods[1]) > 1) {
         range = range.replace(` ${dayPeriods[1]}`, '');
+      }
+    }
+
+    if (options.militaryTime) {
+      if (stringUtils.count(range, dayPeriods[0]) > 1) {
+        range = range.replaceAll(dayPeriods[0], '');
+      }
+
+      if (stringUtils.count(range, dayPeriods[1]) > 1) {
+        range = range.replaceAll(` ${dayPeriods[1]}`, '');
       }
     }
 
