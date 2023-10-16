d4059c256952a115a89f23fbfcb8b1757a76e354
Yohannah Bautista <yohannahbautista@gmail.com>
2023-10-16 14:14:02 +0800

    7947. Style change

--- src/components/personalize/personalize.styles.js
+++ src/components/personalize/personalize.styles.js
@@ -448,10 +448,14 @@ html[class*="theme-new"] .header.is-personalizable:not(.has-alternate-tabs) .tab
   border-bottom: 4px solid ${colors.base};
 }
 
-.header.is-personalizable .searchfield-wrapper.has-categories button.btn-icon:not(:disabled):hover,
 .header.is-personalizable .searchfield-wrapper.has-categories button:not(.searchfield-category-button):not(:disabled):hover .icon {
-  background-color: ${colors.contrast} !important;
+  background-color: ${colors.contrast};
 }
+
+.header.is-personalizable .searchfield-wrapper.has-categories button.btn-icon:not(:disabled):hover {
+  background-color: ${colors.contrast}
+}
+
 
 html[class*="theme-new-"] .header.is-personalizable button:not(:disabled):hover .app-header.icon > span,
 html[class*="theme-new-"] .header.is-personalizable .toolbar [class^='btn']:hover:not(.go-button):not(.close):not(.searchfield-category-button):not([disabled]),
