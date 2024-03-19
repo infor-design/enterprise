8ada16333c71b47a6e8e95a859121954144c09c3
Yohannah Bautista <yohannahbautista@gmail.com>
2024-03-19 12:01:00 +0800

    8443. Update chart color correctly

--- src/components/charts/charts.js
+++ src/components/charts/charts.js
@@ -343,6 +343,7 @@ charts.addLegend = function (series, chartType, settings, container) {
   if (series.length === 0) {
     return;
   }
+
   // Legend width
   let width = 0;
   let currentWidth;
@@ -528,16 +529,21 @@ charts.addLegend = function (series, chartType, settings, container) {
 
         const hexColor = charts.chartColor(j, chartType || (series.length === 1 ? 'bar-single' : 'bar'), series[j]);
         const colorName = charts.chartColorName(j, chartType || (series.length === 1 ? 'bar-single' : 'bar'), series[j]);
-
+        let seriesColor;
         let color = '';
         if (colorName.substr(0, 1) === '#') {
           color = $('<span class="chart-popup-menu-color"></span>');
+          seriesColor = series[i].pattern;
           if (!series[i].pattern) {
             color.css('background-color', hexColor);
+            seriesColor = hexColor;
           }
         } else {
           color = $(`<span class="chart-popup-menu-color ${series[i].pattern ? '' : colorName}"></span>`);
+          seriesColor = colorName;
         }
+
+        series[j].color = seriesColor;
 
         listItem.find('div').append(color, textBlock);
         popupList.append(listItem);
@@ -638,8 +644,8 @@ charts.handleElementClick = function (idx, line, series, settings, container) {
 
   if (isTwoColumn && this.hasLegendPopup) {
     const chartType = settings.type === 'donut' ? 'pie' : settings.type;
-    const hexColor = charts.chartColor(idx, chartType || (series.length === 1 ? 'bar-single' : 'bar'), elem);
-    const colorName = charts.chartColorName(idx, chartType || (series.length === 1 ? 'bar-single' : 'bar'), elem);
+    const hexColor = charts.chartColor(idx, chartType || (series.length === 1 ? 'bar-single' : 'bar'), series[idx]);
+    const colorName = charts.chartColorName(idx, chartType || (series.length === 1 ? 'bar-single' : 'bar'), series[idx]);
     let color = '';
     if (colorName.substr(0, 1) === '#') {
       color = $('<span class="chart-legend-color"></span>');
@@ -654,6 +660,7 @@ charts.handleElementClick = function (idx, line, series, settings, container) {
     const chartLegendItem = container.find('.chart-legend-item');
 
     chartLegendItem.empty();
+    // may kulang dito somehow
     chartLegendItem.append(color, `<span class="audible">${Locale.translate('Highlight')}</span>`, textBlock);
     chartLegendItem.attr('index-id', `chart-legend-${idx}`);
     container.find('.list-button').data('popupmenu').menu.children().removeClass('is-hidden');
