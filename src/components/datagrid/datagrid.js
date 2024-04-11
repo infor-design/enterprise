990b3e5a4ba748719b2937464ad15ed78ee4f843
Yohannah Bautista <yohannahbautista@gmail.com>
2024-04-11 15:12:07 +0800

    8457. Additional check to catch tab key

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -10055,7 +10055,7 @@ Datagrid.prototype = {
           cell = ((key === 37 && !isRTL) || (key === 39 && isRTL)) ? 0 : lastCell;
           self.setActiveCell(row, cell);
         } else if ((!self.quickEditMode || (key === 9))) {
-          if (col.editor !== undefined && node.is('.is-editing')) {
+          if ((key === 9 && !self.settings.actionableMode) && (col.editor !== undefined && node.is('.is-editing'))) {
             return;
           }
 
