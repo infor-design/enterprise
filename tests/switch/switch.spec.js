7345f8cde7e1afc0a1f35d5756e2fe51fb6f6e63
Joseph Dilag <joseph.dilag@infor.com>
2024-07-09 15:21:14 -0400

    add test, add compact mixin, rename short

---
+++ tests/switch/switch.spec.js
@@ -0,0 +1,74 @@
+import AxeBuilder from '@axe-core/playwright';
+import percySnapshot from '@percy/playwright';
+import { expect } from '@playwright/test';
+import { test } from '../base-fixture';
+
+test.describe('Switch tests', () => {
+  const url = '/components/switch/example-index.html';
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+  });
+
+  test.describe('general page checks', () => {
+    test('should have a title', async ({ page }) => {
+      await expect(page).toHaveTitle('IDS Enterprise');
+    });
+  });
+
+  test.describe('accessibility tests', () => {
+    test('should pass an Axe scan', async ({ page, browserName }) => {
+      if (browserName !== 'chromium') return;
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .disableRules(['meta-viewport'])
+        .exclude('[disabled]')
+        .analyze();
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe('snapshot tests', () => {
+    test('should match the visual snapshot in percy', async ({ page, browserName }) => {
+      if (browserName !== 'chromium') return;
+      await percySnapshot(page, 'switch-light');
+    });
+  });
+
+  test.describe('functionality tests', () => {
+  });
+});
+
+test.describe('Switch Compact tests', () => {
+  const url = '/components/switch/example-compact.html';
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(url);
+  });
+
+  test.describe('general page checks', () => {
+    test('should have a title', async ({ page }) => {
+      await expect(page).toHaveTitle('IDS Enterprise');
+    });
+  });
+
+  test.describe('accessibility tests', () => {
+    test('should pass an Axe scan', async ({ page, browserName }) => {
+      if (browserName !== 'chromium') return;
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .disableRules(['meta-viewport'])
+        .exclude('[disabled]')
+        .analyze();
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe('snapshot tests', () => {
+    test('should match the visual snapshot in percy', async ({ page, browserName }) => {
+      if (browserName !== 'chromium') return;
+      await percySnapshot(page, 'switch-compact-light');
+    });
+  });
+
+  test.describe('functionality tests', () => {
+  });
+});
