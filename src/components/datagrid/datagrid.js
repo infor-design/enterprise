edda7e5d0b9be769c35760fab0e90e840ea92a1c
Tim McConechy <tim.mcconechy@infor.com>
2024-07-19 14:26:53 -0400

    Add patch

--- src/components/datagrid/datagrid.js
+++ src/components/datagrid/datagrid.js
@@ -10644,25 +10644,23 @@ Datagrid.prototype = {
       newValue = xssUtils.escapeHTML(newValue);
     }
 
-    let rowIndex;
+    let rowIndex = this.actualRowIndex(cellNode?.parent());
     let dataRowIndex;
     if (this.settings.source !== null && isUseActiveRow) {
-      if (cellNode && this.actualRowIndex(cellNode.parent()) !== this.activeCell.rowIndex) {
-        rowIndex = this.actualRowIndex(cellNode.parent());
+      if (!isNaN(rowIndex) && rowIndex !== this.activeCell.rowIndex) {
         dataRowIndex = this.dataRowIndex(cellNode.parent());
       } else {
         rowIndex = this.activeCell.rowIndex;
         dataRowIndex = this.activeCell.dataRow;
       }
     } else {
-      rowIndex = this.actualRowIndex(cellNode.parent());
       dataRowIndex = this.dataRowIndex(cellNode.parent());
     }
-
     const cell = cellNode.attr('aria-colindex') - 1;
     const col = this.columnSettings(cell);
     const rowData = this.settings.treeGrid ? this.settings.treeDepth[dataRowIndex].node :
       this.getActiveDataset()[dataRowIndex];
+
     let oldValue = this.fieldValue(rowData, col.field);
     if (col.beforeCommitCellEdit && (isFileupload || !isCallback)) {
       const vetoCommit = col.beforeCommitCellEdit({
