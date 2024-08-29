d6c35bb41d97a477759df20c9d0e14f951dfe798
James Allen <jamesallen.tadique@infor.com>
2024-08-29 19:19:52 +0800

    Fix Lint

--- tests/autocomplete/autocomplete.spec.js
+++ tests/autocomplete/autocomplete.spec.js
@@ -4,46 +4,48 @@ import { expect } from '@playwright/test';
 import { test } from '../base-fixture';
 
 test.describe('Autocomplete tests', () => {
-  const url = '/components/autocomplete/example-index.html';
+  test.describe('Autocomplete example tests', () => {
+    const url = '/components/autocomplete/example-index.html';
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto(url);
-  });
-
-  test.describe('general page checks', () => {
-    test('should have a title', async ({ page }) => {
-      await expect(page).toHaveTitle('IDS Enterprise');
+    test.beforeEach(async ({ page }) => {
+      await page.goto(url);
     });
-  });
 
-  test.describe('accessibility tests', () => {
-    test('should pass an Axe scan', async ({ page, browserName }) => {
-      if (browserName !== 'chromium') return;
-      const accessibilityScanResults = await new AxeBuilder({ page })
-        .disableRules(['meta-viewport', 'aria-required-attr'])
-        .exclude('[disabled]')
-        .analyze();
-      expect(accessibilityScanResults.violations).toEqual([]);
-    });
-  });
-
-  test.describe('snapshot tests', () => {
-    test('should match innerHTML snapshot', async ({ page, browserName }) => {
-      if (browserName !== 'chromium') return;
-      const html = await page.evaluate(() => {
-        const elem = document.querySelector('.autocomplete');
-        return elem?.outerHTML;
+    test.describe('general page checks', () => {
+      test('should have a title', async ({ page }) => {
+        await expect(page).toHaveTitle('IDS Enterprise');
       });
-      await expect(html).toMatchSnapshot('autocomplete-html');
     });
 
-    test('should match the visual snapshot in percy', async ({ page, browserName }) => {
-      if (browserName !== 'chromium') return;
-      await percySnapshot(page, 'autocomplete-light');
+    test.describe('accessibility tests', () => {
+      test('should pass an Axe scan', async ({ page, browserName }) => {
+        if (browserName !== 'chromium') return;
+        const accessibilityScanResults = await new AxeBuilder({ page })
+          .disableRules(['meta-viewport', 'aria-required-attr'])
+          .exclude('[disabled]')
+          .analyze();
+        expect(accessibilityScanResults.violations).toEqual([]);
+      });
     });
-  });
 
-  test.describe('functionality tests', () => {
+    test.describe('snapshot tests', () => {
+      test('should match innerHTML snapshot', async ({ page, browserName }) => {
+        if (browserName !== 'chromium') return;
+        const html = await page.evaluate(() => {
+          const elem = document.querySelector('.autocomplete');
+          return elem?.outerHTML;
+        });
+        await expect(html).toMatchSnapshot('autocomplete-html');
+      });
+
+      test('should match the visual snapshot in percy', async ({ page, browserName }) => {
+        if (browserName !== 'chromium') return;
+        await percySnapshot(page, 'autocomplete-light');
+      });
+    });
+
+    test.describe('functionality tests', () => {
+    });
   });
 
   test.describe('Autocomplete long list tests', () => {
